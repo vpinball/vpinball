1bc555c3a4da8a44f1c3543beb67f59851c803e6
fuzzelhjb <chschmidt9@gmail.com>
2013-11-02 20:22:29 +0000

    add switch to disable lighting on ramp images
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@699 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- resource.h
+++ resource.h
@@ -526,6 +526,7 @@
 #define IDC_CHECK_PRIMITIVE_LIGHTING    394
 #define IDC_GLOBAL_REFLECTION_CHECK     394
 #define IDC_ENABLE_OFF_LIGHTING         394
+#define IDC_ENABLE_RAMP_LIGHTING        394
 #define IDC_CHECK3                      396
 #define IDC_PRIM_ENV_MAP                396
 #define IDC_EDIT2                       397
