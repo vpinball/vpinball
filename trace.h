e35c3893f03519425726eeb1ab095b8d3a5a9cbc
fuzzelhjb <fuzzelhjb@14438d79-0ffd-4333-8ae7-ea2bdd901387>
2014-03-17 12:50:11 +0000

    trace.h added
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@940 14438d79-0ffd-4333-8ae7-ea2bdd901387

---
+++ trace.h
@@ -0,0 +1,37 @@
+#pragma once
+
+//#define ENABLE_TRACE
+
+////////////////////////////////////////////////////////////////////////////////
+
+#ifdef ENABLE_TRACE
+
+#include <d3d9.h>
+
+#define _STR(x)    # x
+#define STR(x)     _STR(x)
+#define _WIDEN(x)  L ## x
+#define WIDEN(x)   _WIDEN(x)
+
+#define TRACE_FUNCTION()  ScopedTracer(WIDEN(__FUNCTION__))
+
+class ScopedTracer
+{
+public:
+    ScopedTracer(const WCHAR *name)
+    {
+        D3DPERF_BeginEvent(RGB(0,255,0), name);
+        OutputDebugStringW(name);
+    }
+
+    ~ScopedTracer()
+    {
+        D3DPERF_EndEvent();
+    }
+};
+
+#else
+
+#define TRACE_FUNCTION()
+
+#endif
