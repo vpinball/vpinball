bd1331656be0fd094307ced0fb6f69c91665c422
fuzzelhjb <fuzzelhjb@14438d79-0ffd-4333-8ae7-ea2bdd901387>
2014-03-17 12:51:12 +0000

    hash.h added
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@941 14438d79-0ffd-4333-8ae7-ea2bdd901387

---
+++ hash.h
@@ -0,0 +1,20 @@
+#pragma once
+
+unsigned long StringHash(const unsigned char *str);
+inline unsigned long StringHash(const char *str)             { return StringHash((const unsigned char*)str); }
+
+struct StringHashFunctor
+{
+    unsigned long operator() (const char* str) const
+    {
+        return StringHash(str);
+    }
+};
+
+struct StringComparator
+{
+    bool operator()(const char* str1, const char* str2) const
+    {
+        return strcmp(str1, str2) == 0;
+    }
+};
