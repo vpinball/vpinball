769cd725d4fbdfb843c62582375151638f399594
mukuste <mukuste@14438d79-0ffd-4333-8ae7-ea2bdd901387>
2014-04-23 21:44:04 +0000

    Forgot file vpversion.h.
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@1015 14438d79-0ffd-4333-8ae7-ea2bdd901387

---
+++ vpversion.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define VP_VERSION_MAJOR    9
+#define VP_VERSION_MINOR    9
+#define VP_VERSION_REV      0
+
+#define _STR(x)    #x
+#define STR(x)     _STR(x)
+
+#define VP_VERSION_STRING STR(VP_VERSION_MAJOR) "." STR(VP_VERSION_MINOR) "." STR(VP_VERSION_REV)
+#define VP_VERSION_STRING_COMMAS STR(VP_VERSION_MAJOR) ", " STR(VP_VERSION_MINOR) ", " STR(VP_VERSION_REV) ", 0"
+
+#define PASTE2(a,b) a##b
+#define PASTE(a,b) PASTE2(a,b)
