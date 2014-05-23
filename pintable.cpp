aa83833ba525b473edbde9ba4d83f0d0c5bff7aa
mukuste <chofreither@gmail.com>
2014-05-23 21:21:27 +0000

    Solid/Outline option is now stored in registry.
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@1069 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- pintable.cpp
+++ pintable.cpp
@@ -538,7 +538,7 @@ PinTable::PinTable()
       m_activeLayers[i]=true;
    m_toggleAllLayers=false;
    m_savingActive=false;
-   m_renderSolid = true;
+   m_renderSolid = GetRegBoolWithDefault("Editor", "RenderSolid", true);
 
    m_vmultisel.AddElement((ISelect *)this);
    m_undo.m_ptable = this;
