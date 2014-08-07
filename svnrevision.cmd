2e54cd466b93b199fd23b2442aef79203a54430c
toxie <toxie@ainc.de>
2014-08-07 06:53:22 +0000

    remove old build version stuff
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@1165 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- svnrevision.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-echo //%time% %date%>svntemp5674564.txt
-for /F "skip=1 tokens=1*" %%i in (SVNRevision.h) do echo %%i %%j>>svntemp5674564.txt
-type svntemp5674564.txt>SVNRevision.h
-del svntemp5674564.txt
