a6ad4fd3a1ab41e3488ef5d04a2deea23a4e5ec9
toxieainc <toxieainc@14438d79-0ffd-4333-8ae7-ea2bdd901387>
2015-03-04 06:58:24 +0000

    change day->night brightness scaling for backdrop image to better match the rest of the table
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@1799 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- pin3d.cpp
+++ pin3d.cpp
@@ -297,7 +297,7 @@ void Pin3D::DrawBackground()
 
 		g_pplayer->m_pin3d.DisableAlphaBlend();
 
-		g_pplayer->Spritedraw(0.f,0.f,1.f,1.f,0xFFFFFFFF,pin,0.0f,0.0f,1.0f,1.0f,ptable->m_ImageBackdropNightDay ? ptable->m_globalEmissionScale : 1.0f);
+		g_pplayer->Spritedraw(0.f,0.f,1.f,1.f,0xFFFFFFFF,pin,0.0f,0.0f,1.0f,1.0f,ptable->m_ImageBackdropNightDay ? sqrtf(ptable->m_globalEmissionScale) : 1.0f);
 
 		if(g_pplayer->m_ptable->m_tblMirrorEnabled)
 			m_pd3dDevice->SetRenderState(RenderDevice::CULLMODE, D3DCULL_CCW);
