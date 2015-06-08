a8f73afd0ff5c87abca9edc7460fc80c62241973
toxie <toxie@ainc.de>
2015-06-08 06:51:43 +0000

    revert most of rev 2040
    ..leads to problems on ramp collisions (need to track down which parts specifically), for example on the default table
    
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@2042 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- pin/collideex.cpp
+++ pin/collideex.cpp
@@ -500,7 +500,7 @@ float Hit3DPoly::HitTest(const Ball * pball, float dtime, CollisionEvent& coll)
 	float hittime;
 	if (rigid) //rigid polygon
 	{
-		if (bnd < -pball->m_radius*2.0f) return -1.0f;	// (ball normal distance) excessive penetration of object skin ... no collision HACK
+		if (bnd < -pball->m_radius/**2.0f*/) return -1.0f;	// (ball normal distance) excessive penetration of object skin ... no collision HACK //!! *2 necessary?
 			
 		if (bnd <= (float)PHYS_TOUCH)
 		{
@@ -694,7 +694,7 @@ float HitTriangle::HitTest(const Ball * pball, float dtime, CollisionEvent& coll
 
 	float hittime;
 
-    if (bnd < -pball->m_radius*2.0f)
+	if (bnd < -pball->m_radius/**2.0f*/) //!! *2 necessary?
         return -1.0f;	// (ball normal distance) excessive pentratration of object skin ... no collision HACK
 
     bool isContact = false;
