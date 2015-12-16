772e4b55dac72cddfbad38500dbcfc388f91835f
toxie <toxie@ainc.de>
2015-12-16 08:43:22 +0000

    add code to get sunrise/sunset local time
    ..will be used for optional map to night day slider
    
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@2429 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- wintimer.cpp
+++ wintimer.cpp
@@ -1,4 +1,5 @@
 #include "StdAfx.h"
+#include <time.h>
 
 static unsigned int sTimerInit = 0;
 static LARGE_INTEGER TimerFreq;
@@ -49,3 +50,199 @@ void uSleep(const unsigned long long u)
       QueryPerformanceCounter(&TimerNow);
    } while (TimerNow.QuadPart - sTimerStart.QuadPart < TimerEnd.QuadPart);
 }
+
+//
+
+// (Rough) angle of the day (radian)
+double AngleOfDay(const unsigned int day, const unsigned int month, const unsigned int year)
+{
+	static const unsigned int daysPerMonths[11] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 }; // Number of days per month
+	
+	bool leapYear;
+	unsigned int totalDaysInYear;
+    if ((year % 400) == 0)
+    {
+        totalDaysInYear = 366;
+		leapYear = true;
+    }
+    else if ((year % 100) == 0)
+    {
+        totalDaysInYear = 365;
+		leapYear = false;
+    }
+    else if ((year % 4) == 0)
+    {
+        totalDaysInYear = 366;
+		leapYear = true;
+    }
+    else
+    {
+        totalDaysInYear = 365;
+		leapYear = false;
+    }
+
+	unsigned int numOfDays = 0;
+    for (unsigned int i = 1; i < month; i++)
+		numOfDays += daysPerMonths[i-1];
+	if ((month > 2) && leapYear)
+        numOfDays++;
+    numOfDays += day;
+
+    return ((2. * M_PI)*(numOfDays - 1)) / totalDaysInYear;
+}
+
+double SolarDeclination(const double dayAngle) // radian
+{
+	const double c = cos(dayAngle);
+	const double s = sin(dayAngle);
+	return 0.006918
+         - 0.399912 * c
+         + 0.070257 * s
+         - 0.006758 * (2.*c*c - 1.)
+         + 0.000907 * 2.*c*s
+         - 0.002697 * (c*(4.*c*c - 3.))
+         + 0.00148  * (s*(-4.*s*s + 3.));
+}
+
+double EquationOfTimeRadian(const double dayAngle) // radian
+{
+	const double c = cos(dayAngle);
+	const double s = sin(dayAngle);
+	return 0.000075
+         + 0.001868 * c
+         - 0.032077 * s
+		 - 0.014615 * (2.*c*c - 1.)
+		 - 0.04089  * 2.*c*s;
+}
+
+inline double DayDurationHalfRadian(const double declination, const double rlat) // radian, result radian*0.5
+{
+    return acos(-tan(rlat) * tan(declination));
+}
+
+inline double DayDurationHours(const double declination, const double rlat) // radian
+{
+	return DayDurationHalfRadian(declination, rlat) * (24. / M_PI);
+}
+
+// Decimal hour of sunset/sunrise: result in universal time
+double SunsetSunriseUniversalTime(const unsigned int day, const unsigned int month, const unsigned int year, const double rlong, const double rlat, const bool sunrise) // longitude in radians (positive east)
+{
+    const double dayAngle = AngleOfDay(day, month, year);
+    const double ddh = fabs(DayDurationHours(SolarDeclination(dayAngle), rlat));
+
+	return 12. + (sunrise ? -0.5 : 0.5)*ddh - (rlong + EquationOfTimeRadian(dayAngle)) * (12. / M_PI);
+}
+
+double LocalTimeAdjust()
+{
+    time_t hour_machine;
+    time(&hour_machine);
+	struct tm *gmt_hour;
+	gmtime_s(gmt_hour, &hour_machine);
+	struct tm *local_hour;
+	localtime_s(local_hour, &hour_machine);
+
+	const int dif = local_hour->tm_hour - gmt_hour->tm_hour;
+	return (dif < -12) ? dif + 24 : dif;
+}
+
+// Decimal hour of sunset/sunrise: result in local hour
+double SunsetSunriseLocalTime(const unsigned int day, const unsigned int month, const unsigned int year, const double rlong, const double rlat, const bool sunrise) // longitude in radians (positive east)
+{
+	return SunsetSunriseUniversalTime(day, month, year, rlong, rlat, sunrise) + LocalTimeAdjust();
+}
+
+#if 0
+double OrbitalExcentricity(const double dayAngle)
+{
+	const double c = cos(dayAngle);
+	const double s = sin(dayAngle);
+	return 1.000110
+		+ 0.034221 * c
+		+ 0.001280 * s
+		+ 0.000719 * (2.*c*c - 1.)
+		+ 0.000077 * 2.*c*s;
+}
+
+// Theoretical energy flux for the day radiation (radian)
+double TheoreticRadiation(const unsigned int day, const unsigned int month, const unsigned int year, const double rlat)
+{
+	const double dayAngle = AngleOfDay(day, month, year);
+	const double declination = SolarDeclination(dayAngle);
+	const double e0 = OrbitalExcentricity(dayAngle);
+	const double sunriseHourAngle = DayDurationHalfRadian(declination, rlat);
+
+	const double c0 = cos(declination - rlat);
+	const double c1 = cos(declination + rlat);
+	// Theoretical radiation in W.m-2
+	const double solarConst = 1367.; // solar constant W.m-2
+	return 0.5 * solarConst * e0 * ((c0 + c1)*sin(sunriseHourAngle) / sunriseHourAngle + c0 - c1);
+}
+
+// Height of sun in radians
+double SolarHeight(const unsigned int tu, // universal time (0,1,2,.....,23)
+	               const unsigned int day, const unsigned int month, const unsigned int year, const double rlong, const double rlat) // longitude in radian (positive east and negative west)
+{
+    const double dayAngle = AngleOfDay(day, month, year);
+    const double declination = SolarDeclination(dayAngle);
+	const double tsvh = tu * (M_PI / 12.) + rlong * (180. / (15.*12.)) + EquationOfTimeRadian(dayAngle);
+
+	const double c0 = cos(rlat - declination);
+	const double c1 = cos(rlat + declination);
+	const double c2 = cos(tsvh);
+	return asin(0.5*(c0 - c1 - c2*(c0 + c1)));
+}
+
+// http://www.cs.utsa.edu/~cs1063/projects/Spring2011/Project1/jdn-explanation.html
+int JulianConversion(const unsigned int year, const unsigned int month, const unsigned int day)
+{
+    const int a = (14 - month) / 12;
+    const int y = year + 4800 - a;
+    const int m = month + 12 * a - 3;
+    if (year > 1582 || (year == 1582 && month > 10) || (year == 1582 && month == 10 && day >= 15))
+        return day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
+    else
+        return day + (153 * m + 2) / 5 + 365 * y + y / 4 - 32083;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	const unsigned int day = 16;
+	const unsigned int month = 12;
+	const unsigned int year = 2015;
+	const double lat = 52.52;
+	const double lon = 13.37;
+
+    //
+
+	const double rlat = lat * (M_PI / 180.);
+	const double rlong = lon * (M_PI / 180.);
+
+	const double _AngleOfDay = AngleOfDay(day, month, year);
+    std::cout << "Angle of day: " << _AngleOfDay << "\n";
+
+	const double _Declination = SolarDeclination(_AngleOfDay);
+    std::cout << "Declination (Delta): " << _Declination << "\n";
+
+	const double _EquationOfTime = EquationOfTimeRadian(_AngleOfDay) * (12. / M_PI);
+    std::cout << "_EquationOfTime (Delta): " << _EquationOfTime << "\n";
+
+	const double _DayDurationHours = DayDurationHours(_Declination, rlat);
+    std::cout << "Day duration: " << _DayDurationHours << "\n";
+
+	const double _OrbitalExcentricity = OrbitalExcentricity(_AngleOfDay);
+	std::cout << "Excentricity: " << _OrbitalExcentricity << "\n";
+
+	const double _TheoreticRadiation = TheoreticRadiation(day, month, year, rlat);
+    std::cout << "Theoretical radiation: " << _TheoreticRadiation << "\n";
+
+	const double _SunriseLocalTime = SunsetSunriseLocalTime(day, month, year, rlong, rlat, true);
+    std::cout << "Sunrise Local Time: " << _SunriseLocalTime << "\n";
+
+	const double _SunsetLocalTime = SunsetSunriseLocalTime(day, month, year, rlong, rlat, false);
+    std::cout << "Sunset Local Time: " << _SunsetLocalTime << "\n";
+
+    return 0;
+}
+#endif
