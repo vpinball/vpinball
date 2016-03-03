dd9c4831bd397e37e1f9b515cb88a1588a180d16
fuzzelhjb <fuzzelhjb@14438d79-0ffd-4333-8ae7-ea2bdd901387>
2016-03-03 14:12:18 +0000

    hit/drop target: fire hit event after animation is over
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@2524 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- pin/collide.cpp
+++ pin/collide.cpp
@@ -28,8 +28,7 @@ void HitObject::FireHitEvent(Ball* pball)
       // is this the same place as last event? if same then ignore it
       const Vertex3Ds dist = pball->m_Event_Pos - pball->m_pos;
       pball->m_Event_Pos = pball->m_pos;    //remember last collide position
-
-      if (dist.LengthSquared() > 0.25f) // must be a new place if only by a little //!! magic distance
+      if ((m_ObjType!=eHitTarget) && (dist.LengthSquared() > 0.25f)) // must be a new place if only by a little //!! magic distance
          m_pfe->FireGroupEvent(DISPID_HitEvents_Hit);
    }
 }
