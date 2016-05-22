da520b551e2b3d44d18a4bb066fb2433820da665
toxie <toxie@ainc.de>
2016-05-22 12:15:57 +0000

    fix stupid error from last commit
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@2628 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- pin3d.cpp
+++ pin3d.cpp
@@ -262,9 +262,11 @@ HRESULT Pin3D::InitPin3D(const HWND hwnd, const bool fullScreen, const int width
    if(m_pd3dDevice->DepthBufferReadBackAvailable() && (stereo3D || useAO)) {
       m_pdds3DZBuffer = !m_pd3dDevice->m_useNvidiaApi ? (D3DTexture*)m_pd3dDevice->AttachZBufferTo(m_pddsBackBuffer) : m_pd3dDevice->DuplicateDepthTexture((RenderTarget*)m_pddsZBuffer);
 
-      ShowError("Unable to create depth texture!\r\nTry to (un)set \"Alternative Depth Buffer processing\" in the video options!\r\nOr disable Ambient Occlusion and/or 3D stereo!");
       if (!m_pdds3DZBuffer)
+      {
+         ShowError("Unable to create depth texture!\r\nTry to (un)set \"Alternative Depth Buffer processing\" in the video options!\r\nOr disable Ambient Occlusion and/or 3D stereo!");
          return E_FAIL;
+      }
    }
 
    if(m_pd3dDevice->DepthBufferReadBackAvailable() && useAO) 
