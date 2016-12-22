6b5832ca29580fefb887cba793f6e06e284b221e
toxieainc <toxieainc@14438d79-0ffd-4333-8ae7-ea2bdd901387>
2016-12-22 00:25:35 +0000

    update version
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@2882 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- svn_version.h
+++ svn_version.h
@@ -1,6 +1,6 @@
 #ifndef _SVN_VERSION_H_
 #define _SVN_VERSION_H_
 
-#define SVN_REVISION            2765       // Highest committed revision number in the working copy
+#define SVN_REVISION            2882       // Highest committed revision number in the working copy
 
 #endif
