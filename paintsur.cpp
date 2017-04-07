ba2b99e06bb3c69809d19c69daeffbea047cc22f
toxieainc <toxieainc@14438d79-0ffd-4333-8ae7-ea2bdd901387>
2017-04-07 16:43:45 +0000

    use clip regions for new alpha blended images
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@3025 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- paintsur.cpp
+++ paintsur.cpp
@@ -142,26 +142,31 @@ void PaintSur::PolygonImage(const std::vector<RenderVertex> &rgv, HBITMAP hbm, c
    const int ix2 = SCALEXf(right);
    const int iy2 = SCALEYf(bottom);
 
-   HDC hdcNew = CreateCompatibleDC(m_hdc);
-   HBITMAP hbmOld = (HBITMAP)SelectObject(hdcNew, hbm);
+   const HDC hdcNew = CreateCompatibleDC(m_hdc);
+   const HBITMAP hbmOld = (HBITMAP)SelectObject(hdcNew, hbm);
 
-#if 1 // use alpha in bitmap (RGB needs to be premultiplied with alpha, too!)
+   std::vector<POINT> rgpt(rgv.size());
+   for (size_t i = 0; i < rgv.size(); i++)
+   {
+      rgpt[i].x = SCALEXf(rgv[i].x);
+      rgpt[i].y = SCALEYf(rgv[i].y);
+   }
+
+#if 1 // use the alpha in the bitmap (RGB needs to be premultiplied with alpha, too, then! see CopyTo_ConvertAlpha())
+   const HRGN hrgn = CreatePolygonRgn(rgpt.data(), (int)rgv.size(), WINDING);
+   SelectClipRgn(m_hdc, hrgn); 
+
    const BLENDFUNCTION blendf = { AC_SRC_OVER, 0, 255, AC_SRC_ALPHA };
    AlphaBlend(m_hdc, ix, iy, ix2 - ix, iy2 - iy, hdcNew, 0, 0, bitmapwidth, bitmapheight, blendf);
-#else
+
+   SelectClipRgn(m_hdc, NULL);
+   DeleteObject(hrgn);
+#else // do XOR trick for masking (draw image, draw black polygon, draw image again, and the XOR will do an implicit mask op)
    SetStretchBltMode(m_hdc, HALFTONE); // somehow enables filtering
    StretchBlt(m_hdc, ix, iy, ix2 - ix, iy2 - iy, hdcNew, 0, 0, bitmapwidth, bitmapheight, SRCINVERT);
 
    SelectObject(m_hdc, GetStockObject(BLACK_BRUSH));
    SelectObject(m_hdc, GetStockObject(NULL_PEN));
-
-   std::vector<POINT> rgpt(rgv.size());
-
-   for (unsigned i = 0; i < rgv.size(); i++)
-   {
-      rgpt[i].x = SCALEXf(rgv[i].x);
-      rgpt[i].y = SCALEYf(rgv[i].y);
-   }
 
    ::Polygon(m_hdc, rgpt.data(), (int)rgv.size());
 
