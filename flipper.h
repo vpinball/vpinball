d9eda6bb58312148a533f48e8b28112f9564c01f
toxie <toxie@ainc.de>
2017-05-26 12:23:38 +0000

    add the two missing spots where anglespeed is updated with the new speedscale param
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@3087 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- flipper.h
+++ flipper.h
@@ -44,7 +44,7 @@ public:
    float m_rampUp;
    float m_torqueDamping;
    float m_torqueDampingAngle;
-   float m_angleSpeedFactor;
+   float m_angleSpeedFactor; //!! no UI/script only, also not saved
 
    float m_scatter; // scatter angle on hit
    //float m_angleEOS; // angle at which EOS switch opens, as measured from EOS parked position //!! reenable?
@@ -207,8 +207,8 @@ public:
    STDMETHOD(put_EOSTorque)(/*[in]*/ float newVal);
    STDMETHOD(get_EOSTorqueAngle)(/*[out, retval]*/ float *pVal);
    STDMETHOD(put_EOSTorqueAngle)(/*[in]*/ float newVal);
-   STDMETHODIMP get_SpeedFactor(float *pVal);
-   STDMETHODIMP put_SpeedFactor(float newVal);
+   STDMETHOD(get_SpeedFactor)(/*[out, retval]*/ float *pVal);
+   STDMETHOD(put_SpeedFactor)(/*[in]*/ float newVal);
 };
 
 #endif // !defined(AFX_FLIPPER_H__D65AA2A2_9F89_4AA4_88F3_D325B1001825__INCLUDED_)
--- svn_version.h
+++ svn_version.h
@@ -1,6 +1,6 @@
 #ifndef _SVN_VERSION_H_
 #define _SVN_VERSION_H_
 
-#define SVN_REVISION            3082       // Highest committed revision number in the working copy
+#define SVN_REVISION            3087       // Highest committed revision number in the working copy
 
 #endif
