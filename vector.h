b464e3128690023402ce3117af019418212e47fc
fuzzelhjb <fuzzelhjb@14438d79-0ffd-4333-8ae7-ea2bdd901387>
2017-06-26 09:10:29 +0000

    fix memory deallocating and couting variable
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@3112 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- vector.h
+++ vector.h
@@ -316,19 +316,19 @@ public:
 
     ~VectorProtected()
     {
-       unsigned long counter;
+       unsigned long counter=0;
        //try to enter the critical section. If it's used by another thread try again up to 1 second
        while ((TryEnterCriticalSection((LPCRITICAL_SECTION)&hCriticalSection) == 0) && (counter<10))
        {
           Sleep(100);
           counter++;
        }
-       if (counter < 10)
+       if(counter < 10)
        {
-          //critical section is now blocked by us leave and delete is
-          //if counter=10 don't do anything
-          LeaveCriticalSection((LPCRITICAL_SECTION)&hCriticalSection);
-          DeleteCriticalSection((LPCRITICAL_SECTION)&hCriticalSection);
+           //critical section is now blocked by us leave and delete is
+           //if counter=10 don't do anything
+           LeaveCriticalSection((LPCRITICAL_SECTION)&hCriticalSection);
+           DeleteCriticalSection((LPCRITICAL_SECTION)&hCriticalSection);
        }
     }
 
