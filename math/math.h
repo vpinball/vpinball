ad7aee6775501961373f81c10bc16c331f8a8ed2
toxie <toxie@ainc.de>
2017-07-27 18:00:58 +0000

    add some (de)quantization routines
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@3143 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- math/math.h
+++ math/math.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <assert.h>
 #include <math.h>
 #include "vector.h"
 
@@ -10,10 +11,77 @@ __forceinline float exp2f(const float e)
 }
 #endif
 
+
 __forceinline float sqrf(const float x)
 {
    return x*x;
 }
+
+
+__forceinline float precise_divide(const float a, const float b)
+{
+    return _mm_cvtss_f32(_mm_div_ss(_mm_set_ss(a), _mm_set_ss(b)));
+}
+
+
+// from "Scalar quantization to a signed integer" by Rutanen, keeps values under continuous quantize/dequantize cycles constant (and more)
+template <unsigned char bits> // bits to map to, including negative numbers, e.g. mapping just to unsigned: bits+1 (0..255 -> bits = 9)
+__forceinline float dequantizeSigned(const int i)
+{
+    enum { N = (1 << (bits - 1)) - 1 };
+    return clamp(precise_divide((float)i, (float)((double)N + 0.5)), -1.f, 1.f); //!! test: optimize div or does this break precision?
+}
+
+template <unsigned char bits> // bits to map to, including negative numbers, e.g. mapping just to unsigned: bits+1 (0..255 -> bits = 9)
+__forceinline int quantizeSigned(const float x)
+{
+    enum { N = (1 << (bits - 1)) - 1 };
+    const float sign = (x >= 0.f) ? 0.5f : -0.5f;
+    return (int)(clamp(x * (float)((double)N + 0.5) + sign, -(float)N, (float)N));
+}
+
+template <unsigned char bits> // bits to map to
+__forceinline float dequantizeUnsigned(const unsigned int i)
+{
+    enum { N = (1 << bits) - 1 };
+    return min(precise_divide((float)i, (float)((double)N + 0.5)), 1.f); //!! test: optimize div or does this break precision?
+}
+
+template <unsigned char bits> // bits to map to
+__forceinline unsigned int quantizeUnsigned(const float x)
+{
+    enum { N = (1 << bits) - 1 };
+    assert(x >= 0.f);
+    return (unsigned int)(min(x * (float)((double)N + 0.5) + 0.5f, (float)N));
+}
+
+
+__forceinline float dequantizeSignedPercent(const int i)
+{
+    enum { N = 100 };
+    return clamp(precise_divide((float)i, (float)((double)N + 0.5)), -1.f, 1.f); //!! test: optimize div or does this break precision?
+}
+
+__forceinline int quantizeSignedPercent(const float x)
+{
+    enum { N = 100 };
+    const float sign = (x >= 0.f) ? 0.5f : -0.5f;
+    return (int)(clamp(x * (float)((double)N + 0.5) + sign, -(float)N, (float)N));
+}
+
+__forceinline float dequantizeUnsignedPercent(const unsigned int i)
+{
+    enum { N = 100 };
+    return min(precise_divide((float)i, (float)((double)N + 0.5)), 1.f); //!! test: optimize div or does this break precision?
+}
+
+__forceinline unsigned int quantizeUnsignedPercent(const float x)
+{
+    enum { N = 100 };
+    assert(x >= 0.f);
+    return (unsigned int)(min(x * (float)((double)N + 0.5) + 0.5f, (float)N));
+}
+
 
 // Math stuff which doesn't fit elsewhere
 
