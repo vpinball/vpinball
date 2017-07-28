31f0324f16e1b44a822646cde7dfe16aea8ead18
toxie <toxie@ainc.de>
2017-07-28 16:11:29 +0000

    change profiling code from D3D11 to D3D9 and improve precision
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@3146 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- inc/gpuprofiler.cpp
+++ inc/gpuprofiler.cpp
@@ -1,22 +1,20 @@
-#include "gpuprofiler.hpp"
-#include <d3d11.h>
+#include "stdafx.h"
+#include "gpuprofiler.h"
+#include <d3d9.h>
 
-extern ID3D11Device * g_pDevice;
-extern ID3D11DeviceContext * g_pContext;
-void ErrorPrintf (const char * message, ...);
-void DebugPrintf (const char * message, ...);
-float Time ();		// Retrieve time in seconds, using QueryPerformanceCounter or whatever
+#define FLUSH_DATA 0 //D3DGETDATA_FLUSH
 
+#define ErrorPrintf(x, ...) { char sz[256]; sprintf_s(sz,x,__VA_ARGS__); ShowError(sz); }
+#define DebugPrintf(x, ...) { char sz[256]; sprintf_s(sz,x,__VA_ARGS__); ShowError(sz); }
 
-// CGpuProfiler implementation
-
-CGpuProfiler g_gpuProfiler;
 
 CGpuProfiler::CGpuProfiler ()
 :	m_iFrameQuery(0),
 	m_iFrameCollect(-1),
 	m_frameCountAvg(0),
-	m_tBeginAvg(0.0f)
+	m_tBeginAvg(0.0),
+	m_frequencyQuery(NULL),
+	m_device(NULL)
 {
 	memset(m_apQueryTsDisjoint, 0, sizeof(m_apQueryTsDisjoint));
 	memset(m_apQueryTs, 0, sizeof(m_apQueryTs));
@@ -25,40 +23,53 @@ CGpuProfiler::CGpuProfiler ()
 	memset(m_adTTotalAvg, 0, sizeof(m_adT));
 }
 
-bool CGpuProfiler::Init ()
+bool CGpuProfiler::Init (IDirect3DDevice9 * const pDevice)
 {
+	const HRESULT tsHr  = pDevice->CreateQuery(D3DQUERYTYPE_TIMESTAMP, NULL);
+	const HRESULT tsdHr = pDevice->CreateQuery(D3DQUERYTYPE_TIMESTAMPDISJOINT, NULL);
+	const HRESULT tsfHr = pDevice->CreateQuery(D3DQUERYTYPE_TIMESTAMPFREQ, NULL);
+
+	if (tsHr || tsdHr || tsfHr)
+	{
+		ErrorPrintf("GPU Profiler: Query not supported");
+		return false;
+	}
+
 	// Create all the queries we'll need
-
-	D3D11_QUERY_DESC queryDesc = { D3D11_QUERY_TIMESTAMP_DISJOINT, 0 };
-
-	if (FAILED(g_pDevice->CreateQuery(&queryDesc, &m_apQueryTsDisjoint[0])))
+	if (FAILED(pDevice->CreateQuery(D3DQUERYTYPE_TIMESTAMPDISJOINT, &m_apQueryTsDisjoint[0])))
 	{
-		ErrorPrintf("Could not create timestamp disjoint query for frame 0!");
+		ErrorPrintf("GPU Profiler: Could not create timestamp disjoint query for frame 0!");
 		return false;
 	}
 
-	if (FAILED(g_pDevice->CreateQuery(&queryDesc, &m_apQueryTsDisjoint[1])))
+	if (FAILED(pDevice->CreateQuery(D3DQUERYTYPE_TIMESTAMPDISJOINT, &m_apQueryTsDisjoint[1])))
 	{
-		ErrorPrintf("Could not create timestamp disjoint query for frame 1!");
+		ErrorPrintf("GPU Profiler: Could not create timestamp disjoint query for frame 1!");
 		return false;
 	}
-
-	queryDesc.Query = D3D11_QUERY_TIMESTAMP;
 
 	for (GTS gts = GTS_BeginFrame; gts < GTS_Max; gts = GTS(gts + 1))
 	{
-		if (FAILED(g_pDevice->CreateQuery(&queryDesc, &m_apQueryTs[gts][0])))
+		if (FAILED(pDevice->CreateQuery(D3DQUERYTYPE_TIMESTAMP, &m_apQueryTs[gts][0])))
 		{
-			ErrorPrintf("Could not create start-frame timestamp query for GTS %d, frame 0!", gts);
+			ErrorPrintf("GPU Profiler: Could not create start-frame timestamp query for GTS %d, frame 0!", gts);
 			return false;
 		}
 
-		if (FAILED(g_pDevice->CreateQuery(&queryDesc, &m_apQueryTs[gts][1])))
+		if (FAILED(pDevice->CreateQuery(D3DQUERYTYPE_TIMESTAMP, &m_apQueryTs[gts][1])))
 		{
-			ErrorPrintf("Could not create start-frame timestamp query for GTS %d, frame 1!", gts);
+			ErrorPrintf("GPU Profiler: Could not create start-frame timestamp query for GTS %d, frame 1!", gts);
 			return false;
 		}
 	}
+
+	if(FAILED(pDevice->CreateQuery(D3DQUERYTYPE_TIMESTAMPFREQ, &m_frequencyQuery)))
+	{
+		ErrorPrintf("GPU Profiler: Could not create frequency query!");
+		return false;
+	}
+
+	m_device = pDevice;
 
 	return true;
 }
@@ -79,29 +90,45 @@ void CGpuProfiler::Shutdown ()
 		if (m_apQueryTs[gts][1])
 			m_apQueryTs[gts][1]->Release();
 	}
+
+	if (m_frequencyQuery)
+		m_frequencyQuery->Release();
 }
 
 void CGpuProfiler::BeginFrame ()
 {
-	g_pContext->Begin(m_apQueryTsDisjoint[m_iFrameQuery]);
+	if (!m_device)
+		return;
+
+	m_frequencyQuery->Issue(D3DISSUE_END);
+	m_apQueryTsDisjoint[m_iFrameQuery]->Issue(D3DISSUE_END);
 	Timestamp(GTS_BeginFrame);
 }
 
 void CGpuProfiler::Timestamp (GTS gts)
 {
-	g_pContext->End(m_apQueryTs[gts][m_iFrameQuery]);
+	if (!m_device)
+		return;
+
+	m_apQueryTs[gts][m_iFrameQuery]->Issue(D3DISSUE_END);
 }
 
 void CGpuProfiler::EndFrame ()
 {
-	Timestamp(GTS_EndFrame);
-	g_pContext->End(m_apQueryTsDisjoint[m_iFrameQuery]);
+	if (!m_device)
+		return;
 
-	++m_iFrameQuery &= 1;
+	Timestamp(GTS_EndFrame);
+	m_apQueryTsDisjoint[m_iFrameQuery]->Issue(D3DISSUE_END);
+
+	m_iFrameQuery = (m_iFrameQuery+1)&1;
 }
 
 void CGpuProfiler::WaitForDataAndUpdate ()
 {
+	if (!m_device)
+		return;
+
 	if (m_iFrameCollect < 0)
 	{
 		// Haven't run enough frames yet to have data
@@ -110,60 +137,80 @@ void CGpuProfiler::WaitForDataAndUpdate ()
 	}
 
 	// Wait for data
-	while (g_pContext->GetData(m_apQueryTsDisjoint[m_iFrameCollect], NULL, 0, 0) == S_FALSE)
+	UINT32 c = 0;
+	BOOL disjoint;
+	while (m_apQueryTsDisjoint[m_iFrameCollect]->GetData(&disjoint,sizeof(BOOL), FLUSH_DATA) == S_FALSE && c < 10)
 	{
-		Sleep(1);
+		c++;
+		Sleep(1); //!!
+	}
+	if(c >= 10)
+	{
+		DebugPrintf("GPU Profiler: Failed while waiting for data");
+		return;
+	}
+
+	if(disjoint)
+	{
+		DebugPrintf("GPU Profiler: Timing interval disjoint");
+		return;
 	}
 
 	int iFrame = m_iFrameCollect;
 	++m_iFrameCollect &= 1;
 
-	D3D11_QUERY_DATA_TIMESTAMP_DISJOINT timestampDisjoint;
-	if (g_pContext->GetData(m_apQueryTsDisjoint[iFrame], &timestampDisjoint, sizeof(timestampDisjoint), 0) != S_OK)
+	UINT64 frequency;
+	if (m_frequencyQuery->GetData(&frequency,sizeof(UINT64),FLUSH_DATA) != S_OK)
 	{
-		DebugPrintf("Couldn't retrieve timestamp disjoint query data");
-		return;
-	}
-
-	if (timestampDisjoint.Disjoint)
-	{
-		// Throw out this frame's data
-		DebugPrintf("Timestamps disjoint");
+		DebugPrintf("GPU Profiler: Couldn't retrieve frequency data");
 		return;
 	}
 
 	UINT64 timestampPrev;
-	if (g_pContext->GetData(m_apQueryTs[GTS_BeginFrame][iFrame], &timestampPrev, sizeof(UINT64), 0) != S_OK)
+	if (m_apQueryTs[GTS_BeginFrame][iFrame]->GetData(&timestampPrev, sizeof(UINT64), FLUSH_DATA) != S_OK)
 	{
-		DebugPrintf("Couldn't retrieve timestamp query data for GTS %d", GTS_BeginFrame);
+		DebugPrintf("GPU Profiler: Couldn't retrieve timestamp query data for GTS %d", GTS_BeginFrame);
 		return;
 	}
 
 	for (GTS gts = GTS(GTS_BeginFrame + 1); gts < GTS_Max; gts = GTS(gts + 1))
 	{
 		UINT64 timestamp;
-		if (g_pContext->GetData(m_apQueryTs[gts][iFrame], &timestamp, sizeof(UINT64), 0) != S_OK)
+		/*if (m_apQueryTs[gts][iFrame]->GetData(&timestamp, sizeof(UINT64), 0) != S_OK)
 		{
-			DebugPrintf("Couldn't retrieve timestamp query data for GTS %d", gts);
+			DebugPrintf("GPU Profiler: Couldn't retrieve timestamp query data for GTS %d", gts);
+			return;
+		}*/
+		c = 0;
+		while (m_apQueryTs[gts][iFrame]->GetData(&timestamp, sizeof(UINT64), FLUSH_DATA) != S_OK && c < 10)
+		{
+			c++;
+			Sleep(1); //!!
+		}
+		if(c >= 10)
+		{
+			DebugPrintf("GPU Profiler: Couldn't retrieve timestamp query data for GTS %d", gts);
 			return;
 		}
 
-		m_adT[gts] = float(timestamp - timestampPrev) / float(timestampDisjoint.Frequency);
+		m_adT[gts] = (double)(timestamp - timestampPrev) / (double)frequency;
 		timestampPrev = timestamp;
 
 		m_adTTotalAvg[gts] += m_adT[gts];
 	}
 
 	++m_frameCountAvg;
-	if (Time() > m_tBeginAvg + 0.5f)
+	const unsigned long long us = usec();
+	const double t = (double)us*1e-6;
+	if (t > m_tBeginAvg + 0.5)
 	{
 		for (GTS gts = GTS_BeginFrame; gts < GTS_Max; gts = GTS(gts + 1))
 		{
-			m_adTAvg[gts] = m_adTTotalAvg[gts] / m_frameCountAvg;
-			m_adTTotalAvg[gts] = 0.0f;
+			m_adTAvg[gts] = m_adTTotalAvg[gts] / (double)m_frameCountAvg;
+			m_adTTotalAvg[gts] = 0.0;
 		}
 
 		m_frameCountAvg = 0;
-		m_tBeginAvg = Time();
+		m_tBeginAvg = t;
 	}
 }
--- inc/gpuprofiler.h
+++ inc/gpuprofiler.h
@@ -1,19 +1,45 @@
 // GPU performance-measurement subsystem
 
+// Original license for the D3D11 codebase coming up, code is modified though by VP team to work with D3D9 (and more)
+
+/*
+Copyright(c) 2011 by Nathan Reed.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met :
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and / or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER "AS IS" AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.IN NO
+EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #pragma once
-
-struct ID3D11DeviceContext;
-struct ID3D11Query;
-
 
 // Enum of GPU timestamps to record
 enum GTS
 {
 	GTS_BeginFrame,
-	GTS_ShadowClear,
-	GTS_ShadowObjects,
-	GTS_MainClear,
-	GTS_MainObjects,
+	GTS_PlayfieldGraphics,
+	GTS_NonTransparent,
+	GTS_LightBuffer,
+	GTS_Transparent,
+	GTS_AO,
+	GTS_PostProcess,
 	GTS_EndFrame,
 
 	GTS_Max
@@ -24,33 +50,32 @@ class CGpuProfiler
 public:
 	CGpuProfiler ();
 
-	bool Init ();
+	bool Init (IDirect3DDevice9 * const pDevice);
 	void Shutdown ();
 
 	void BeginFrame ();
-	void Timestamp (GTS gts);
+	void Timestamp (const GTS gts);
 	void EndFrame ();
 
 	// Wait on GPU for last frame's data (not this frame's) to be available
 	void WaitForDataAndUpdate ();
 
-	float Dt (GTS gts)
-		{ return m_adT[gts]; }
-	float DtAvg (GTS gts)
-		{ return m_adTAvg[gts]; }
+	double Dt (const GTS gts)    { return m_adT[gts]; }
+	double DtAvg(const GTS gts)  { return m_adTAvg[gts]; }
 
 protected:
 	int m_iFrameQuery;							// Which of the two sets of queries are we currently issuing?
 	int m_iFrameCollect;						// Which of the two did we last collect?
-	ID3D11Query * m_apQueryTsDisjoint[2];		// "Timestamp disjoint" query; records whether timestamps are valid
-	ID3D11Query * m_apQueryTs[GTS_Max][2];		// Individual timestamp queries for each relevant point in the frame
+	IDirect3DQuery9 * m_apQueryTsDisjoint[2];	// "Timestamp disjoint" query; records whether timestamps are valid
+	IDirect3DQuery9 * m_apQueryTs[GTS_Max][2];	// Individual timestamp queries for each relevant point in the frame
+	IDirect3DQuery9 * m_frequencyQuery;
 
-	float m_adT[GTS_Max];						// Last frame's timings (each relative to previous GTS)
-	float m_adTAvg[GTS_Max];					// Timings averaged over 0.5 second
+	double m_adT[GTS_Max];						// Last frame's timings (each relative to previous GTS)
+	double m_adTAvg[GTS_Max];					// Timings averaged over 0.5 second
 
-	float m_adTTotalAvg[GTS_Max];				// Total timings thus far within this averaging period
+	double m_adTTotalAvg[GTS_Max];				// Total timings thus far within this averaging period
 	int m_frameCountAvg;						// Frames rendered in current averaging period
-	float m_tBeginAvg;							// Time at which current averaging period started
-};
+	double m_tBeginAvg;							// Time at which current averaging period started
 
-extern CGpuProfiler g_gpuProfiler;
+	IDirect3DDevice9 * m_device;
+};
