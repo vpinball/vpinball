1b05e39f014396b0c5c76c0f5fc375e99a24c025
toxie <toxie@ainc.de>
2018-11-20 22:57:04 +0000

    add success message when pressing compile and there are no errors
    ..also remove code completely for undef'd slintf's
    
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@3561 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- worker.h
+++ worker.h
@@ -1,11 +1,10 @@
 #pragma once
+
 #define COMPLETE_AUTOSAVE		WM_USER+100
-#define COMPLETE_SAVE			WM_USER+101
 #define HANG_SNOOP_START		WM_USER+102
 #define HANG_SNOOP_STOP			WM_USER+103
 
 #define DONE_AUTOSAVE			WM_USER+200
-#define DONE_SAVE				WM_USER+201
 
 extern HANDLE g_hWorkerStarted;
 
