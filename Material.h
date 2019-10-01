37446272451c90c5db23e2e9d395a10e4dedb3fd
toxie <toxie@ainc.de>
2019-10-01 19:59:00 +0000

    replace old .bmp saving code with more general freeimage code, mainly interesting at the moment only for the blueprint export
    ..also replace save-dialog-flags for the other export dialogs
    
    ..also use m_pt instead of g_pplayer->m_ptable in ScriptGlobalTable
    
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@3777 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- Material.h
+++ Material.h
@@ -30,28 +30,71 @@ class Material
 {
 public:
 
-   inline Material()
+   inline Material() :
+         m_fWrapLighting(0.0f)
+       , m_fRoughness(0.0f)
+       , m_fGlossyImageLerp(1.0f)
+       , m_fThickness(0.05f)
+       , m_fEdge(1.0f)
+       , m_fEdgeAlpha(1.0f)
+       , m_fOpacity(1.0f)
+       , m_cBase(0xB469FF)
+       , m_cGlossy(0)
+       , m_cClearcoat(0)
+       , m_bIsMetal(false)
+       , m_bOpacityActive(false)
+       , m_fElasticity(0.0f)
+       , m_fElasticityFalloff(0.0f)
+       , m_fFriction(0.0f)
+       , m_fScatterAngle(0.0f)
    {
-      m_fWrapLighting = 0.0f;
-      m_fRoughness = 0.0f;
-      m_fGlossyImageLerp = 1.0f;
-      m_fThickness = 0.05f;
-      m_fEdge = 1.0f;
-      m_fEdgeAlpha = 1.0f;
-      m_fOpacity = 1.0f;
-      m_cBase = 0xB469FF;
-      m_cGlossy = 0;
-      m_cClearcoat = 0;
-      m_bIsMetal = false;
-      m_bOpacityActive = false;
-
-      m_fElasticity = 0.0f;
-      m_fElasticityFalloff = 0.0f;
-      m_fFriction = 0.0f;
-      m_fScatterAngle = 0.0f;
-
       memset(m_szName, 0, 32);
-      strcat_s(m_szName, "dummyMaterial");
+      strcpy_s(m_szName, "dummyMaterial");
+   }
+
+   inline Material(float wrapLighting, float roughness, float glossyImageLerp, float thickness, float edge, float edgeAlpha, float opacity,
+       COLORREF base, COLORREF glossy, COLORREF clearcoat, bool isMetal, bool opacityActive,
+       float elasticity, float elasticityFalloff, float friction, float scatterAngle) :
+         m_fWrapLighting(wrapLighting)
+       , m_fRoughness(roughness)
+       , m_fGlossyImageLerp(glossyImageLerp)
+       , m_fThickness(thickness)
+       , m_fEdge(edge)
+       , m_fEdgeAlpha(edgeAlpha)
+       , m_fOpacity(opacity)
+       , m_cBase(base)
+       , m_cGlossy(glossy)
+       , m_cClearcoat(clearcoat)
+       , m_bIsMetal(isMetal)
+       , m_bOpacityActive(opacityActive)
+       , m_fElasticity(elasticity)
+       , m_fElasticityFalloff(elasticityFalloff)
+       , m_fFriction(friction)
+       , m_fScatterAngle(scatterAngle)
+   {
+      memset(m_szName, 0, 32);
+      strcpy_s(m_szName, "dummyMaterial");
+   }
+
+   inline Material(const Material * const pmat) :
+         m_fWrapLighting(pmat->m_fWrapLighting)
+       , m_fRoughness(pmat->m_fRoughness)
+       , m_fGlossyImageLerp(pmat->m_fGlossyImageLerp)
+       , m_fThickness(pmat->m_fThickness)
+       , m_fEdge(pmat->m_fEdge)
+       , m_fEdgeAlpha(pmat->m_fEdgeAlpha)
+       , m_fOpacity(pmat->m_fOpacity)
+       , m_cBase(pmat->m_cBase)
+       , m_cGlossy(pmat->m_cGlossy)
+       , m_cClearcoat(pmat->m_cClearcoat)
+       , m_bIsMetal(pmat->m_bIsMetal)
+       , m_bOpacityActive(pmat->m_bOpacityActive)
+       , m_fElasticity(pmat->m_fElasticity)
+       , m_fElasticityFalloff(pmat->m_fElasticityFalloff)
+       , m_fFriction(pmat->m_fFriction)
+       , m_fScatterAngle(pmat->m_fScatterAngle)
+   {
+      memcpy(m_szName, pmat->m_szName, 32);
    }
 
    inline unsigned long long hash() const
