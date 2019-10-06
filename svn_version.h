4b7a8a5190e917b2025baa07244023d3cab98031
toxie <toxie@ainc.de>
2019-10-06 12:08:24 +0000

    enter new release cycle
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@3790 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- svn_version.h
+++ svn_version.h
@@ -1,6 +1,6 @@
 #ifndef _SVN_VERSION_H_
 #define _SVN_VERSION_H_
 
-#define SVN_REVISION            3735       // Highest committed revision number in the working copy
+#define SVN_REVISION            3784       // Highest committed revision number in the working copy
 
 #endif
