e05e515d65f237dc57ae1cdf15c7f1fe7ad9b230
fuzzelhjb <fuzzelhjb@14438d79-0ffd-4333-8ae7-ea2bdd901387>
2019-10-20 14:07:58 +0000

    new toolbar update
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@3816 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- dialogs/ToolbarDialog.cpp
+++ dialogs/ToolbarDialog.cpp
@@ -19,125 +19,163 @@ BOOL ToolbarDialog::OnInitDialog()
     m_hwnd = GetDlgItem(IDD_TOOLBAR).GetHwnd();
 
     SendDlgItemMessage(IDC_SELECT, BM_SETCHECK, BST_CHECKED, 0);
-    
+
+    AttachItem(ID_TABLE_MAGNIFY, m_magnifyButton);
+    AttachItem(IDC_SELECT, m_selectButton);
+    AttachItem(ID_EDIT_PROPERTIES, m_optionsButton);
+    AttachItem(ID_EDIT_SCRIPT, m_scriptButton);
+    AttachItem(ID_EDIT_BACKGLASSVIEW, m_backglassButton);
+    AttachItem(ID_TABLE_PLAY, m_playButton);
+    AttachItem(ID_LAYER_LAYER1, m_layerButtons[0]);
+    AttachItem(ID_LAYER_LAYER2, m_layerButtons[1]);
+    AttachItem(ID_LAYER_LAYER3, m_layerButtons[2]);
+    AttachItem(ID_LAYER_LAYER4, m_layerButtons[3]);
+    AttachItem(ID_LAYER_LAYER5, m_layerButtons[4]);
+    AttachItem(ID_LAYER_LAYER6, m_layerButtons[5]);
+    AttachItem(ID_LAYER_LAYER7, m_layerButtons[6]);
+    AttachItem(ID_LAYER_LAYER8, m_layerButtons[7]);
+    AttachItem(ID_LAYER_LAYER9, m_layerButtons[8]);
+    AttachItem(ID_LAYER_LAYER10, m_layerButtons[9]);
+    AttachItem(ID_LAYER_LAYER11, m_layerButtons[10]);
+    AttachItem(ID_LAYER_TOGGLEALL, m_layerButtons[11]);
+    AttachItem(ID_INSERT_WALL, m_wallButton);
+    AttachItem(ID_INSERT_GATE, m_gateButton);
+    AttachItem(ID_INSERT_RAMP, m_rampButton);
+    AttachItem(ID_INSERT_FLIPPER, m_flipperButton);
+    AttachItem(ID_INSERT_PLUNGER, m_plungerButton);
+    AttachItem(ID_INSERT_BUMPER, m_bumperButton);
+    AttachItem(ID_INSERT_SPINNER, m_spinnerButton);
+    AttachItem(ID_INSERT_TIMER, m_timerButton);
+    AttachItem(ID_INSERT_TRIGGER, m_triggerButton);
+    AttachItem(ID_INSERT_LIGHT, m_lightButton);
+    AttachItem(ID_INSERT_KICKER, m_kickerButton);
+    AttachItem(ID_INSERT_TARGET, m_targetButton);
+    AttachItem(ID_INSERT_DECAL, m_decalButton);
+    AttachItem(ID_INSERT_TEXTBOX, m_textboxButton);
+    AttachItem(ID_INSERT_DISPREEL, m_reelButton);
+    AttachItem(ID_INSERT_LIGHTSEQ, m_lightseqButton);
+    AttachItem(ID_INSERT_PRIMITIVE, m_primitiveButton);
+    AttachItem(ID_INSERT_FLASHER, m_flasherButton);
+    AttachItem(ID_INSERT_RUBBER, m_rubberButton);
+
     const int iconSize = 24;
     HANDLE hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MAGNIFY), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_TABLE_MAGNIFY), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_magnifyButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_SELECT), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(IDC_SELECT), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_selectButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_OPTIONS), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_EDIT_PROPERTIES), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_optionsButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_SCRIPT), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_EDIT_SCRIPT), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_scriptButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_BACKGLASS), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_EDIT_BACKGLASSVIEW), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_backglassButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_PLAY), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_TABLE_PLAY), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_playButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_WALL), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_WALL), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_wallButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_GATE), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_GATE), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_gateButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_RAMP), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_RAMP), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_rampButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_FLIPPER), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_FLIPPER), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_flipperButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_PLUNGER), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_PLUNGER), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_plungerButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_BUMPER), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_BUMPER), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_bumperButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_SPINNER), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_SPINNER), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_spinnerButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_TIMER), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_TIMER), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_timerButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_TRIGGER), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_TRIGGER), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_triggerButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_LIGHT), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_LIGHT), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_lightButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_KICKER), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_KICKER), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_kickerButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_TARGET), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_TARGET), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_targetButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_DECAL), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_DECAL), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_decalButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_TEXTBOX), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_TEXTBOX), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_textboxButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_REEL), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_DISPREEL), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_reelButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_LIGHTSEQ), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_LIGHTSEQ), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_lightseqButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_PRIMITIVE), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_PRIMITIVE), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_primitiveButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_FLASHER), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_FLASHER), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_flasherButton.SetIcon((HICON)hIcon);
 
     hIcon = ::LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_RUBBER), IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR);
-    ::SendMessage(GetDlgItem(ID_INSERT_RUBBER), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hIcon);
+    m_rubberButton.SetIcon((HICON)hIcon);
 
     m_resizer.Initialize(*this, CRect(0, 0, 61, 422));
 
-    m_resizer.AddChild(GetDlgItem(ID_TABLE_MAGNIFY), topleft, 0);
-    m_resizer.AddChild(GetDlgItem(IDC_SELECT), topright, 0);
-    m_resizer.AddChild(GetDlgItem(ID_EDIT_PROPERTIES), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_EDIT_SCRIPT), rightcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_EDIT_BACKGLASSVIEW), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_TABLE_PLAY), rightcenter, 0);
+    m_resizer.AddChild(m_magnifyButton, topleft, 0);
+    m_resizer.AddChild(m_selectButton, topright, 0);
+    m_resizer.AddChild(m_optionsButton, leftcenter, 0);
+    m_resizer.AddChild(m_scriptButton, rightcenter, 0);
+    m_resizer.AddChild(m_backglassButton, leftcenter, 0);
+    m_resizer.AddChild(m_playButton, rightcenter, 0);
     m_resizer.AddChild(GetDlgItem(IDC_STATIC_LAYERS), topleft, RD_STRETCH_WIDTH|RD_STRETCH_HEIGHT);
-    m_resizer.AddChild(GetDlgItem(ID_LAYER_LAYER1), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_LAYER_LAYER2), center, 0);
-    m_resizer.AddChild(GetDlgItem(ID_LAYER_LAYER3), rightcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_LAYER_LAYER4), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_LAYER_LAYER5), center, 0);
-    m_resizer.AddChild(GetDlgItem(ID_LAYER_LAYER6), rightcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_LAYER_LAYER7), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_LAYER_LAYER8), center, 0);
-    m_resizer.AddChild(GetDlgItem(ID_LAYER_LAYER9), rightcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_LAYER_LAYER10), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_LAYER_LAYER11), center, 0);
-    m_resizer.AddChild(GetDlgItem(ID_LAYER_TOGGLEALL), rightcenter, 0);
+    m_resizer.AddChild(m_layerButtons[0], leftcenter, 0);
+    m_resizer.AddChild(m_layerButtons[1], center, 0);
+    m_resizer.AddChild(m_layerButtons[2], rightcenter, 0);
+    m_resizer.AddChild(m_layerButtons[3], leftcenter, 0);
+    m_resizer.AddChild(m_layerButtons[4], center, 0);
+    m_resizer.AddChild(m_layerButtons[5], rightcenter, 0);
+    m_resizer.AddChild(m_layerButtons[6], leftcenter, 0);
+    m_resizer.AddChild(m_layerButtons[7], center, 0);
+    m_resizer.AddChild(m_layerButtons[8], rightcenter, 0);
+    m_resizer.AddChild(m_layerButtons[9], leftcenter, 0);
+    m_resizer.AddChild(m_layerButtons[10], center, 0);
+    m_resizer.AddChild(m_layerButtons[11], rightcenter, 0);
     m_resizer.AddChild(GetDlgItem(IDC_STATIC_ELEMENTS), bottomright, RD_STRETCH_WIDTH | RD_STRETCH_HEIGHT);
 
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_WALL), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_GATE), rightcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_RAMP), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_FLIPPER), rightcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_PLUNGER), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_BUMPER), rightcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_SPINNER), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_TIMER), rightcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_TRIGGER), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_LIGHT), rightcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_KICKER), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_TARGET), rightcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_DECAL), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_TEXTBOX), rightcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_DISPREEL), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_LIGHTSEQ), rightcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_PRIMITIVE), leftcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_FLASHER), rightcenter, 0);
-    m_resizer.AddChild(GetDlgItem(ID_INSERT_RUBBER), leftcenter, 0);
+    m_resizer.AddChild(m_wallButton, leftcenter, 0);
+    m_resizer.AddChild(m_gateButton, rightcenter, 0);
+    m_resizer.AddChild(m_rampButton, leftcenter, 0);
+    m_resizer.AddChild(m_flipperButton, rightcenter, 0);
+    m_resizer.AddChild(m_plungerButton, leftcenter, 0);
+    m_resizer.AddChild(m_bumperButton, rightcenter, 0);
+    m_resizer.AddChild(m_spinnerButton, leftcenter, 0);
+    m_resizer.AddChild(m_timerButton, rightcenter, 0);
+    m_resizer.AddChild(m_triggerButton, leftcenter, 0);
+    m_resizer.AddChild(m_lightButton, rightcenter, 0);
+    m_resizer.AddChild(m_kickerButton, leftcenter, 0);
+    m_resizer.AddChild(m_targetButton, rightcenter, 0);
+    m_resizer.AddChild(m_decalButton, leftcenter, 0);
+    m_resizer.AddChild(m_textboxButton, rightcenter, 0);
+    m_resizer.AddChild(m_reelButton, leftcenter, 0);
+    m_resizer.AddChild(m_lightseqButton, rightcenter, 0);
+    m_resizer.AddChild(m_primitiveButton, leftcenter, 0);
+    m_resizer.AddChild(m_flasherButton, rightcenter, 0);
+    m_resizer.AddChild(m_rubberButton, leftcenter, 0);
     m_resizer.RecalcLayout();
     return TRUE;
 }
--- dialogs/ToolbarDialog.h
+++ dialogs/ToolbarDialog.h
@@ -13,8 +13,36 @@ protected:
     virtual INT_PTR DialogProc(UINT msg, WPARAM wparam, LPARAM lparam);
     virtual BOOL OnCommand(WPARAM wParam, LPARAM lParam);
 
+private:
+
     HWND m_hwnd;
     CResizer m_resizer;
+    CButton m_magnifyButton;
+    CButton m_selectButton;
+    CButton m_optionsButton;
+    CButton m_scriptButton;
+    CButton m_backglassButton;
+    CButton m_playButton;
+    CButton m_layerButtons[12];
+    CButton m_wallButton;
+    CButton m_gateButton;
+    CButton m_rampButton;
+    CButton m_flipperButton;
+    CButton m_plungerButton;
+    CButton m_bumperButton;
+    CButton m_spinnerButton;
+    CButton m_timerButton;
+    CButton m_triggerButton;
+    CButton m_lightButton;
+    CButton m_kickerButton;
+    CButton m_targetButton;
+    CButton m_decalButton;
+    CButton m_textboxButton;
+    CButton m_reelButton;
+    CButton m_lightseqButton;
+    CButton m_primitiveButton;
+    CButton m_flasherButton;
+    CButton m_rubberButton;
 };
 
 #endif H_TOOLBAR_DIALOG
