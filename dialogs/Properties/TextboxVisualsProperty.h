124dc8fe046c25f839a44b694f602192655f8f33
fuzzelhjb <fuzzelhjb@14438d79-0ffd-4333-8ae7-ea2bdd901387>
2019-11-11 15:08:13 +0000

    add new textbox visuals properties
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@3861 14438d79-0ffd-4333-8ae7-ea2bdd901387

---
+++ dialogs/Properties/TextboxVisualsProperty.h
@@ -0,0 +1,35 @@
+#ifndef H_TEXTBOX_VISUALS_PROPERTY
+#define H_TEXTBOX_VISUALS_PROPERTY
+
+class TextboxVisualsProperty : public BasePropertyDialog
+{
+public:
+    TextboxVisualsProperty(VectorProtected<ISelect> *pvsel);
+    virtual ~TextboxVisualsProperty();
+
+    void UpdateVisuals();
+    void UpdateProperties(const int dispid);
+
+protected:
+    virtual BOOL OnInitDialog();
+    virtual INT_PTR DialogProc(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+private:
+    HWND        m_hTransparentCheck;
+    CComboBox   m_alignmentCombo;
+    CButton     m_fontDialogButton;
+    CFontDialog m_fontDialog;
+    ColorButton  m_backgroundColorButton;
+    ColorButton  m_textColorButton;
+    CColorDialog m_colorDialog;
+    CEdit        m_textIntensityEdit;
+    CEdit        m_posXEdit;
+    CEdit        m_posYEdit;
+    CEdit        m_widthEdit;
+    CEdit        m_heightEdit;
+    CFont       *m_font;
+    vector<string> m_alignList;
+
+};
+
+#endif
