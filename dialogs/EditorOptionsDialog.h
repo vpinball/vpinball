37314ff5b2307d3c60b937afc5ab42c3b47b6d2e
fuzzelhjb <fuzzelhjb@14438d79-0ffd-4333-8ae7-ea2bdd901387>
2019-12-01 11:35:20 +0000

    replace old color buttons with new ones
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@3892 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- dialogs/EditorOptionsDialog.h
+++ dialogs/EditorOptionsDialog.h
@@ -1,6 +1,13 @@
 #ifndef H_EDITOR_OPTIONS_DIALOG
 #define H_EDITOR_OPTIONS_DIALOG
 
+#include <dlgs.h>
+#include <cderr.h>
+#include <wxx_appcore.h>		// Add CCriticalSection, CObject, CWinThread, CWinApp
+#include <wxx_commondlg.h>		// Add CCommonDialog, CColorDialog, CFileDialog, CFindReplace, CFontDialog 
+#include "Properties/PropertyDialog.h"
+#include <WindowsX.h>
+
 class EditorOptionsDialog : public CDialog
 {
 public:
@@ -17,6 +24,13 @@ private:
     void AddToolTip(char *text, HWND parentHwnd, HWND toolTipHwnd, HWND controlHwnd);
 
     CToolTip *m_toolTip;
+    CColorDialog m_colorDialog;
+    ColorButton m_colorButton2;
+    ColorButton m_colorButton3;
+    ColorButton m_colorButton4;
+    ColorButton m_colorButton5;
+    ColorButton m_colorButton6;
+
 };
 
 #endif // !H_EDITOR_OPTIONS_DIALOG
