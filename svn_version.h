f668c3b7c499b67c731adf60f0b23979559cd4f4
fuzzelhjb <chschmidt9@gmail.com>
2020-01-31 20:04:34 +0000

    fix crash on exit and auto save timer
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@3947 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- svn_version.h
+++ svn_version.h
@@ -1,6 +1,6 @@
 #ifndef _SVN_VERSION_H_
 #define _SVN_VERSION_H_
 
-#define SVN_REVISION            3943       // Highest committed revision number in the working copy
+#define SVN_REVISION            3946       // Highest committed revision number in the working copy
 
 #endif
