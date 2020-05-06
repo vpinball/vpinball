1b9bb9a99542e12af919a430c9cd1fbeb701169c
fuzzelhjb <chschmidt9@gmail.com>
2020-05-06 06:45:58 +0000

    fix timer value property handling
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@4076 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- svn_version.h
+++ svn_version.h
@@ -1,6 +1,6 @@
 #ifndef _SVN_VERSION_H_
 #define _SVN_VERSION_H_
 
-#define SVN_REVISION            4071       // Highest committed revision number in the working copy
+#define SVN_REVISION            4075       // Highest committed revision number in the working copy
 
 #endif
