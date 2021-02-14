445616188111a93b7038706918e38c8e729a4614
toxie <toxie@ainc.de>
2021-02-14 19:40:27 +0000

    d'oh!
    git-svn-id: https://svn.code.sf.net/p/vpinball/code/trunk@4399 14438d79-0ffd-4333-8ae7-ea2bdd901387

--- imgui/imgui_impl_win32.h
+++ imgui/imgui_impl_win32.h
@@ -25,9 +25,9 @@ IMGUI_IMPL_API void     ImGui_ImplWin32_NewFrame();
 // Win32 message handler your application need to call.
 // - Intentionally commented out in a '#if 0' block to avoid dragging dependencies on <windows.h> from this helper.
 // - You should COPY the line below into your .cpp code to forward declare the function and then you can call it.
-#if 0
+//#if 0
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-#endif
+//#endif
 
 // DPI-related helpers (optional)
 // - Use to enable DPI awareness without having to create an application manifest.
