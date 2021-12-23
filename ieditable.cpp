3010b3da0857407f04abc74c3adcf9381e525e78
toxie <toxieainc@users.noreply.github.com>
2021-12-23 21:51:48 +0100

    use correct scope, as a pointer uses that data

--- ieditable.cpp
+++ ieditable.cpp
@@ -219,9 +219,9 @@ void IEditable::SetName(const std::string& name)
     const WCHAR* namePtr = newName;
     MultiByteToWideCharNull(CP_ACP, 0, name.c_str(), -1, newName, sizeof(GetScriptable()->m_wzName)/sizeof(GetScriptable()->m_wzName[0]));
     const bool isEqual = (wcscmp(newName, GetScriptable()->m_wzName) == 0);
+    WCHAR uniqueName[sizeof(GetScriptable()->m_wzName)/sizeof(GetScriptable()->m_wzName[0])];
     if(!isEqual && !pt->IsNameUnique(newName))
     {
-       WCHAR uniqueName[sizeof(GetScriptable()->m_wzName) / sizeof(GetScriptable()->m_wzName[0])];
        pt->GetUniqueName(newName, uniqueName, sizeof(uniqueName) / sizeof(uniqueName[0]));
        namePtr = uniqueName;
     }
