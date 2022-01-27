4b34afa8af91895b43e206e30bc3735b990b931c
toxie <toxieainc@users.noreply.github.com>
2022-01-27 22:17:32 +0100

    add from VPVR

---
+++ TextureManager.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <map>
+
+#include "Texture.h"
+#include "stdafx.h"
+#include "typeDefs3D.h"
+
+class RenderDevice;
+
+class TextureManager {
+public:
+   TextureManager(RenderDevice& rd) : m_rd(rd)
+   { }
+
+   ~TextureManager()
+   {
+      UnloadAll();
+   }
+
+   D3DTexture* LoadTexture(BaseTexture* memtex, const bool linearRGB, const bool clamptoedge = false);
+   void SetDirty(BaseTexture* memtex);
+   void UnloadTexture(BaseTexture* memtex);
+   void UnloadAll();
+
+private:
+   struct TexInfo
+   {
+      D3DTexture* d3dtex;
+      int texWidth;
+      int texHeight;
+      bool dirty;
+   };
+
+   RenderDevice& m_rd;
+   std::map<BaseTexture*, TexInfo> m_map;
+   typedef std::map<BaseTexture*, TexInfo>::iterator Iter;
+};
