9d2871ff7c5e988113c0184c1d1aaee8f2e002b2
Vincent Bousquet <vincent.bousqt@gmail.com>
2022-10-01 10:44:23 +0200

    Cleanups to sync with VPVR

--- Texture.cpp
+++ Texture.cpp
@@ -11,6 +11,16 @@
 #define STBI_NO_FAILURE_STRINGS
 #include "stb_image.h"
 
+BaseTexture::BaseTexture(const unsigned int w, const unsigned int h, const Format format)
+   : m_width(w)
+   , m_height(h)
+   , m_data((format == RGBA || format == SRGBA ? 4 : 3) * (format == RGB_FP32 ? 4 : format == RGB_FP16 ? 2 : 1) * w * h)
+   , m_realWidth(w)
+   , m_realHeight(h)
+   , m_format(format)
+{
+}
+
 BaseTexture* BaseTexture::CreateFromFreeImage(FIBITMAP* dib, bool resize_on_low_mem)
 {
    // check if Textures exceed the maximum texture dimension
