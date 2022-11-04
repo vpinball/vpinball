63762a6eae15ffb4dc229ab76b56ddda26f74f9e
Vincent Bousquet <vincent.bousqt@gmail.com>
2022-11-04 16:31:30 +0100

    Lazily create postprocess render targets
    Fix https://github.com/vpinball/vpinball/issues/163

--- RenderTarget.h
+++ RenderTarget.h
@@ -6,7 +6,7 @@ class RenderDevice;
 class RenderTarget final
 {
 public:
-   RenderTarget(RenderDevice* rd, int width = -1, int height = -1); // Default output render target
+   RenderTarget(RenderDevice* rd, int width, int height, colorFormat format); // Default output render target
    RenderTarget(RenderDevice* rd, const int width, const int height, const colorFormat format, bool with_depth, int nMSAASamples, StereoMode stereo, const char* failureMessage, RenderTarget* sharedDepth = nullptr);
    ~RenderTarget();
 
