0e7c25e482b064a455a1cc31ad96ab523ada73d8
Vincent Bousquet <vincent.bousqt@gmail.com>
2022-11-17 23:58:30 +0100

    Clean up & share ball shadow code

--- shader/LightShader.hlsl
+++ shader/LightShader.hlsl
@@ -1,8 +1,7 @@
+#include "Helpers.fxh"
+
 // transformation matrices
 const float4x4 matWorldViewProj : WORLDVIEWPROJ;
-
-#define NUM_BALLS 8
-const float4 balls[NUM_BALLS];
 
 const float4   lightColor_intensity;
 const float4   lightColor2_falloff_power;
@@ -31,43 +30,27 @@ VS_LIGHTBULB_OUTPUT vs_lightbulb_main (const in float4 vPosition : POSITION0/*,
 
 float4 PS_BulbLight(const in VS_LIGHTBULB_OUTPUT IN) : COLOR
 {
-	const float len = length(lightCenter_maxRange.xyz - IN.tablePos) * lightCenter_maxRange.w;
+   const float3 light_dir = IN.tablePos - lightCenter_maxRange.xyz;
+   const float light_dist = length(light_dir);
+	const float len = light_dist * lightCenter_maxRange.w;
 	const float atten = pow(1.0 - saturate(len), lightColor2_falloff_power.w);
 	const float3 lcolor = lerp(lightColor2_falloff_power.xyz, lightColor_intensity.xyz, sqrt(len));
-	return float4(
-	 lcolor*(-blend_modulate_vs_add*atten*lightColor_intensity.w), // negative as it will be blended with '1.0-thisvalue' (the 1.0 is needed to modulate the underlying elements correctly, but not wanted for the term below)
-	 1.0/blend_modulate_vs_add - 1.0); //saturate(atten*lightColor_intensity.w));
+	const float3 color = lcolor * (-blend_modulate_vs_add * atten * lightColor_intensity.w); // negative as it will be blended with '1.0-thisvalue' (the 1.0 is needed to modulate the underlying elements correctly, but not wanted for the term below)
+   return float4(color, 1.0 / blend_modulate_vs_add - 1.0); //saturate(atten*lightColor_intensity.w));
 }
+
+#include "BallShadows.fxh"
 
 float4 PS_BulbLight_with_ball_shadows(const in VS_LIGHTBULB_OUTPUT IN) : COLOR
 {
-	const float len = length(lightCenter_maxRange.xyz - IN.tablePos) * lightCenter_maxRange.w;
+   const float3 light_dir = IN.tablePos - lightCenter_maxRange.xyz;
+   const float light_dist = length(light_dir);
+	const float len = light_dist * lightCenter_maxRange.w;
 	const float atten = pow(1.0 - saturate(len), lightColor2_falloff_power.w);
 	const float3 lcolor = lerp(lightColor2_falloff_power.xyz, lightColor_intensity.xyz, sqrt(len));
-	float3 color = lcolor * (-blend_modulate_vs_add * atten * lightColor_intensity.w); // negative as it will be blended with '1.0-thisvalue' (the 1.0 is needed to modulate the underlying elements correctly, but not wanted for the term below)
-
-	// Raytraced ball shadows
-	for (int i = 0; i < NUM_BALLS; i++)
-	{
-		const float light_r = 10.0; // light radius in VPX units
-		const float ball_r = balls[i].w;
-		const float3 ball_pos = balls[i].xyz;
-		const float3 light_dir = IN.tablePos - lightCenter_maxRange.xyz;
-		const float light_dist = length(light_dir);
-		const float3 light_ray = light_dir / light_dist;
-		const float3 light_ball_ray = ball_pos - lightCenter_maxRange.xyz;
-		const float dot_lbr_lr = dot(light_ball_ray, light_ray);
-		// Don't cast shadow behind the light or before occluder
-		if (dot_lbr_lr > 0.0 && dot_lbr_lr < light_dist)
-		{
-			const float3 dist = light_ball_ray - dot_lbr_lr * light_ray;
-			const float d2 = length(dist);
-			const float smoothness = light_r * (1.0 - dot_lbr_lr / light_dist);
-			color.rgb *= 0.1 + 0.9 * smoothstep(ball_r - smoothness, ball_r + smoothness, d2);
-		}
-	}
-
-	return float4(color, 1.0 / blend_modulate_vs_add - 1.0); //saturate(atten*lightColor_intensity.w));
+	const float3 color = lcolor * (-blend_modulate_vs_add * atten * lightColor_intensity.w); // negative as it will be blended with '1.0-thisvalue' (the 1.0 is needed to modulate the underlying elements correctly, but not wanted for the term below)
+   const float shadow = get_light_ball_shadow(lightCenter_maxRange.xyz, light_dir, light_dist);
+	return float4(color * shadow, 1.0 / blend_modulate_vs_add - 1.0); //saturate(atten*lightColor_intensity.w));
 }
 
 technique bulb_light
@@ -76,7 +59,6 @@ technique bulb_light
    {
 		vertexshader = compile vs_3_0 vs_lightbulb_main();
 		pixelshader  = compile ps_3_0 PS_BulbLight();
-		// pixelshader  = compile ps_3_0 PS_BulbLight_with_ball_shadows();
    }
 }
 
