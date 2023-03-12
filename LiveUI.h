0b78d7b97fc5f928df36b3dd5eb542b0b7590ecc
Vincent Bousquet <vincent.bousqt@gmail.com>
2023-03-12 22:51:26 +0100

    LiveUI: full rework for live/startup property edition

--- LiveUI.h
+++ LiveUI.h
@@ -2,6 +2,8 @@
 
 #include "stdafx.h"
 
+#include "imgui/imgui.h"
+
 class LiveUI
 {
 public:
@@ -31,15 +33,25 @@ private:
    void UpdateRendererInspectionModal();
 
    // UI Selection & properties
-   void CameraProperties();
-   void MaterialProperties();
-   void FlasherProperties();
-   void LightProperties();
-   void PrimitiveProperties();
-   void RampProperties();
-   void RubberProperties();
-   void SurfaceProperties();
-   void TableProperties();
+   void CameraProperties(bool is_live);
+   void MaterialProperties(bool is_live);
+   void TableProperties(bool is_live);
+   void FlasherProperties(bool is_live, Flasher *startup_obj, Flasher *live_obj);
+   void LightProperties(bool is_live, Light *startup_obj, Light *live_obj);
+   void PrimitiveProperties(bool is_live, Primitive *startup_obj, Primitive *live_obj);
+   void RampProperties(bool is_live, Ramp *startup_obj, Ramp *live_obj);
+   void RubberProperties(bool is_live, Rubber *startup_obj, Rubber *live_obj);
+   void SurfaceProperties(bool is_live, Surface *startup_obj, Surface *live_obj);
+
+   // Helpers for property edition
+   typedef std::function<void(LiveUI*, float prev, float v)> OnFloatPropChange;
+   void PropSeparator(const char *label = nullptr);
+   void PropCheckbox(const char *label, bool is_live, bool *startup_v, bool *live_v);
+   void PropFloat(const char *label, bool is_live, float *startup_v, float *live_v, float step, float step_fast, const char *format = "%.3f",
+      ImGuiInputTextFlags flags = ImGuiInputTextFlags_CharsDecimal, OnFloatPropChange chg_callback = nullptr);
+   void PropInt(const char *label, bool is_live, int *startup_v, int *live_v);
+   void PropRGB(const char *label, bool is_live, COLORREF *startup_v, COLORREF *live_v, ImGuiColorEditFlags flags = 0);
+   void PropVec3(const char *label, bool is_live, Vertex3Ds *startup_v, Vertex3Ds *live_v, const char *format = "%.3f", ImGuiInputTextFlags flags = ImGuiInputTextFlags_CharsDecimal);
 
    // Enter/Exit edit mode (manage table backup, dynamic mode,...)
    void HideUI();
@@ -51,12 +63,13 @@ private:
    VPinball *m_app;
    Player *m_player;
    PinTable *m_table; // The edited table
-   PinTable *m_live_table; // The live copy of the edited being played by the player
+   PinTable *m_live_table; // The live copy of the edited table being played by the player (all properties can be changed at any time by the script)
    PinInput *m_pininput;
    Pin3D *m_pin3d;
    struct Selection
    {
       enum SelectionType { S_NONE, S_CAMERA, S_MATERIAL, S_EDITABLE } type = S_NONE;
+      bool is_live;
       union
       {
          int camera;
