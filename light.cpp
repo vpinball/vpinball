22bd53d9db03e44a3b43b1bca6813588026756a4
Vincent Bousquet <vincent.bousqt@gmail.com>
2023-05-06 23:27:52 +0200

    Minor Clean ups

--- light.cpp
+++ light.cpp
@@ -816,7 +816,6 @@ void Light::UpdateAnimation(const float diff_time_msec)
             m_currentFilamentTemperature = bulb_cool_down(BULB_44, m_currentFilamentTemperature, remaining_time);
          }
          m_currentIntensity = (float)(bulb_filament_temperature_to_emission(m_currentFilamentTemperature) * m_d.m_intensity * m_d.m_intensity_scale);
-         OutputDebugString(std::to_string(m_currentFilamentTemperature).append("\n"s).c_str());
       }
       break;
       }
