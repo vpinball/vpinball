5b270b26cd4b2175b5e9b65d5cc4fde3235d5444
toxie <toxieainc@users.noreply.github.com>
2023-10-14 16:24:58 +0200

    whoops

--- dialogs/KeysConfigDialog.h
+++ dialogs/KeysConfigDialog.h
@@ -16,7 +16,7 @@ protected:
 private:
     void AddToolTip(char *text, HWND parentHwnd, HWND toolTipHwnd, HWND controlHwnd);
     void AddStringDOF(const string& name, const int idc);
-    void AddStringAxis(const string& name, const int idc);
+    void AddStringAxis(const string& name, const int idc, const int def);
     void AddJoyCustomKey(const string& name, const int idc, const int default_key);
     void AddWndProc(const int idc, const size_t MyKeyButtonProc, const size_t pksw);
     HWND GetItemHwnd(int nID);
