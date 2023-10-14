9a1d8d92d9cd0adcb8016989039aa9e7849bff0a
Vincent Bousquet <vincent.bousqt@gmail.com>
2023-10-14 08:12:19 +0200

    Fix flipped pincab glass

--- pin/player.cpp
+++ pin/player.cpp
@@ -1058,7 +1058,7 @@ void Player::AddCabinetBoundingHitShapes()
    // glass:
    Vertex3Ds glassNormal(0, m_ptable->m_bottom - m_ptable->m_top, m_ptable->m_glassBottomHeight - m_ptable->m_glassTopHeight);
    glassNormal.Normalize();
-   m_hitTopGlass = HitPlane(Vertex3Ds(0, -glassNormal.z, glassNormal.y), m_ptable->m_glassTopHeight);
+   m_hitTopGlass = HitPlane(Vertex3Ds(0, glassNormal.z, -glassNormal.y), -m_ptable->m_glassTopHeight);
    m_hitTopGlass.m_elasticity = 0.2f;
 }
 
