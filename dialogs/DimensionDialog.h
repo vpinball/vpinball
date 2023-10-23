514dd81825bd3177ef404db8a7a0963f27140c00
Vincent Bousquet <vincent.bousqt@gmail.com>
2023-10-23 14:43:37 +0200

    Move dimension DB to assets, prepare for adding glass position

--- dialogs/DimensionDialog.h
+++ dialogs/DimensionDialog.h
@@ -10,6 +10,21 @@ protected:
    virtual BOOL OnInitDialog();
    virtual INT_PTR DialogProc(UINT uMsg, WPARAM wParam, LPARAM lParam);
    virtual BOOL OnCommand(WPARAM wParam, LPARAM lParam);
+   virtual LRESULT OnNotify(WPARAM wparam, LPARAM lparam);
+
+private:
+   CListView m_listView;
+   struct ManufacturerDimensions
+   {
+      string name;
+      float width;
+      float height;
+      float glassBottom;
+      float glassTop;
+      string comment;
+   };
+   vector<ManufacturerDimensions> m_dimensions;
+   bool m_discardChangeNotification = false;
 };
 
 #endif
