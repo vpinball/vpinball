424a905337d5c7cc436bef5a997408c96e7ef779
Vincent Bousquet <vincent.bousqt@gmail.com>
2023-10-31 19:30:19 +0100

    Render API: update Flasher

--- src/parts/flasher.h
+++ src/parts/flasher.h
@@ -96,6 +96,14 @@ public:
 
    void AddPoint(int x, int y, const bool smooth) final;
 
+   virtual void RenderSetup(RenderDevice *device);
+   virtual void Render(const unsigned int renderMask);
+   virtual void RenderRelease();
+
+protected:
+   RenderDevice *m_rd = nullptr;
+
+public:
    bool IsTransparent() const final { return !m_d.m_isDMD; }
    float GetDepth(const Vertex3Ds& viewDir) const final
    {
@@ -134,7 +142,6 @@ public:
    bool m_inPlayState;
 
 private:
-   void UpdateMesh();
    void InitShape();
 
    PinTable *m_ptable;
