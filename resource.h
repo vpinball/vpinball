b520af1602d2fb28e0cc68e6f863727181fb4a90
toxie <toxieainc@users.noreply.github.com>
2023-11-29 23:22:06 +0100

    correct _APS_NEXT_CONTROL_VALUE

--- resource.h
+++ resource.h
@@ -1451,7 +1451,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        675
 #define _APS_NEXT_COMMAND_VALUE         4030
-#define _APS_NEXT_CONTROL_VALUE         927
+#define _APS_NEXT_CONTROL_VALUE         926
 #define _APS_NEXT_SYMED_VALUE           188
 #endif
 #endif
