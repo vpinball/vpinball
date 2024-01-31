c714f4b34ea2a292c9865d9f6cff487f77b7d6cf
Vincent Bousquet <vincent.bousqt@gmail.com>
2024-01-31 23:07:26 +0100

    Add Reflection/Refraction probe to script API
    Fix https://github.com/vpinball/vpinball/issues/1390

--- src/core/player.cpp
+++ src/core/player.cpp
@@ -3040,8 +3040,8 @@ void Player::DrawBulbLightBuffer()
       if (hitable->HitableGetItemType() == eItemLight)
          hitable->Render(m_render_mask);
    m_render_mask &= ~LIGHT_BUFFER;
-
    bool hasLight = p3dDevice->GetCurrentPass()->GetCommandCount() > 0;
+
    if (hasLight)
    { // Only apply blur if we have actually rendered some lights
       RenderPass* renderPass = p3dDevice->GetCurrentPass();
