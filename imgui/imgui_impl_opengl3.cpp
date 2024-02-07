2bd5f8797b733e019ffae21f27254566c63b823f
Vincent Bousquet <vincent.bousqt@gmail.com>
2024-02-07 23:12:57 +0100

    Fix VS build

--- imgui/imgui_impl_opengl3.cpp
+++ imgui/imgui_impl_opengl3.cpp
@@ -104,11 +104,6 @@
 //  ES 2.0    100       "#version 100"      = WebGL 1.0
 //  ES 3.0    300       "#version 300 es"   = WebGL 2.0
 //----------------------------------------
-
-#ifndef __STANDALONE__
-#include "stdafx.h"
-#endif
-
 #if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
 #define _CRT_SECURE_NO_WARNINGS
 #endif
