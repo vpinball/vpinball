ffcef3dccab373db87170e997652341fbb9f3c12
Yellow Labrador <132696731+YellowLabrador@users.noreply.github.com>
2024-02-19 12:34:15 -0800

    Android launcher fixes: actually using a background thread to copy files from assets, added a spinner to while copying files and fixed theme to work with dark mode.

--- standalone/android/android-project/app/src/main/java/org/vpinball/app/VpxLauncherActivity.kt
+++ standalone/android/android-project/app/src/main/java/org/vpinball/app/VpxLauncherActivity.kt
@@ -17,6 +17,8 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -27,33 +29,33 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
@@ -62,6 +64,7 @@ import androidx.core.content.edit
 import androidx.core.net.toUri
 import androidx.documentfile.provider.DocumentFile
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.vpinball.app.ui.theme.AndroidprojectTheme
 import java.io.File
@@ -73,6 +76,8 @@ class VpxLauncherActivity : ComponentActivity() {
     val tablesList = mutableStateListOf("")
     val isConfigured = mutableStateOf(false)
     val showErrorDialog = mutableStateOf(false)
+    val showWorkingOverlay = mutableStateOf(false)
+    val overlayMessage = mutableStateOf("")
     var vpxDir: Uri? = null
     var vpxDirFd: ParcelFileDescriptor? = null
 
@@ -116,6 +121,10 @@ class VpxLauncherActivity : ComponentActivity() {
                         PinballTables(tablesList)
                     else
                         InitialMessage()
+
+                    if (showWorkingOverlay.value) {
+                        WorkingOverlay(overlayMessage)
+                    }
                 }
             }
         }
@@ -185,30 +194,47 @@ class VpxLauncherActivity : ComponentActivity() {
                 tablesDir = doc.createDirectory("tables")
             }
 
-            // Tell Compose to display the list of tables
-            isConfigured.value = true
-
             if (updateAssets) {
-                lifecycleScope.launch {
+                // Show an overlay while copying asset files
+                // On physical devices this can take quile a while
+                Log.d(TAG, "Copying assets")
+                overlayMessage.value = getString(R.string.copying_assets_message)
+                showWorkingOverlay.value = true
+
+                // Copy files using a background thread
+                lifecycleScope.launch(Dispatchers.IO) {
                     copyAssets("", doc)
+
+                    Log.d(TAG, "Done copying assets to the selected VPX dir")
+                    overlayMessage.value = ""
+                    showWorkingOverlay.value = false
+
+                    updateTables(tablesDir!!)
                 }
             }
 
-            // Add all VPX tables to the UI
-            tablesList.clear()
-            tablesDir!!.listFiles()
-                .filter {
-                    it.name?.endsWith(".vpx", ignoreCase = true) == true
-                }.forEach {
-                    val tableName = it.name!!.dropLast(4)
-                    tablesList.add(tableName)
-                }
+            // Tell Compose to display the list of tables
+            isConfigured.value = true
+
+            updateTables(tablesDir!!)
         } catch (e: Exception) {
             // Request access from the user either if there is no VPX directory selected
             // or we cannot access it anymore
             val suggestedVpxDir = File(Environment.getExternalStorageDirectory(), "vpx").toUri()
             askUserForVpxDirectory(suggestedVpxDir)
         }
+    }
+
+    // Add all VPX tables to the UI
+    private fun updateTables(tablesDir: DocumentFile) {
+        tablesList.clear()
+        tablesDir!!.listFiles()
+            .filter {
+                it.name?.endsWith(".vpx", ignoreCase = true) == true
+            }.forEach {
+                val tableName = it.name!!.dropLast(4)
+                tablesList.add(tableName)
+            }
     }
 
     @Throws(IOException::class)
@@ -278,7 +304,7 @@ class VpxLauncherActivity : ComponentActivity() {
                 }
 
                 vpxDir = uri
-                openVpxDirectory()
+                openVpxDirectory(true)
             }
         }
     }
@@ -298,9 +324,9 @@ fun InitialMessage() {
     Column(modifier = Modifier.padding(horizontal = 14.dp)) {
 
         Column(modifier = Modifier.weight(1f).fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally) {
+            horizontalAlignment = Alignment.CenterHorizontally) {
             Spacer(modifier = Modifier.height(48.dp))
-            Text(text = stringResource(R.string.select_dir_msg))
+            Text(stringResource(R.string.select_dir_msg))
             Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
                 Button(
                     onClick = {
@@ -340,10 +366,8 @@ fun PinballTables(tablesList: SnapshotStateList<String>) {
                 .padding(horizontal = 8.dp)
         ) {
             tables.forEach { table ->
-                ClickableText(text = AnnotatedString(table),
-                    modifier = Modifier.fillMaxWidth(),
-                    overflow = TextOverflow.Visible,
-                    onClick = {
+                Text(text = table,
+                    modifier = Modifier.fillMaxWidth().clickable {
                         val intent = Intent(activity, VPinballActivity::class.java).apply {
                             putExtra("table", "$table.vpx")
                             putExtra("dirFd", activity.vpxDirFd!!.detachFd())
@@ -351,7 +375,7 @@ fun PinballTables(tablesList: SnapshotStateList<String>) {
                         activity.startActivity(intent)
                     })
 
-                Divider()
+                HorizontalDivider()
             }
         }
 
@@ -373,5 +397,25 @@ fun PinballTables(tablesList: SnapshotStateList<String>) {
             }
         }
 
+    }
+}
+
+
+@Composable
+fun WorkingOverlay(message: MutableState<String>) {
+    Box(Modifier
+        .fillMaxSize()
+        .background(Color.Gray.copy(alpha = 0.5f))
+        .pointerInput(Unit) {}
+    ) {
+        Column(
+            modifier = Modifier.matchParentSize().padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            CircularProgressIndicator()
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(message.value)
+        }
     }
 }
