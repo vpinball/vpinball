600638cca2d4ee660d08983999dc0874db1f759f
Vincent Bousquet <vincent.bousqt@gmail.com>
2024-03-09 08:23:13 +0100

    Fix crash on exit when VideoCap is used
    Fix https://github.com/vpinball/vpinball/issues/1511

--- src/parts/flasher.cpp
+++ src/parts/flasher.cpp
@@ -267,7 +267,6 @@ void Flasher::EndPlay()
    IEditable::EndPlay();
    // ensure not locked just in case the player exits during a LS sequence
    m_lockedByLS = false;
-   ResetVideoCap();
 }
 
 void Flasher::SetObjectPos()
@@ -1180,6 +1179,7 @@ void Flasher::RenderSetup(RenderDevice *device)
 void Flasher::RenderRelease()
 {
    assert(m_rd != nullptr);
+   ResetVideoCap();
    delete m_meshBuffer;
    delete[] m_vertices;
    delete m_texdmd;
