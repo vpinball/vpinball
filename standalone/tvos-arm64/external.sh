6294f522b6b6d74ef5b3200aec4ad9f19e700007
Jason Millard <jsm174@gmail.com>
2024-03-11 07:53:19 -0400

    standalone: overhaul externals caching strategy

--- standalone/tvos-arm64/external.sh
+++ standalone/tvos-arm64/external.sh
@@ -2,26 +2,28 @@
 
 set -e
 
-BGFX_CMAKE_VERSION=1.125.8678-462
-
+FREEIMAGE_VERSION=3.18.0
 SDL2_VERSION=2.30.0
 SDL2_IMAGE_VERSION=2.8.2
 SDL2_TTF_VERSION=2.22.0
-
-PINMAME_SHA=e867f6e50e12238e0db658ccc9dde6d19a350c12
+PINMAME_SHA=963dfb17b27932c7e69e926f7aec8a630156f4f0
 LIBALTSOUND_SHA=9ac08a76e2aabc1fba57d3e5a3b87e7f63c09e07
-LIBDMDUTIL_SHA=c6ab88089ab81b4a5bc676927369351059becde5
+LIBDMDUTIL_SHA=ba8ed0c1f8d8abef89f109e35a1f3556903bd1f8
+BGFX_CMAKE_VERSION=1.125.8678-462
+FFMPEG_SHA=e38092ef9395d7049f871ef4d5411eb410e283e0
 
 NUM_PROCS=$(sysctl -n hw.ncpu)
 
 echo "Building external libraries..."
-echo "  BGFX_CMAKE_VERSION: ${BGFX_CMAKE_VERSION}"
+echo "  FREEIMAGE_VERSION: ${FREEIMAGE_VERSION}"
 echo "  SDL2_VERSION: ${SDL2_VERSION}"
 echo "  SDL2_IMAGE_VERSION: ${SDL2_IMAGE_VERSION}"
 echo "  SDL2_TTF_VERSION: ${SDL2_TTF_VERSION}"
 echo "  PINMAME_SHA: ${PINMAME_SHA}"
 echo "  LIBALTSOUND_SHA: ${LIBALTSOUND_SHA}"
 echo "  LIBDMDUTIL_SHA: ${LIBDMDUTIL_SHA}"
+echo "  BGFX_CMAKE_VERSION: ${BGFX_CMAKE_VERSION}"
+echo "  FFMPEG_SHA: ${FFMPEG_SHA}"
 echo ""
 
 if [ -z "${BUILD_TYPE}" ]; then
@@ -32,136 +34,252 @@ echo "Build type: ${BUILD_TYPE}"
 echo "Procs: ${NUM_PROCS}"
 echo ""
 
-rm -rf external
-mkdir external
-mkdir external/framework
-mkdir external/include
-mkdir external/lib
+CACHE_DIR="external/cache/${BUILD_TYPE}"
+
+rm -rf external/include external/lib external/framework
+mkdir -p external/include external/lib external/framework ${CACHE_DIR}
 
 rm -rf tmp
 mkdir tmp
 cd tmp
 
 #
-# build bgfx and copy to external
-#
-
-curl -sL https://github.com/bkaradzic/bgfx.cmake/releases/download/v${BGFX_CMAKE_VERSION}/bgfx.cmake.v${BGFX_CMAKE_VERSION}.tar.gz -o bgfx.cmake.v${BGFX_CMAKE_VERSION}.tar.gz
-tar -xvzf bgfx.cmake.v${BGFX_CMAKE_VERSION}.tar.gz
-cd bgfx.cmake
-cp -r bgfx/include/bgfx ../../external/include
-cp -r bimg/include/bimg ../../external/include
-cp -r bx/include/bx ../../external/include
-cmake -S. \
-   -DCMAKE_SYSTEM_NAME=tvOS \
-   -DCMAKE_OSX_DEPLOYMENT_TARGET=16.0 \
-   -DCMAKE_OSX_ARCHITECTURES=arm64 \
-   -DBGFX_BUILD_EXAMPLES=OFF \
-   -DCMAKE_BUILD_TYPE=Release \
-   -B build
-cmake --build build -- -j${NUM_PROCS}
-cp build/cmake/bgfx/*.a ../../external/lib
-cp build/cmake/bimg/*.a ../../external/lib
-cp build/cmake/bx/*.a ../../external/lib
-cd ..
-
-#
 # build freeimage and copy to external
 #
 
-curl -sL https://downloads.sourceforge.net/project/freeimage/Source%20Distribution/3.18.0/FreeImage3180.zip -o FreeImage3180.zip
-unzip FreeImage3180.zip
-cd FreeImage
-patch -i ../../freeimage/FreeImage3180.patch
-cp ../../freeimage/Makefile.tvos.arm64 .
-make -f Makefile.tvos.arm64 -j${NUM_PROCS}
-cp Dist/libfreeimage.a ../../external/lib
-cd ..
+CACHE_NAME="FreeImage-${FREEIMAGE_VERSION}"
+
+if [ ! -f "../${CACHE_DIR}/${CACHE_NAME}.cache" ]; then
+   FREEIMAGE_BASENAME="FreeImage${FREEIMAGE_VERSION//./}"
+   curl -sL https://downloads.sourceforge.net/project/freeimage/Source%20Distribution/${FREEIMAGE_VERSION}/${FREEIMAGE_BASENAME}.zip -o ${FREEIMAGE_BASENAME}.zip
+   unzip ${FREEIMAGE_BASENAME}.zip
+   cd FreeImage
+   patch -i ../../freeimage/FreeImage3180.patch
+   cp ../../freeimage/Makefile.tvos.arm64 .
+   make -f Makefile.tvos.arm64 -j${NUM_PROCS}
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cp Dist/libfreeimage.a ../../${CACHE_DIR}/${CACHE_NAME}/lib/libfreeimage.a
+   cd ..
+   touch "../${CACHE_DIR}/${CACHE_NAME}.cache"
+fi
+
+cp ../${CACHE_DIR}/${CACHE_NAME}/lib/*.a ../external/lib
 
 #
-# copy bass24 framework to external
-# 
+# download bass24 and copy to external
+#
 # Note: The BASS library needs to be replaced with the tvOS version.
 # Leaving the iOS version here so we can test the compile. It will fail when linking. 
 #
 
-curl -s https://www.un4seen.com/files/bass24-ios.zip -o bass.zip
-unzip bass.zip 
-cp -r bass.xcframework/ios-arm64_armv7_armv7s/bass.framework ../external/framework
+CACHE_NAME="bass24"
+
+if [ ! -f "../${CACHE_DIR}/${CACHE_NAME}.cache" ]; then
+   curl -sL https://www.un4seen.com/files/bass24-tvos.zip -o bass.zip
+   unzip bass.zip
+   mkdir -p ../${CACHE_DIR}/${CACHE_NAME}/framework
+   cp -r bass.xcframework/ios-arm64_armv7_armv7s/bass.framework ../${CACHE_DIR}/${CACHE_NAME}/framework
+   touch "../${CACHE_DIR}/${CACHE_NAME}.cache"
+fi
+
+cp -r ../${CACHE_DIR}/${CACHE_NAME}/framework/*.framework ../external/framework
 
 #
 # build SDL2 and copy to external
 #
 
-curl -sL https://github.com/libsdl-org/SDL/releases/download/release-${SDL2_VERSION}/SDL2-${SDL2_VERSION}.zip -o SDL2-${SDL2_VERSION}.zip
-unzip SDL2-${SDL2_VERSION}.zip
-cp -r SDL2-${SDL2_VERSION}/include ../external/include/SDL2
+CACHE_NAME="SDL2-${SDL2_VERSION}"
 
-xcrun xcodebuild -project "SDL2-${SDL2_VERSION}/Xcode/SDL/SDL.xcodeproj" -target "Static Library-tvOS" -sdk appletvos -configuration Release clean build CONFIGURATION_BUILD_DIR="$(pwd)/sdl-build"
-cp sdl-build/libSDL2.a ../external/lib
+if [ ! -f "../${CACHE_DIR}/${CACHE_NAME}.cache" ]; then
+   curl -sL https://github.com/libsdl-org/SDL/releases/download/release-${SDL2_VERSION}/SDL2-${SDL2_VERSION}.zip -o SDL2-${SDL2_VERSION}.zip
+   unzip SDL2-${SDL2_VERSION}.zip
+   cd SDL2-${SDL2_VERSION}
+   xcrun xcodebuild \
+      -project "Xcode/SDL/SDL.xcodeproj" \
+      -target "Static Library-tvOS" \
+      -sdk appletvos \
+      -configuration Release \
+      clean build CONFIGURATION_BUILD_DIR="$(pwd)/sdl-build"
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/include
+   cp include/*.h ../../${CACHE_DIR}/${CACHE_NAME}/include
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cp sdl-build/*.a ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cd ..
+   touch "../${CACHE_DIR}/${CACHE_NAME}.cache"
+fi
+ 
+mkdir -p ../external/include/SDL2
+cp -r ../${CACHE_DIR}/${CACHE_NAME}/include/* ../external/include/SDL2
+cp ../${CACHE_DIR}/${CACHE_NAME}/lib/*.a ../external/lib
 
 #
 # build SDL2_image and copy to external
 #
 
-curl -sL https://github.com/libsdl-org/SDL_image/releases/download/release-${SDL2_IMAGE_VERSION}/SDL2_image-${SDL2_IMAGE_VERSION}.zip -o SDL2_image-${SDL2_IMAGE_VERSION}.zip
-unzip SDL2_image-${SDL2_IMAGE_VERSION}.zip
-cp SDL2_image-${SDL2_IMAGE_VERSION}/include/SDL_image.h ../external/include/SDL2
+CACHE_NAME="SDL2_image-${SDL2_IMAGE_VERSION}"
 
-xcrun xcodebuild -project "SDL2_image-${SDL2_IMAGE_VERSION}/Xcode/SDL_image.xcodeproj" -target "Static Library" -sdk appletvos -configuration Release clean build CONFIGURATION_BUILD_DIR="$(pwd)/sdl_image-build"
-cp sdl_image-build/libSDL2_image.a ../external/lib
+if [ ! -f "../${CACHE_DIR}/${CACHE_NAME}.cache" ]; then
+   curl -sL https://github.com/libsdl-org/SDL_image/releases/download/release-${SDL2_IMAGE_VERSION}/SDL2_image-${SDL2_IMAGE_VERSION}.zip -o SDL2_image-${SDL2_IMAGE_VERSION}.zip
+   unzip SDL2_image-${SDL2_IMAGE_VERSION}.zip
+   cd SDL2_image-${SDL2_IMAGE_VERSION}
+   xcrun xcodebuild \
+      -project "Xcode/SDL_image.xcodeproj" \
+      -target "Static Library" \
+      -sdk appletvos -configuration Release \
+      clean build CONFIGURATION_BUILD_DIR="$(pwd)/sdl_image-build"
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/include
+   cp include/*.h ../../${CACHE_DIR}/${CACHE_NAME}/include
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cp sdl_image-build/*.a ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cd ..
+   touch "../${CACHE_DIR}/${CACHE_NAME}.cache"
+fi
+
+cp -r ../${CACHE_DIR}/${CACHE_NAME}/include/* ../external/include/SDL2
+cp ../${CACHE_DIR}/${CACHE_NAME}/lib/*.a ../external/lib
 
 #
 # build SDL2_ttf and copy to external
 #
 
-curl -sL https://github.com/libsdl-org/SDL_ttf/releases/download/release-${SDL2_TTF_VERSION}/SDL2_ttf-${SDL2_TTF_VERSION}.zip -o SDL2_ttf-${SDL2_TTF_VERSION}.zip
-unzip SDL2_ttf-${SDL2_TTF_VERSION}.zip
-cp -r SDL2_ttf-${SDL2_TTF_VERSION}/SDL_ttf.h ../external/include/SDL2
+CACHE_NAME="SDL2_ttf-${SDL2_TTF_VERSION}"
 
-xcrun xcodebuild -project "SDL2_ttf-${SDL2_TTF_VERSION}/Xcode/SDL_ttf.xcodeproj" -target "Static Library" -sdk appletvos -configuration Release clean build CONFIGURATION_BUILD_DIR="$(pwd)/sdl_ttf-build"
-cp sdl_ttf-build/libSDL2_ttf.a ../external/lib
+if [ ! -f "../${CACHE_DIR}/${CACHE_NAME}.cache" ]; then
+   curl -sL https://github.com/libsdl-org/SDL_ttf/releases/download/release-${SDL2_TTF_VERSION}/SDL2_ttf-${SDL2_TTF_VERSION}.zip -o SDL2_ttf-${SDL2_TTF_VERSION}.zip
+   unzip SDL2_ttf-${SDL2_TTF_VERSION}.zip
+   cd SDL2_ttf-${SDL2_TTF_VERSION}
+   xcrun xcodebuild \
+      -project "Xcode/SDL_ttf.xcodeproj" \
+      -target "Static Library" \
+      -sdk appletvos \
+      -configuration Release \
+      clean build CONFIGURATION_BUILD_DIR="$(pwd)/sdl_ttf-build"
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/include
+   cp *.h ../../${CACHE_DIR}/${CACHE_NAME}/include
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cp sdl_ttf-build/*.a ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cd ..
+   touch "../${CACHE_DIR}/${CACHE_NAME}.cache"
+fi
+
+cp -r ../${CACHE_DIR}/${CACHE_NAME}/include/* ../external/include/SDL2
+cp ../${CACHE_DIR}/${CACHE_NAME}/lib/*.a ../external/lib
 
 #
 # build libpinmame and copy to external
 #
 
-curl -sL https://github.com/vpinball/pinmame/archive/${PINMAME_SHA}.zip -o pinmame.zip
-unzip pinmame.zip
-cd pinmame-$PINMAME_SHA
-cp src/libpinmame/libpinmame.h ../../external/include
-cp cmake/libpinmame/CMakeLists_tvos-arm64.txt CMakeLists.txt
-cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -B build
-cmake --build build -- -j${NUM_PROCS}
-cp build/libpinmame.3.6.a ../../external/lib
-cd ..
+CACHE_NAME="pinmame-${PINMAME_SHA}"
+
+if [ ! -f "../${CACHE_DIR}/${CACHE_NAME}.cache" ]; then
+   curl -sL https://github.com/vpinball/pinmame/archive/${PINMAME_SHA}.zip -o pinmame.zip
+   unzip pinmame.zip
+   cd pinmame-$PINMAME_SHA
+   cp cmake/libpinmame/CMakeLists_tvos-arm64.txt CMakeLists.txt
+   cmake \
+      -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+      -B build
+   cmake --build build -- -j${NUM_PROCS}
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/include
+   cp src/libpinmame/libpinmame.h ../../${CACHE_DIR}/${CACHE_NAME}/include
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cp build/*.a ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cd ..
+   touch "../${CACHE_DIR}/${CACHE_NAME}.cache"
+fi
+
+cp -r ../${CACHE_DIR}/${CACHE_NAME}/include/* ../external/include
+cp ../${CACHE_DIR}/${CACHE_NAME}/lib/*.a ../external/lib
 
 #
 # build libaltsound and copy to external
 #
 
-curl -sL https://github.com/vpinball/libaltsound/archive/${LIBALTSOUND_SHA}.zip -o libaltsound.zip
-unzip libaltsound.zip
-cd libaltsound-$LIBALTSOUND_SHA
-cp src/altsound.h ../../external/include
-platforms/tvos/arm64/external.sh
-cmake -DPLATFORM=tvos -DARCH=arm64 -DBUILD_SHARED=OFF -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -B build
-cmake --build build -- -j${NUM_PROCS}
-cp build/libaltsound.a ../../external/lib
-cd ..
+CACHE_NAME="libaltsound-${LIBALTSOUND_SHA}"
+
+if [ ! -f  "../${CACHE_DIR}/${CACHE_NAME}.cache" ]; then
+   curl -sL https://github.com/vpinball/libaltsound/archive/${LIBALTSOUND_SHA}.zip -o libaltsound.zip
+   unzip libaltsound.zip
+   cd libaltsound-$LIBALTSOUND_SHA
+   platforms/tvos/arm64/external.sh
+   cmake \
+      -DPLATFORM=tvos \
+      -DARCH=arm64 \
+      -DBUILD_SHARED=OFF \
+      -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+      -B build
+   cmake --build build -- -j${NUM_PROCS}
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/include
+   cp src/altsound.h ../../${CACHE_DIR}/${CACHE_NAME}/include
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cp build/*.a ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cd ..
+   touch "../${CACHE_DIR}/${CACHE_NAME}.cache"
+fi
+
+cp -r ../${CACHE_DIR}/${CACHE_NAME}/include/* ../external/include
+cp ../${CACHE_DIR}/${CACHE_NAME}/lib/*.a ../external/lib
 
 #
 # build libdmdutil (and deps) and copy to external
 #
 
-curl -sL https://github.com/vpinball/libdmdutil/archive/${LIBDMDUTIL_SHA}.zip -o libdmdutil.zip
-unzip libdmdutil.zip
-cd libdmdutil-$LIBDMDUTIL_SHA
-cp -r include/DMDUtil ../../external/include
-platforms/tvos/arm64/external.sh
-cp -r third-party/include/sockpp ../../external/include
-cmake -DPLATFORM=tvos -DARCH=arm64 -DBUILD_SHARED=OFF -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -B build
-cmake --build build -- -j${NUM_PROCS}
-cp third-party/build-libs/tvos/arm64/*.a ../../external/lib
-cp build/libdmdutil.a ../../external/lib
-cd ..
+CACHE_NAME="libdmdutil-${LIBDMDUTIL_SHA}"
+
+if [ ! -f "../${CACHE_DIR}/${CACHE_NAME}.cache" ]; then
+   curl -sL https://github.com/vpinball/libdmdutil/archive/${LIBDMDUTIL_SHA}.zip -o libdmdutil.zip
+   unzip libdmdutil.zip
+   cd libdmdutil-$LIBDMDUTIL_SHA
+   platforms/tvos/arm64/external.sh
+   cmake \
+      -DPLATFORM=tvos \
+      -DARCH=arm64 \
+      -DBUILD_SHARED=OFF \
+      -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+      -B build
+   cmake --build build -- -j${NUM_PROCS}
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/include
+   cp -r include/DMDUtil ../../${CACHE_DIR}/${CACHE_NAME}/include
+   cp -r third-party/include/sockpp ../../${CACHE_DIR}/${CACHE_NAME}/include
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cp third-party/build-libs/tvos/arm64/*.a ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cp build/*.a ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cd ..
+   touch "../${CACHE_DIR}/${CACHE_NAME}.cache"
+fi
+
+cp -r ../${CACHE_DIR}/${CACHE_NAME}/include/* ../external/include
+cp ../${CACHE_DIR}/${CACHE_NAME}/lib/*.a ../external/lib
+
+#
+# build bgfx and copy to external
+#
+
+CACHE_NAME="BGFX_CMAKE-${BGFX_CMAKE_VERSION}"
+
+if [ ! -f "../${CACHE_DIR}/${CACHE_NAME}.cache" ]; then
+   curl -sL https://github.com/bkaradzic/bgfx.cmake/releases/download/v${BGFX_CMAKE_VERSION}/bgfx.cmake.v${BGFX_CMAKE_VERSION}.tar.gz -o bgfx.cmake.v${BGFX_CMAKE_VERSION}.tar.gz
+   tar -xvzf bgfx.cmake.v${BGFX_CMAKE_VERSION}.tar.gz
+   cd bgfx.cmake
+   cmake -S. \
+      -DCMAKE_SYSTEM_NAME=tvOS \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=16.0 \
+      -DCMAKE_OSX_ARCHITECTURES=arm64 \
+      -DBGFX_BUILD_EXAMPLES=OFF \
+      -DCMAKE_BUILD_TYPE=Release \
+      -B build
+   cmake --build build -- -j${NUM_PROCS}
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/include
+   cp -r bgfx/include/bgfx ../../${CACHE_DIR}/${CACHE_NAME}/include
+   cp -r bimg/include/bimg ../../${CACHE_DIR}/${CACHE_NAME}/include
+   cp -r bx/include/bx ../../${CACHE_DIR}/${CACHE_NAME}/include
+   mkdir -p ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cp build/cmake/bgfx/*.a ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cp build/cmake/bimg/*.a ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cp build/cmake/bx/*.a ../../${CACHE_DIR}/${CACHE_NAME}/lib
+   cd ..
+   touch "../${CACHE_DIR}/${CACHE_NAME}.cache"
+fi
+
+cp -r ../${CACHE_DIR}/${CACHE_NAME}/include/* ../external/include
+cp ../${CACHE_DIR}/${CACHE_NAME}/lib/*.a ../external/lib
