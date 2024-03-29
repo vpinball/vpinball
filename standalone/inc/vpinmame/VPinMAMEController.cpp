aa70447305910102abf2f63997d3550e5fc63952
Markus Kalkbrenner <git@kalki.de>
2024-03-29 23:13:32 +0100

    fixed typo

--- standalone/inc/vpinmame/VPinMAMEController.cpp
+++ standalone/inc/vpinmame/VPinMAMEController.cpp
@@ -57,7 +57,7 @@ void PINMAMECALLBACK VPinMAMEController::OnDisplayAvailable(int index, int displ
          pDisplay->pDMD->SetRomName(pController->m_pPinmameGame->name);
       }
 
-      if (g_pplayer->m_ptable->m_settings.LoadValueWithDefault(Settings::Standalone, "PUPCature"s, false)) {
+      if (g_pplayer->m_ptable->m_settings.LoadValueWithDefault(Settings::Standalone, "PUPCapture"s, false)) {
          string szPupVideosPath = g_pvp->m_currentTablePath + "PupVideos" + PATH_SEPARATOR_CHAR;
          pDisplay->pDMD->SetPUPVideosPath(szPupVideosPath.c_str());
          pDisplay->pDMD->SetRomName(pController->m_pPinmameGame->name);
