b9bc347829e4d84542c4da4a3d8cc0dc8dd0c6d4
toxieainc <toxie@ainc.de>
2024-04-02 09:07:35 +0200

    port fRectIntersect3D wrong == test fix

--- math/bbox.h
+++ math/bbox.h
@@ -1,3 +1,5 @@
+// license:GPLv3+
+
 #pragma once
 
 class FRect3D final
@@ -81,19 +83,19 @@ inline bool Intersect(const RECT &rc, const int width, const int height, const P
 #if defined(_M_IX86) || defined(_M_X64)
 inline bool fRectIntersect3D(const FRect3D &rc1, const FRect3D &rc2)
 {
-   const __m128 rc1128 = _mm_loadu_ps(&rc1.left); // this shouldn't use loadu, but doesn't matter anymore nowadays anyhow
-   const __m128 rc1sh = _mm_shuffle_ps(rc1128, rc1128, _MM_SHUFFLE(1, 0, 3, 2));
-   const __m128 test = _mm_cmpge_ps(rc1sh, _mm_loadu_ps(&rc2.left));
-   const int mask = _mm_movemask_ps(test);
-   //   return ((mask == (1|(1<<1)|(0<<2)|(0<<3))) && rc1.zlow <= rc2.zhigh && rc1.zhigh >= rc2.zlow); //!! use SSE, too?
-   return ((mask == 3) && rc1.zlow <= rc2.zhigh && rc1.zhigh >= rc2.zlow); //!! use SSE, too?
-
-   //return (rc1.right >= rc2.left && rc1.bottom >= rc2.top && rc1.left <= rc2.right && rc1.top <= rc2.bottom && rc1.zlow <= rc2.zhigh && rc1.zhigh >= rc2.zlow);
+   const __m128 rc1128 = _mm_loadu_ps(&rc1.left); // L1.R1.T1.B1 // this shouldn't use loadu, but doesn't matter anymore nowadays anyhow
+   const __m128 rc2128 = _mm_loadu_ps(&rc2.left); // L2.R2.T2.B2
+   const __m128 min128 = _mm_shuffle_ps(rc1128, rc2128, _MM_SHUFFLE(1, 3, 1, 3)); // R1.B1.R2.B2
+   const __m128 max128 = _mm_shuffle_ps(rc2128, rc1128, _MM_SHUFFLE(0, 2, 0, 2)); // L2.T2.L1.T1
+   const __m128 test = _mm_cmpge_ps(min128, max128);
+   const int mask = _mm_movemask_ps(test); // bitmask of [R1 >= L2, B1 >= T2, R2 >= L1, B2 >= T1]
+   return ((mask == 15) && rc1.zlow <= rc2.zhigh && rc1.zhigh >= rc2.zlow); //!! use SSE, too?
 }
 #else
 inline bool fRectIntersect3D(const FRect3D &rc1, const FRect3D &rc2)
 {
-   return (rc1.right >= rc2.left && rc1.bottom >= rc2.top && rc1.left <= rc2.right && rc1.top <= rc2.bottom && rc1.zlow <= rc2.zhigh && rc1.zhigh >= rc2.zlow);
+   return (rc1.right >= rc2.left && rc1.bottom >= rc2.top && rc1.zhigh >= rc2.zlow 
+        && rc1.left <= rc2.right && rc1.top <= rc2.bottom && rc1.zlow <= rc2.zhigh);
 }
 #endif
 
