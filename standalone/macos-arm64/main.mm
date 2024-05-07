86537ea08e9c2f14656cb009c942aadaa81b508c
kaicherry <kaicherry@mac.com>
2024-05-07 09:02:17 -0700

    Update main.mm with NSOpenPanel
    Adds NSOpenPanel to allow user to open a .vpx file if the double-click the app icon. Note that this opens the table with -DisableTrueFullscreen flag

--- standalone/macos-arm64/main.mm
+++ standalone/macos-arm64/main.mm
@@ -1,57 +1,48 @@
 #import <Cocoa/Cocoa.h>
-#import <Foundation/Foundation.h>
 
 extern "C" char **g_argv;
 extern "C" int g_argc;
 extern "C" int WinMain(void*, void*, void*, int);
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
+- (void)makeWindowExit;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
-   if (g_argc == 1) {
-      NSAlert* alert = [[NSAlert alloc] init];
-      [alert setMessageText:@"VPinballX_GL\n\nThis app must be run using command line arguments or double clicking a \".vpx\" file."];
-      [alert addButtonWithTitle:@"OK"];
-      [alert setAlertStyle:NSAlertStyleWarning];
-      [alert runModal];
-   }
+    if (g_argc == 1) {
+        NSOpenPanel *panel = [NSOpenPanel openPanel];
+        panel.message = @"Select a Visual Pinball Table File:";
+        panel.allowsMultipleSelection = NO;
+        panel.canChooseDirectories = NO;
 
-   int status = WinMain(NULL, NULL, NULL, 0);
-   exit(status);
-}
+        [panel beginWithCompletionHandler:^(NSInteger result) {
+            if (result == NSModalResponseOK) {
+                NSURL *theDoc = panel.URLs[0];
+                NSString *filename = [NSString stringWithUTF8String:[theDoc fileSystemRepresentation]];
 
-- (BOOL)application:(NSApplication *)sender openFile:(NSString *)filename
-{
-   if (g_argc == 1 && (filename && filename.length > 0 && [[filename.lowercaseString pathExtension] isEqualToString:@"vpx"])) {
-      char **new_argv = (char**)malloc(3 * sizeof(char *));
+                if (filename && filename.length > 0 && [[filename.lowercaseString pathExtension] isEqualToString:@"vpx"]) {
+                    char **new_argv = (char **)malloc(4 * sizeof(char *));
+                    new_argv[0] = g_argv[0];
+                    new_argv[1] = strdup("-DisableTrueFullscreen");
+                    new_argv[2] = strdup("-play");
+                    new_argv[3] = strdup([filename UTF8String]);
 
-      new_argv[0] = g_argv[0];
-      new_argv[1] = strdup("-play");
-      new_argv[2] = strdup([filename UTF8String]);
-
-      g_argc = 3;
-      g_argv = new_argv;
+                    g_argc = 4;
+                    g_argv = new_argv;
+                }
+            } else {
+                NSAlert *alert = [[NSAlert alloc] init];
+                [alert setMessageText:@"VPinballX_GL\n\nThis app must be run using command line arguments or double clicking a \".vpx\" file."];
+                [alert addButtonWithTitle:@"OK"];
+                [alert setAlertStyle:NSAlertStyleWarning];
+                [alert runModal];
+            }
+            [self makeWindowExit];
+        }];
     }
-
-    return YES;
 }
 
 @end
-
-int main(int argc, const char * argv[])
-{
-   @autoreleasepool {
-      g_argc = argc;
-      g_argv = (char**)argv;
-
-      [NSApplication sharedApplication];
-      AppDelegate *delegate = [[AppDelegate alloc] init];
-      [NSApp setDelegate:delegate];
-      [NSApp run];
-   }
-   return 0;
-}
