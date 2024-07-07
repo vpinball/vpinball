33efcfbc3a76e92513efc3caed8ed212010bf15b
Vincent Bousquet <vincent.bousqt@gmail.com>
2024-07-07 11:10:54 +0200

    BGFX: better texture update handling

--- src/renderer/Sampler.h
+++ src/renderer/Sampler.h
@@ -94,7 +94,7 @@ private:
    bgfx::TextureHandle m_nomipsTexture = BGFX_INVALID_HANDLE; // The texture without any mipmaps
    bgfx::FrameBufferHandle m_mipsFramebuffer = BGFX_INVALID_HANDLE; // The framebuffer and texture where mipmaps are being generated
    bgfx::TextureHandle m_mipsTexture = BGFX_INVALID_HANDLE;
-   const bgfx::Memory* m_textureUpdate = nullptr;
+   std::atomic<const bgfx::Memory*> m_textureUpdate[16] = { nullptr };
    uint32_t m_mips_gpu_frame = 0;
    uintptr_t m_texture_override = 0;
 #elif defined(ENABLE_OPENGL)
