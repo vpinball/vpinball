b6ab62c498cc8cdf8f360efde439a46a8f9e0bc8
toxieainc <toxie@ainc.de>
2024-07-29 10:12:02 +0200

    fix standalone builds

--- src/core/def.h
+++ src/core/def.h
@@ -775,7 +775,7 @@ string color_to_hex(OLE_COLOR color);
 bool string_contains_case_insensitive(const string& str1, const string& str2);
 bool string_compare_case_insensitive(const string& str1, const string& str2);
 bool string_starts_with_case_insensitive(const std::string& str, const std::string& prefix);
-string string_to_lower(const string& str);
+string string_to_lower(string str);
 string string_replace_all(const string& szStr, const string& szFrom, const string& szTo);
 string create_hex_dump(const UINT8* buffer, size_t size);
 vector<unsigned char> base64_decode(const string &encoded_string);
