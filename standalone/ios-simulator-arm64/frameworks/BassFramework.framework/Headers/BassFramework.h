7e67086395cecb91b1269e3fd63f0df84283b757
Jason Millard <jsm174@gmail.com>
2024-09-27 14:46:44 -0400

    standalone: add support for building iOS frameworks and dSYM (#1925)

---
+++ standalone/ios-simulator-arm64/frameworks/BassFramework.framework/Headers/BassFramework.h
@@ -0,0 +1,17 @@
+//
+//  BassFramework.h
+//  BassFramework
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BassFramework.
+FOUNDATION_EXPORT double BassFrameworkVersionNumber;
+
+//! Project version string for BassFramework.
+FOUNDATION_EXPORT const unsigned char BassFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BassFramework/PublicHeader.h>
+
+
