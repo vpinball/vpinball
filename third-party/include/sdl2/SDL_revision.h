6df0b45c287d7648dbbccd7de37bedbb5314ee7c
toxieainc <toxie@ainc.de>
2024-11-03 22:44:23 +0100

    update to latest

--- third-party/include/sdl2/SDL_revision.h
+++ third-party/include/sdl2/SDL_revision.h
@@ -1,6 +1,6 @@
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.30.8-0 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-release-2.30.9-0 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-release-2.30.8-0"
+#define SDL_REVISION "SDL-release-2.30.9-0"
 #endif
 #define SDL_REVISION_NUMBER 0
