51aac56d064615651b632e8dfde63479c66ce78b
Francis De Brabandere <francisdb@gmail.com>
2025-05-07 02:45:00 +0200

    standalone: disable triggers for PUP_SCREEN_MODE_OFF, limit media player queue size

--- standalone/inc/pup/PUPScreen.cpp
+++ standalone/inc/pup/PUPScreen.cpp
@@ -507,7 +507,7 @@ void PUPScreen::ProcessQueue()
       std::unique_lock<std::mutex> lock(m_queueMutex);
       m_queueCondVar.wait(lock, [this] { return !m_queue.empty() || !m_isRunning; });
 
-      if (!m_isRunning) {
+      if (!m_isRunning || m_mode == PUP_SCREEN_MODE_OFF) {
          while (!m_queue.empty()) {
             PUPScreenRequest* pRequest = m_queue.front();
             m_queue.pop();
