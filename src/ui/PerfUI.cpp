0fb3792cbcbac1e182984ac81ede7269d5fb8377
Vincent Bousquet <vincent.bousqt@gmail.com>
2025-07-03 22:44:34 +0200

    PerfUI: fix plot crash when window is hidden

--- src/ui/PerfUI.cpp
+++ src/ui/PerfUI.cpp
@@ -8,6 +8,89 @@
 #include "imgui/imgui_internal.h"
 #include "implot/implot.h"
 
+
+PerfUI::PlotData::PlotData()
+   : m_maxSize(500)
+{
+   m_data.reserve(m_maxSize);
+   SetRolling(!m_rolling);
+}
+
+void PerfUI::PlotData::SetRolling(bool rolling)
+{
+   if (rolling == m_rolling)
+      return;
+   if (!m_data.empty())
+      m_data.shrink(0);
+   m_offset = 0;
+   m_rolling = rolling;
+}
+
+void PerfUI::PlotData::AddPoint(const float x, const float y)
+{
+   if (std::isinf(y))
+      return;
+
+   if (m_rolling)
+   {
+      const float xmod = fmodf(x, m_timeSpan);
+      if (!m_data.empty() && m_data.back().x == xmod)
+         return;
+
+      if (y > m_movingMax)
+         m_movingMax = y;
+      else
+         m_movingMax = lerp(m_movingMax, y, 0.01f);
+
+      if (!m_data.empty() && xmod < m_data.back().x)
+         m_data.shrink(0);
+
+      m_data.push_back(ImVec2(xmod, y));
+   }
+   else
+   {
+      if (!m_data.empty() && m_data.back().x == x)
+         return;
+
+      if (y > m_movingMax)
+         m_movingMax = y;
+      else
+         m_movingMax = lerp(m_movingMax, y, 0.01f);
+
+      if (m_data.size() < m_maxSize)
+         m_data.push_back(ImVec2(x, y));
+      else
+      {
+         m_data[m_offset] = ImVec2(x, y);
+         m_offset++;
+         if (m_offset == m_maxSize)
+            m_offset = 0;
+      }
+   }
+}
+
+bool PerfUI::PlotData::HasData() const
+{
+   return !m_data.empty();
+}
+
+ImVec2 PerfUI::PlotData::GetLast() const
+{
+   if (m_data.empty())
+      return ImVec2 { 0.f, 0.f };
+   else if (m_data.size() < m_maxSize || m_offset == 0)
+      return m_data.back();
+   else
+      return m_data[m_offset - 1];
+}
+
+float PerfUI::PlotData::GetMovingMax() const
+{
+   return m_movingMax;
+}
+
+
+
 PerfUI::PerfUI(Player *const player)
    : m_player(player)
 {
--- src/ui/PerfUI.h
+++ src/ui/PerfUI.h
@@ -34,61 +34,13 @@ private:
    class PlotData
    {
    public:
-      PlotData() : m_maxSize(500)
-      {
-         m_data.reserve(m_maxSize);
-         SetRolling(!m_rolling);
-      }
-      void SetRolling(bool rolling)
-      {
-         if (rolling == m_rolling)
-            return;
-         if (!m_data.empty())
-            m_data.shrink(0);
-         m_offset = 0;
-         m_rolling = rolling;
-      }
-      void AddPoint(const float x, const float y)
-      {
-         if (!m_data.empty() && m_data.back().x == x)
-            return;
-         if (std::isinf(y))
-            return;
-         if (y > m_movingMax)
-            m_movingMax = y;
-         else
-            m_movingMax = lerp(m_movingMax, y, 0.01f);
-         if (m_rolling)
-         {
-            const float xmod = fmodf(x, m_timeSpan);
-            if (!m_data.empty() && xmod < m_data.back().x)
-               m_data.shrink(0);
-            m_data.push_back(ImVec2(xmod, y));
-         }
-         else
-         {
-            if (m_data.size() < m_maxSize)
-               m_data.push_back(ImVec2(x, y));
-            else
-            {
-               m_data[m_offset] = ImVec2(x, y);
-               m_offset++;
-               if (m_offset == m_maxSize)
-                  m_offset = 0;
-            }
-         }
-      }
-      bool HasData() const { return !m_data.empty(); }
-      ImVec2 GetLast() const
-      {
-         if (m_data.empty())
-            return ImVec2 { 0.f, 0.f };
-         else if (m_data.size() < m_maxSize || m_offset == 0)
-            return m_data.back();
-         else
-            return m_data[m_offset - 1];
-      }
-      float GetMovingMax() const { return m_movingMax; }
+      PlotData();
+      
+      void SetRolling(bool rolling);
+      void AddPoint(const float x, const float y);
+      bool HasData() const;
+      ImVec2 GetLast() const;
+      float GetMovingMax() const;
 
    public:
       int m_offset = 0;
@@ -100,5 +52,6 @@ private:
    private:
       const int m_maxSize;
    };
+
    PlotData m_plotFPS, m_plotFPSSmoothed, m_plotPhysx, m_plotPhysxSmoothed, m_plotScript, m_plotScriptSmoothed;
 };
