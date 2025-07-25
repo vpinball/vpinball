3613c8d1dfe7b9fa2d48c9b7290a2c6e224a7be3
Vincent Bousquet <vincent.bousqt@gmail.com>
2025-07-25 13:54:09 +0200

    Sound: cleaner fix
    Fix https://github.com/vpinball/vpinball/issues/2606

--- src/audio/AudioPlayer.h
+++ src/audio/AudioPlayer.h
@@ -12,6 +12,45 @@ struct ma_device_ex;
 namespace VPX
 {
 
+// Playfield sound playback mode
+// =============================
+// This option select how the playfield table sounds are rendered. While default backglass audio and streamed audio (VPinMAME, 
+// AltSound, PuP, FlexDMD, ...) always plays from the front channels of the selected audio device (which may be a
+// different device or not)
+enum SoundConfigTypes : int
+{
+   // 2CH: Render playfield sounds as 2 channels to the front channel of the selected audio device (which should be a different 
+   // one than the audio device playing backglass audio)
+   SNDCFG_SND3D2CH = 0,
+
+   // ALLREAR: Render playfield sounds as 2 channels to the rear channel of the audio card
+   // This can replace the need to use two sound cards to move table audio inside the cab.
+   SNDCFG_SND3DALLREAR = 1,
+
+   // FRONTISREAR: Render playfield sounds as up to 6 channels to the front/side/rear channels of the audio card (depending 
+   // on the audio card type), with the rear channels near the front of the cab (where the player stands).
+   // Table effects are mapped such that the front of the cab is the rear surround channels. If you were to play
+   // VPX in a home theater system with the TV in front of you, this would produce an appropriate result with the ball coming
+   // from the rear channels as it gets closer to you.
+   SNDCFG_SND3DFRONTISREAR = 2,
+
+   // FRONTISFRONT: Render playfield sounds as up to 6 channels to the front/side/rear channels of the audio card (depending 
+   // on the audio card type), with the front channels near the front of the cab (where the player stands).
+   // Recommended mapping for a dedicated sound card attached to the playfield. Front channel maps to the front
+   // of the cab. We "flip" the rear to the standard 2 channels, so older versions of VP still play sounds on the front most
+   // channels of the cab. This mapping could also be used to place 6 channels on the playfield.
+   SNDCFG_SND3DFRONTISFRONT = 3,
+
+   // 6CH: Render playfield sounds to 4 channels: side (back of the cab) & rear (front of the cab) channels of the audio 
+   // card, leaving the front channels for backglass audio (similar to SNDCFG_SND3DFRONTISFRONT but leaving front channels empty)
+   // Rear of playfield shifted to the sides, and front of playfield shifted to the far rear. Leaves front channels open
+   // for default backglass and VPinMAME.
+   SNDCFG_SND3D6CH = 4,
+
+   // SSF: Same as 6CH but with a different sound horizontal panning and vertical fading are enhanced for a more realistic experience.
+   SNDCFG_SND3DSSF = 5
+};
+
 struct SoundSpec
 {
    float lengthInSeconds;
--- src/parts/Sound.h
+++ src/parts/Sound.h
@@ -14,56 +14,25 @@ enum SoundOutTypes : char
    SNDOUT_BACKGLASS = 1
 };
 
-// Playfield sound playback mode
-// =============================
-// This option select how the playfield table sounds are rendered. While default backglass audio and streamed audio (VPinMAME, 
-// AltSound, PuP, FlexDMD, ...) always plays from the front channels of the selected audio device (which may be a
-// different device or not)
-enum SoundConfigTypes : int
-{
-   // 2CH: Render playfield sounds as 2 channels to the front channel of the selected audio device (which should be a different 
-   // one than the audio device playing backglass audio)
-   SNDCFG_SND3D2CH = 0,
-
-   // ALLREAR: Render playfield sounds as 2 channels to the rear channel of the audio card
-   // This can replace the need to use two sound cards to move table audio inside the cab.
-   SNDCFG_SND3DALLREAR = 1,
-
-   // FRONTISREAR: Render playfield sounds as up to 6 channels to the front/side/rear channels of the audio card (depending 
-   // on the audio card type), with the rear channels near the front of the cab (where the player stands).
-   // Table effects are mapped such that the front of the cab is the rear surround channels. If you were to play
-   // VPX in a home theater system with the TV in front of you, this would produce an appropriate result with the ball coming
-   // from the rear channels as it gets closer to you.
-   SNDCFG_SND3DFRONTISREAR = 2,
-
-   // FRONTISFRONT: Render playfield sounds as up to 6 channels to the front/side/rear channels of the audio card (depending 
-   // on the audio card type), with the front channels near the front of the cab (where the player stands).
-   // Recommended mapping for a dedicated sound card attached to the playfield. Front channel maps to the front
-   // of the cab. We "flip" the rear to the standard 2 channels, so older versions of VP still play sounds on the front most
-   // channels of the cab. This mapping could also be used to place 6 channels on the playfield.
-   SNDCFG_SND3DFRONTISFRONT = 3,
-
-   // 6CH: Render playfield sounds to 4 channels: side (back of the cab) & rear (front of the cab) channels of the audio 
-   // card, leaving the front channels for backglass audio (similar to SNDCFG_SND3DFRONTISFRONT but leaving front channels empty)
-   // Rear of playfield shifted to the sides, and front of playfield shifted to the far rear. Leaves front channels open
-   // for default backglass and VPinMAME.
-   SNDCFG_SND3D6CH = 4,
-
-   // SSF: Same as 6CH but with a different sound horizontal panning and vertical fading are enhanced for a more realistic experience.
-   SNDCFG_SND3DSSF = 5
-};
-
 class Sound final
 {
 public:
-   ~Sound();
-
    static Sound* CreateFromFile(const string& filename);
    static Sound* CreateFromStream(IStream* pstm, const int LoadFileVersion);
 
-   size_t GetFileSize() const { return m_cdata; }
-   const uint8_t* GetFileRaw() const { return m_pdata; }
-   void SetFromFileData(const string& filename, uint8_t* data, size_t size);
+   Sound(string name, string path, vector<uint8_t> data)
+      : m_name(std::move(name))
+      , m_path(std::move(path))
+      , m_data(std::move(data))
+   {
+   }
+
+   const string& GetName() const { return m_name; }
+   void SetName(string name) { m_name = name; }
+   const string& GetImportPath() const { return m_path; }
+   size_t GetFileSize() const { return m_data.size(); }
+   const uint8_t* GetFileRaw() const { return m_data.data(); }
+   void SetFromFileData(std::string_view filename, vector<uint8_t> filedata);
 
    bool SaveToFile(const string& filename) const;
    void SaveToStream(IStream* pstm) const;
@@ -72,7 +41,7 @@ public:
    SoundOutTypes GetOutputTarget() const { return m_outputTarget; }
    void SetOutputTarget(SoundOutTypes target)
    {
-      assert(SoundOutTypes::SNDOUT_TABLE <= target && target <= SoundOutTypes::SNDOUT_BACKGLASS);
+      assert(target == SoundOutTypes::SNDOUT_TABLE || target == SoundOutTypes::SNDOUT_BACKGLASS);
       m_outputTarget = target;
    }
 
@@ -88,31 +57,16 @@ public:
    int GetFrontRearFade() const { return m_frontRearFade; }
    void SetFrontRearFade(const int front_rear_fade) { m_frontRearFade = front_rear_fade; }
 
-   // Sound filename and path
-   string m_name;
-   string m_path;
-
 private:
-   Sound(string name, string path, uint8_t* data, int dataSize)
-      : m_name(std::move(name))
-      , m_path(std::move(path))
-      , m_pdata(data)
-      , m_cdata(dataSize)
-   {
-   }
+   static bool isWav(const string& path) { return path_has_extension(path, "wav"s); }
 
-   // Undecoded data from original file
-   uint8_t* m_pdata = nullptr;
-   size_t m_cdata = 0;
-
-   // at the moment, can only be set in the Sound Resource Manager, stored with the table
+   string m_name;
+   string m_path; // Original file import path
+   vector<uint8_t> m_data; // Undecoded data from original file
    int m_volume = 0;
    int m_pan = 0;
    int m_frontRearFade = 0;
-
    SoundOutTypes m_outputTarget = SoundOutTypes::SNDOUT_BACKGLASS; //Is it table sound device or BG sound device.
-
-   static bool isWav(const string& path) { return path_has_extension(path, "wav"s); }
 };
 
 }
