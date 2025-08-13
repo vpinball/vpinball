360cf8483873dfb1da6a310b27f5376b6284d5f4
Jason Millard <jsm174@gmail.com>
2025-08-13 07:35:23 -0400

    plugins: add AltSound plugin and remove last references of BASS

---
+++ plugins/altsound/common.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "MsgPlugin.h"
+#include "LoggingPlugin.h" 
+#include "ScriptablePlugin.h"
+#include "VPXPlugin.h"
+#include "ControllerPlugin.h"
+
+#include <string>
+#include <memory>
+#include <cstdarg>
+
+using std::string;
+
+#ifdef _WIN32
+#define PATH_SEPARATOR_CHAR '\\'
+#else
+#define PATH_SEPARATOR_CHAR '/'
+#endif
+
+namespace AltSound {
+
+string normalize_path_separators(const string& szPath);
+string find_case_insensitive_file_path(const string& szPath);
+bool StrCompareNoCase(const string& strA, const string& strB);
+
+// LPI_USE(); // Remove this since LPI_IMPLEMENT in .cpp provides the implementation
+#ifndef LOGD
+#define LOGD(...) LPI_LOGD(__VA_ARGS__)
+#define LOGI(...) LPI_LOGI(__VA_ARGS__)
+#define LOGW(...) LPI_LOGW(__VA_ARGS__)
+#define LOGE(...) LPI_LOGE(__VA_ARGS__)
+#endif
+
+PSC_USE_ERROR();
+
+}
