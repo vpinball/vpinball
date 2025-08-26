8f960b6ae9676529d54059a32080995a558e57df
Jason Millard <jsm174@gmail.com>
2025-08-25 21:39:09 -0400

    plugins: update B2SLegacy to register server object like B2S plugin

--- plugins/b2slegacy/B2SLegacyPlugin.cpp
+++ plugins/b2slegacy/B2SLegacyPlugin.cpp
@@ -11,28 +11,32 @@
 #include "classes/B2SScreen.h"
 #include "utils/PinMAMEAPI.h"
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// Plugin interface
+namespace B2SLegacy {
 
-static MsgPluginAPI* msgApi = nullptr;
-static VPXPluginAPI* vpxApi = nullptr;
-static ScriptablePluginAPI* scriptApi = nullptr;
-static uint32_t endpointId = 0;
-
-static unsigned int onGetAuxRendererId = 0;
-static unsigned int onAuxRendererChgId = 0;
-
-static ScriptClassDef* pinmameClassDef = nullptr;
-static int pinmameMemberStartIndex = 0;
-
-static unsigned int getDevSrcMsgId = 0;
-static unsigned int onDevChangedMsgId = 0;
+LPI_IMPLEMENT
 
 int OnRender(VPXRenderContext2D* const renderCtx, void* context);
 void OnGetRenderer(const unsigned int msgId, void* context, void* msgData);
 void OnDevSrcChanged(const unsigned int msgId, void* userData, void* msgData);
 
-namespace B2SLegacy {
+///////////////////////////////////////////////////////////////////////////////
+// B2S Legacy plugin
+
+static MsgPluginAPI* msgApi = nullptr;
+static VPXPluginAPI* vpxApi = nullptr;
+static ScriptablePluginAPI* scriptApi = nullptr;
+static uint32_t endpointId = 0;
+static unsigned int onGetAuxRendererId = 0;
+static unsigned int onAuxRendererChgId = 0;
+static unsigned int getDevSrcMsgId = 0;
+static unsigned int onDevChangedMsgId = 0;
+
+///////////////////////////////////////////////////////////////////////////////
+// Script interface
+//
+
+static ScriptClassDef* pinmameClassDef = nullptr;
+static int pinmameMemberStartIndex = 0;
 
 void MSGPIAPI ForwardPinMAMECall(void* me, int memberIndex, ScriptVariant* pArgs, ScriptVariant* pRet)
 {
@@ -40,29 +44,102 @@ void MSGPIAPI ForwardPinMAMECall(void* me, int memberIndex, ScriptVariant* pArgs
    if (!pinmameClassDef)
       return;
 
-   B2SLegacy::Server* server = static_cast<B2SLegacy::Server*>(me);
+   Server* server = static_cast<Server*>(me);
    if (!server)
       return;
 
-   B2SLegacy::PinMAMEAPI* pinmameApi = server->GetPinMAMEApi();
+   PinMAMEAPI* pinmameApi = server->GetPinMAMEApi();
    if (!pinmameApi)
-      pinmameApi = new B2SLegacy::PinMAMEAPI(server, pinmameClassDef);
+      pinmameApi = new PinMAMEAPI(server, pinmameClassDef);
    if (pinmameApi)
       pinmameApi->HandleCall(memberIndex, pinmameMemberStartIndex, pArgs, pRet);
 }
 
-PSC_ERROR_IMPLEMENT(scriptApi);
+PSC_CLASS_START(Server)
+   PSC_FUNCTION0(Server, void, Dispose)
+   PSC_PROP_R(Server, string, B2SServerVersion)
+   PSC_PROP_R(Server, double, B2SBuildVersion)
+   PSC_PROP_R(Server, string, B2SServerDirectory)
+   PSC_PROP_RW(Server, string, B2SName)
+   PSC_PROP_RW(Server, string, TableName)
+   PSC_PROP_W(Server, string, WorkingDir)
+   PSC_FUNCTION1(Server, void, SetPath, string)
+   PSC_PROP_RW(Server, bool, LaunchBackglass)
+   PSC_PROP_RW(Server, bool, PuPHide)
+   PSC_FUNCTION2(Server, void, B2SSetData, int, int)
+   PSC_FUNCTION2(Server, void, B2SSetData, string, int)
+   PSC_FUNCTION1(Server, void, B2SPulseData, int)
+   PSC_FUNCTION1(Server, void, B2SPulseData, string)
+   PSC_FUNCTION3(Server, void, B2SSetPos, int, int, int)
+   PSC_FUNCTION3(Server, void, B2SSetPos, string, int, int)
+   PSC_FUNCTION2(Server, void, B2SSetIllumination, string, int)
+   PSC_FUNCTION2(Server, void, B2SSetLED, int, int)
+   PSC_FUNCTION2(Server, void, B2SSetLED, int, string)
+   PSC_FUNCTION2(Server, void, B2SSetLEDDisplay, int, string)
+   PSC_FUNCTION2(Server, void, B2SSetReel, int, int)
+   PSC_FUNCTION2(Server, void, B2SSetScore, int, int)
+   PSC_FUNCTION2(Server, void, B2SSetScorePlayer, int, int)
+   PSC_FUNCTION1(Server, void, B2SSetScorePlayer1, int)
+   PSC_FUNCTION1(Server, void, B2SSetScorePlayer2, int)
+   PSC_FUNCTION1(Server, void, B2SSetScorePlayer3, int)
+   PSC_FUNCTION1(Server, void, B2SSetScorePlayer4, int)
+   PSC_FUNCTION1(Server, void, B2SSetScorePlayer5, int)
+   PSC_FUNCTION1(Server, void, B2SSetScorePlayer6, int)
+   PSC_FUNCTION2(Server, void, B2SSetScoreDigit, int, int)
+   PSC_FUNCTION2(Server, void, B2SSetScoreRollover, int, int)
+   PSC_FUNCTION1(Server, void, B2SSetScoreRolloverPlayer1, int)
+   PSC_FUNCTION1(Server, void, B2SSetScoreRolloverPlayer2, int)
+   PSC_FUNCTION1(Server, void, B2SSetScoreRolloverPlayer3, int)
+   PSC_FUNCTION1(Server, void, B2SSetScoreRolloverPlayer4, int)
+   PSC_FUNCTION1(Server, void, B2SSetCredits, int)
+   PSC_FUNCTION2(Server, void, B2SSetCredits, int, int)
+   PSC_FUNCTION1(Server, void, B2SSetPlayerUp, int)
+   PSC_FUNCTION2(Server, void, B2SSetPlayerUp, int, int)
+   PSC_FUNCTION1(Server, void, B2SSetCanPlay, int)
+   PSC_FUNCTION2(Server, void, B2SSetCanPlay, int, int)
+   PSC_FUNCTION1(Server, void, B2SSetBallInPlay, int)
+   PSC_FUNCTION2(Server, void, B2SSetBallInPlay, int, int)
+   PSC_FUNCTION1(Server, void, B2SSetTilt, int)
+   PSC_FUNCTION2(Server, void, B2SSetTilt, int, int)
+   PSC_FUNCTION1(Server, void, B2SSetMatch, int)
+   PSC_FUNCTION2(Server, void, B2SSetMatch, int, int)
+   PSC_FUNCTION1(Server, void, B2SSetGameOver, int)
+   PSC_FUNCTION2(Server, void, B2SSetGameOver, int, int)
+   PSC_FUNCTION1(Server, void, B2SSetShootAgain, int)
+   PSC_FUNCTION2(Server, void, B2SSetShootAgain, int, int)
+   PSC_FUNCTION1(Server, void, B2SStartAnimation, string)
+   PSC_FUNCTION2(Server, void, B2SStartAnimation, string, bool)
+   PSC_FUNCTION1(Server, void, B2SStartAnimationReverse, string)
+   PSC_FUNCTION1(Server, void, B2SStopAnimation, string)
+   PSC_FUNCTION0(Server, void, B2SStopAllAnimations)
+   PSC_FUNCTION1(Server, bool, B2SIsAnimationRunning, string)
+   PSC_FUNCTION1(Server, void, StartAnimation, string)
+   PSC_FUNCTION2(Server, void, StartAnimation, string, bool)
+   PSC_FUNCTION1(Server, void, StopAnimation, string)
+   PSC_FUNCTION0(Server, void, B2SStartRotation)
+   PSC_FUNCTION0(Server, void, B2SStopRotation)
+   PSC_FUNCTION0(Server, void, B2SShowScoreDisplays)
+   PSC_FUNCTION0(Server, void, B2SHideScoreDisplays)
+   PSC_FUNCTION1(Server, void, B2SStartSound, string)
+   PSC_FUNCTION1(Server, void, B2SPlaySound, string)
+   PSC_FUNCTION1(Server, void, B2SStopSound, string)
+   PSC_FUNCTION2(Server, void, B2SMapSound, int, string)
 
-LPI_IMPLEMENT
-
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// Renderer implementations
+   if (pinmameClassDef)
+   {
+      pinmameMemberStartIndex = static_cast<int>(members.size());
+      for (unsigned int i = 0; i < pinmameClassDef->nMembers; i++)
+      {
+         ScriptClassMemberDef member = pinmameClassDef->members[i];
+         member.Call = ForwardPinMAMECall;
+         members.push_back(member);
+      }
+   }
+PSC_CLASS_END(Server)
 
 int OnRender(VPXRenderContext2D* const renderCtx, void* context)
 {
-   B2SLegacy::Server* server = static_cast<B2SLegacy::Server*>(context);
+   Server* server = static_cast<Server*>(context);
    return server ? server->OnRender(renderCtx, context) : 0;
 }
 
@@ -87,107 +164,41 @@ void OnGetRenderer(const unsigned int msgId, void* context, void* msgData)
 
 void OnDevSrcChanged(const unsigned int msgId, void* userData, void* msgData)
 {
-   B2SLegacy::Server* server = static_cast<B2SLegacy::Server*>(userData);
+   Server* server = static_cast<Server*>(userData);
    if (server)
       server->OnDevSrcChanged(msgId, userData, msgData);
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// Script interface
+void RegisterServerObject(void*)
+{
+   pinmameClassDef = scriptApi->GetClassDef("Controller");
+   if (pinmameClassDef) {
+      auto regLambda = [&](ScriptClassDef* scd) { scriptApi->RegisterScriptClass(scd); };
+      RegisterServerSCD(regLambda);
+      Server_SCD->CreateObject = []()
+      {
+         auto server = new Server(msgApi, endpointId, vpxApi);
+         msgApi->SubscribeMsg(endpointId, onGetAuxRendererId = msgApi->GetMsgID(VPXPI_NAMESPACE, VPXPI_MSG_GET_AUX_RENDERER), OnGetRenderer, server);
+         msgApi->SubscribeMsg(endpointId, onDevChangedMsgId, OnDevSrcChanged, server);
+         return static_cast<void*>(server);
+      };
+      scriptApi->SubmitTypeLibrary();
+      scriptApi->SetCOMObjectOverride("B2S.Server", Server_SCD);
+   }
+   else
+      msgApi->RunOnMainThread(0.1, RegisterServerObject, nullptr);
+}
+
+}
 
 using namespace B2SLegacy;
 
-PSC_CLASS_START(Server)
-      PSC_FUNCTION0(Server, void, Dispose)
-      PSC_PROP_R(Server, string, B2SServerVersion)
-      PSC_PROP_R(Server, double, B2SBuildVersion)
-      PSC_PROP_R(Server, string, B2SServerDirectory)
-      PSC_PROP_RW(Server, string, B2SName)
-      PSC_PROP_RW(Server, string, TableName)
-      PSC_PROP_W(Server, string, WorkingDir)
-      PSC_FUNCTION1(Server, void, SetPath, string)
-      PSC_PROP_RW(Server, bool, LaunchBackglass)
-      PSC_PROP_RW(Server, bool, PuPHide)
-      PSC_FUNCTION2(Server, void, B2SSetData, int, int)
-      PSC_FUNCTION2(Server, void, B2SSetData, string, int)
-      PSC_FUNCTION1(Server, void, B2SPulseData, int)
-      PSC_FUNCTION1(Server, void, B2SPulseData, string)
-      PSC_FUNCTION3(Server, void, B2SSetPos, int, int, int)
-      PSC_FUNCTION3(Server, void, B2SSetPos, string, int, int)
-      PSC_FUNCTION2(Server, void, B2SSetIllumination, string, int)
-      PSC_FUNCTION2(Server, void, B2SSetLED, int, int)
-      PSC_FUNCTION2(Server, void, B2SSetLED, int, string)
-      PSC_FUNCTION2(Server, void, B2SSetLEDDisplay, int, string)
-      PSC_FUNCTION2(Server, void, B2SSetReel, int, int)
-      PSC_FUNCTION2(Server, void, B2SSetScore, int, int)
-      PSC_FUNCTION2(Server, void, B2SSetScorePlayer, int, int)
-      PSC_FUNCTION1(Server, void, B2SSetScorePlayer1, int)
-      PSC_FUNCTION1(Server, void, B2SSetScorePlayer2, int)
-      PSC_FUNCTION1(Server, void, B2SSetScorePlayer3, int)
-      PSC_FUNCTION1(Server, void, B2SSetScorePlayer4, int)
-      PSC_FUNCTION1(Server, void, B2SSetScorePlayer5, int)
-      PSC_FUNCTION1(Server, void, B2SSetScorePlayer6, int)
-      PSC_FUNCTION2(Server, void, B2SSetScoreDigit, int, int)
-      PSC_FUNCTION2(Server, void, B2SSetScoreRollover, int, int)
-      PSC_FUNCTION1(Server, void, B2SSetScoreRolloverPlayer1, int)
-      PSC_FUNCTION1(Server, void, B2SSetScoreRolloverPlayer2, int)
-      PSC_FUNCTION1(Server, void, B2SSetScoreRolloverPlayer3, int)
-      PSC_FUNCTION1(Server, void, B2SSetScoreRolloverPlayer4, int)
-      PSC_FUNCTION1(Server, void, B2SSetCredits, int)
-      PSC_FUNCTION2(Server, void, B2SSetCredits, int, int)
-      PSC_FUNCTION1(Server, void, B2SSetPlayerUp, int)
-      PSC_FUNCTION2(Server, void, B2SSetPlayerUp, int, int)
-      PSC_FUNCTION1(Server, void, B2SSetCanPlay, int)
-      PSC_FUNCTION2(Server, void, B2SSetCanPlay, int, int)
-      PSC_FUNCTION1(Server, void, B2SSetBallInPlay, int)
-      PSC_FUNCTION2(Server, void, B2SSetBallInPlay, int, int)
-      PSC_FUNCTION1(Server, void, B2SSetTilt, int)
-      PSC_FUNCTION2(Server, void, B2SSetTilt, int, int)
-      PSC_FUNCTION1(Server, void, B2SSetMatch, int)
-      PSC_FUNCTION2(Server, void, B2SSetMatch, int, int)
-      PSC_FUNCTION1(Server, void, B2SSetGameOver, int)
-      PSC_FUNCTION2(Server, void, B2SSetGameOver, int, int)
-      PSC_FUNCTION1(Server, void, B2SSetShootAgain, int)
-      PSC_FUNCTION2(Server, void, B2SSetShootAgain, int, int)
-      PSC_FUNCTION1(Server, void, B2SStartAnimation, string)
-      PSC_FUNCTION2(Server, void, B2SStartAnimation, string, bool)
-      PSC_FUNCTION1(Server, void, B2SStartAnimationReverse, string)
-      PSC_FUNCTION1(Server, void, B2SStopAnimation, string)
-      PSC_FUNCTION0(Server, void, B2SStopAllAnimations)
-      PSC_FUNCTION1(Server, bool, B2SIsAnimationRunning, string)
-      PSC_FUNCTION1(Server, void, StartAnimation, string)
-      PSC_FUNCTION2(Server, void, StartAnimation, string, bool)
-      PSC_FUNCTION1(Server, void, StopAnimation, string)
-      PSC_FUNCTION0(Server, void, B2SStartRotation)
-      PSC_FUNCTION0(Server, void, B2SStopRotation)
-      PSC_FUNCTION0(Server, void, B2SShowScoreDisplays)
-      PSC_FUNCTION0(Server, void, B2SHideScoreDisplays)
-      PSC_FUNCTION1(Server, void, B2SStartSound, string)
-      PSC_FUNCTION1(Server, void, B2SPlaySound, string)
-      PSC_FUNCTION1(Server, void, B2SStopSound, string)
-      PSC_FUNCTION2(Server, void, B2SMapSound, int, string)
-
-      if (pinmameClassDef)
-      {
-         pinmameMemberStartIndex = static_cast<int>(members.size());
-         for (unsigned int i = 0; i < pinmameClassDef->nMembers; i++)
-         {
-            ScriptClassMemberDef member = pinmameClassDef->members[i];
-            member.Call = B2SLegacy::ForwardPinMAMECall;
-            members.push_back(member);
-         }
-      }
-PSC_CLASS_END(Server)
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// Plugin lifecycle
-
 MSGPI_EXPORT void MSGPIAPI B2SLegacyPluginLoad(const uint32_t sessionId, MsgPluginAPI* api)
 {
-   endpointId = sessionId;
    msgApi = api;
-
+   endpointId = sessionId;
    LPISetup(endpointId, msgApi);
+
    const unsigned int getVpxApiId = msgApi->GetMsgID(VPXPI_NAMESPACE, VPXPI_MSG_GET_API);
    msgApi->BroadcastMsg(endpointId, getVpxApiId, &vpxApi);
    msgApi->ReleaseMsgID(getVpxApiId);
@@ -201,54 +212,20 @@ MSGPI_EXPORT void MSGPIAPI B2SLegacyPluginLoad(const uint32_t sessionId, MsgPlug
    msgApi->BroadcastMsg(endpointId, getScriptApiId, &scriptApi);
    msgApi->ReleaseMsgID(getScriptApiId);
 
-   if (!scriptApi) {
-      LOGE("Failed to get script API");
-      return;
-   }
-
-   pinmameClassDef = scriptApi->GetClassDef("Controller");
-   if (pinmameClassDef) {
-      auto regLambda = [&](ScriptClassDef* scd) { scriptApi->RegisterScriptClass(scd); };
-      RegisterServerSCD(regLambda);
-      Server_SCD->CreateObject = []()
-      {
-         B2SLegacy::Server* server = new B2SLegacy::Server(msgApi, endpointId, vpxApi);
-
-         msgApi->SubscribeMsg(endpointId, onGetAuxRendererId = msgApi->GetMsgID(VPXPI_NAMESPACE, VPXPI_MSG_GET_AUX_RENDERER), OnGetRenderer, server);
-         msgApi->SubscribeMsg(endpointId, onDevChangedMsgId, OnDevSrcChanged, server);
-
-         return static_cast<void*>(server);
-      };
-      scriptApi->SubmitTypeLibrary();
-      scriptApi->SetCOMObjectOverride("B2S.Server", Server_SCD);
-      LOGI("B2SLegacy Plugin registered with PinMAME Controller proxying");
-   }
-   else {
-      LOGI("B2SLegacy Plugin loaded without PinMAME Controller (Controller class not available)");
-   }
-
-   LOGI("B2SLegacy Plugin loaded successfully");
+   RegisterServerObject(nullptr);
 }
 
 MSGPI_EXPORT void MSGPIAPI B2SLegacyPluginUnload()
 {
-   if (scriptApi)
-      scriptApi->SetCOMObjectOverride("B2S.Server", nullptr);
-
-   if (msgApi) {
-      msgApi->UnsubscribeMsg(onGetAuxRendererId, OnGetRenderer);
-      msgApi->BroadcastMsg(endpointId, onAuxRendererChgId, nullptr);
-      msgApi->ReleaseMsgID(onGetAuxRendererId);
-      msgApi->ReleaseMsgID(onAuxRendererChgId);
-
-      msgApi->UnsubscribeMsg(onDevChangedMsgId, OnDevSrcChanged);
-      msgApi->ReleaseMsgID(onDevChangedMsgId);
-      msgApi->ReleaseMsgID(getDevSrcMsgId);
-   }
-
+   scriptApi->SetCOMObjectOverride("B2S.Server", nullptr);
+   msgApi->UnsubscribeMsg(onGetAuxRendererId, OnGetRenderer);
+   msgApi->BroadcastMsg(endpointId, onAuxRendererChgId, nullptr);
+   msgApi->ReleaseMsgID(onGetAuxRendererId);
+   msgApi->ReleaseMsgID(onAuxRendererChgId);
+   msgApi->UnsubscribeMsg(onDevChangedMsgId, OnDevSrcChanged);
+   msgApi->ReleaseMsgID(onDevChangedMsgId);
+   msgApi->ReleaseMsgID(getDevSrcMsgId);
    vpxApi = nullptr;
    scriptApi = nullptr;
    msgApi = nullptr;
-
-   LOGI("B2SLegacy Plugin unloaded");
 }
