8d33b5d6c308531fa3d0938f6b6dadaaa478a98b
toxieainc <toxie@ainc.de>
2025-09-05 11:11:49 +0200

    update to 3.2.22

--- standalone/android/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ standalone/android/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -64,7 +64,7 @@ public class SDLActivity extends ComponentActivity implements View.OnSystemUiVis
     private static final String TAG = "SDL";
     private static final int SDL_MAJOR_VERSION = 3;
     private static final int SDL_MINOR_VERSION = 2;
-    private static final int SDL_MICRO_VERSION = 20;
+    private static final int SDL_MICRO_VERSION = 22;
 /*
     // Display InputType.SOURCE/CLASS of events and devices
     //
