0b882adaf960469730cc0eae0c72b407c08216e0
Vincent Bousquet <vincent.bousqt@gmail.com>
2025-09-11 23:36:19 +0200

    FlexDMD: use LibAV to lazily load heavy dependencies, adapt MSVC project files

--- plugins/flexdmd/actors/Video.cpp
+++ plugins/flexdmd/actors/Video.cpp
@@ -3,21 +3,16 @@
 namespace Flex {
 
 Video::Video(FlexDMD* pFlexDMD, const string& name)
-    : AnimatedActor(pFlexDMD, name) {}
+   : AnimatedActor(pFlexDMD, name)
+   , m_libAv(LibAV::LibAV::GetInstance())
+{
+   assert(m_libAv.isLoaded);
+}
 
 Video::~Video()
 {
    for (auto* pSurface : m_frames)
       SDL_DestroySurface(pSurface);
-
-   if (m_pVideoConversionContext)
-      sws_freeContext(m_pVideoConversionContext);
-
-   if (m_pCodecContext)
-      avcodec_free_context(&m_pCodecContext);
-
-   if (m_pFormatContext)
-      avformat_close_input(&m_pFormatContext);
 }
 
 Video* Video::Create(FlexDMD* pFlexDMD, AssetManager* pAssetManager, const string& path, const string& name, bool loop)
@@ -26,59 +21,76 @@ Video* Video::Create(FlexDMD* pFlexDMD, AssetManager* pAssetManager, const strin
    Video* pVideo = new Video(pFlexDMD, name);
    pVideo->SetLoop(loop);
 
-   if (avformat_open_input(&pVideo->m_pFormatContext, pSrc->GetPath().c_str(), NULL, NULL) != 0)
+   const LibAV::LibAV& m_libAv = LibAV::LibAV::GetInstance();
+   assert(m_libAv.isLoaded);
+
+   SwsContext* m_pVideoConversionContext = nullptr;
+   AVFormatContext* m_pFormatContext = nullptr;
+   AVCodecContext* m_pCodecContext = nullptr;
+   int m_videoStreamIndex = -1;
+
+   if (m_libAv._avformat_open_input(&m_pFormatContext, pSrc->GetPath().c_str(), nullptr, nullptr) != 0)
       return nullptr;
 
-   avformat_find_stream_info(pVideo->m_pFormatContext, NULL);
-   for (unsigned i = 0; i < pVideo->m_pFormatContext->nb_streams; ++i) {
-      if (pVideo->m_pFormatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
-         pVideo->m_videoStreamIndex = i;
+   m_libAv._avformat_find_stream_info(m_pFormatContext, nullptr);
+   for (unsigned i = 0; i < m_pFormatContext->nb_streams; ++i) {
+      if (m_pFormatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+         m_videoStreamIndex = i;
          break;
       }
    }
 
-   if (pVideo->m_videoStreamIndex < 0)
+   if (m_videoStreamIndex < 0)
       return nullptr;
 
-   AVStream* pStream = pVideo->m_pFormatContext->streams[pVideo->m_videoStreamIndex];
-   pVideo->m_frameDuration = 1.0f / av_q2d(pStream->avg_frame_rate);
-   const AVCodec* codec = avcodec_find_decoder(pStream->codecpar->codec_id);
-   pVideo->m_pCodecContext = avcodec_alloc_context3(codec);
-   avcodec_parameters_to_context(pVideo->m_pCodecContext, pStream->codecpar);
-   avcodec_open2(pVideo->m_pCodecContext, codec, NULL);
+   AVStream* pStream = m_pFormatContext->streams[m_videoStreamIndex];
+   pVideo->m_frameDuration = static_cast<float>(1.0 / av_q2d(pStream->avg_frame_rate));
+   const AVCodec* codec = m_libAv._avcodec_find_decoder(pStream->codecpar->codec_id);
+   m_pCodecContext = m_libAv._avcodec_alloc_context3(codec);
+   m_libAv._avcodec_parameters_to_context(m_pCodecContext, pStream->codecpar);
+   m_libAv._avcodec_open2(m_pCodecContext, codec, nullptr);
 
-   int w = pVideo->m_pCodecContext->width;
-   int h = pVideo->m_pCodecContext->height;
-   AVPacket* pPacket = av_packet_alloc();
-   AVFrame* pFrame = av_frame_alloc();
-   while (av_read_frame(pVideo->m_pFormatContext, pPacket) >= 0) {
-      if (pPacket->stream_index == pVideo->m_videoStreamIndex) {
-         avcodec_send_packet(pVideo->m_pCodecContext, pPacket);
-         while (avcodec_receive_frame(pVideo->m_pCodecContext, pFrame) >= 0) {
-         if (!pVideo->m_pVideoConversionContext)
-            pVideo->m_pVideoConversionContext = sws_getContext(w, h, pVideo->m_pCodecContext->pix_fmt,
-                                              w, h, AV_PIX_FMT_RGBA,
-                                              SWS_FAST_BILINEAR, NULL, NULL, NULL);
+   int w = m_pCodecContext->width;
+   int h = m_pCodecContext->height;
+   AVPacket* pPacket = m_libAv._av_packet_alloc();
+   AVFrame* pFrame = m_libAv._av_frame_alloc();
+   while (m_libAv._av_read_frame(m_pFormatContext, pPacket) >= 0)
+   {
+      if (pPacket->stream_index == m_videoStreamIndex) {
+         m_libAv._avcodec_send_packet(m_pCodecContext, pPacket);
+         while (m_libAv._avcodec_receive_frame(m_pCodecContext, pFrame) >= 0)
+         {
+            if (!m_pVideoConversionContext)
+               m_pVideoConversionContext = m_libAv._sws_getCachedContext(nullptr, w, h, m_pCodecContext->pix_fmt, w, h, AV_PIX_FMT_RGBA, SWS_FAST_BILINEAR, nullptr, nullptr, nullptr);
 
-            int bufSize = av_image_get_buffer_size(AV_PIX_FMT_RGBA, w, h, 1);
-            uint8_t* pBuffer = (uint8_t*)av_malloc(bufSize);
-            uint8_t* dst[4] = { pBuffer, NULL, NULL, NULL };
+            int bufSize = m_libAv._av_image_get_buffer_size(AV_PIX_FMT_RGBA, w, h, 1);
+            uint8_t* pBuffer = (uint8_t*)m_libAv._av_malloc(bufSize);
+            uint8_t* dst[4] = { pBuffer, nullptr, nullptr, nullptr };
             int dstLines[4] = { 4 * w, 0, 0, 0 };
-            sws_scale(pVideo->m_pVideoConversionContext, pFrame->data, pFrame->linesize, 0, h, dst, dstLines);
+            m_libAv._sws_scale(m_pVideoConversionContext, pFrame->data, pFrame->linesize, 0, h, dst, dstLines);
 
             SDL_Surface* pSurface = SDL_CreateSurface(w, h, SDL_PIXELFORMAT_RGBA32);
             SDL_LockSurface(pSurface);
             memcpy(pSurface->pixels, pBuffer, bufSize);
             SDL_UnlockSurface(pSurface);
-            av_free(pBuffer);
+            m_libAv._av_free(pBuffer);
             pVideo->m_frames.push_back(pSurface);
          }
       }
-      av_packet_unref(pPacket);
+      m_libAv._av_packet_unref(pPacket);
     }
 
-    av_frame_free(&pFrame);
-    av_packet_free(&pPacket);
+    m_libAv._av_frame_free(&pFrame);
+    m_libAv._av_packet_free(&pPacket);
+
+    if (m_pVideoConversionContext)
+       m_libAv._sws_freeContext(m_pVideoConversionContext);
+
+    if (m_pCodecContext)
+       m_libAv._avcodec_free_context(&m_pCodecContext);
+
+    if (m_pFormatContext)
+       m_libAv._avformat_close_input(&m_pFormatContext);
 
     pVideo->SetPrefWidth((float)w);
     pVideo->SetPrefHeight((float)h);
