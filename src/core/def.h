af1d2370030c47e8fbba0b44f062cc9ef5811259
Vincent Bousquet <vincent.bousqt@gmail.com>
2025-09-29 16:59:39 +0200

    Fix invalid module path casts

--- src/core/def.h
+++ src/core/def.h
@@ -737,7 +737,7 @@ template <class T> T GetModulePath(HMODULE hModule) // string or wstring
    while (true)
    {
       path.resize(size);
-      const DWORD length = ::GetModuleFileName(hModule, (TCHAR*)path.data(), size);
+      const DWORD length = ::GetModuleFileName(hModule, path.data(), size);
       if (length == 0)
          return {};
       if (length < size)
