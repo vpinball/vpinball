646af4ea34d156029a434562baa7c5bcb16b102b
Vincent Bousquet <vincent.bousqt@gmail.com>
2025-11-08 20:33:43 +0100

    Fix UI mesh uploading on main thread causing latency issues

--- src/ui/live/LiveUI.cpp
+++ src/ui/live/LiveUI.cpp
@@ -574,13 +574,11 @@ void LiveUI::Update()
             vb[i].tv = cmd_list->VtxBuffer[i].uv.y;
          }
          m_meshBuffers[n]->m_vb->Unlock();
-         m_meshBuffers[n]->m_vb->Upload();
 
          uint32_t *ib;
          m_meshBuffers[n]->m_ib->Lock(ib);
          memcpy(ib, cmd_list->IdxBuffer.begin(), numIndices * sizeof(ImDrawIdx));
          m_meshBuffers[n]->m_ib->Unlock();
-         m_meshBuffers[n]->m_ib->Upload();
       }
 
       for (const ImDrawCmd *cmd = cmd_list->CmdBuffer.begin(), *cmdEnd = cmd_list->CmdBuffer.end(); cmd != cmdEnd; cmd++)
