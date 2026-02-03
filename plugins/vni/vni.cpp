084194c608e122369bd70a293fe3234556d4d90b
Jason Millard <jsm174@gmail.com>
2026-02-02 20:08:44 -0500

    plugins: update VNIPlugin to support pin2dmd.[pal|vni] naming

--- plugins/vni/vni.cpp
+++ plugins/vni/vni.cpp
@@ -206,28 +206,47 @@ static void OnControllerGameStart(const unsigned int eventId, void* userData, vo
    const std::string gameId = msg->gameId;
    const std::filesystem::path palFile = gameId + ".pal"s;
    const std::filesystem::path vniFile = gameId + ".vni"s;
+   const std::filesystem::path pin2dmdPal = "pin2dmd.pal"s;
+   const std::filesystem::path pin2dmdVni = "pin2dmd.vni"s;
 
    std::filesystem::path palPath, vniPath;
 
+   // Priority 1: vni/<rom>/<rom>.pal and vni/<rom>/<rom>.vni
    if (auto path1 = find_case_insensitive_file_path(tablePath.parent_path() / "vni" / gameId / palFile); !path1.empty())
    {
       palPath = path1;
       if (auto path2 = find_case_insensitive_file_path(tablePath.parent_path() / "vni" / gameId / vniFile); !path2.empty())
          vniPath = path2;
    }
-   else if (auto path3 = find_case_insensitive_file_path(tablePath.parent_path() / "pinmame" / "altcolor" / gameId / palFile); !path3.empty())
+   // Priority 2: vni/<rom>/pin2dmd.pal and vni/<rom>/pin2dmd.vni
+   else if (auto path3 = find_case_insensitive_file_path(tablePath.parent_path() / "vni" / gameId / pin2dmdPal); !path3.empty())
    {
       palPath = path3;
-      if (auto path4 = find_case_insensitive_file_path(tablePath.parent_path() / "pinmame" / "altcolor" / gameId / vniFile); !path4.empty())
+      if (auto path4 = find_case_insensitive_file_path(tablePath.parent_path() / "vni" / gameId / pin2dmdVni); !path4.empty())
          vniPath = path4;
    }
+   // Priority 3: pinmame/altcolor/<rom>/<rom>.pal and pinmame/altcolor/<rom>/<rom>.vni
+   else if (auto path5 = find_case_insensitive_file_path(tablePath.parent_path() / "pinmame" / "altcolor" / gameId / palFile); !path5.empty())
+   {
+      palPath = path5;
+      if (auto path6 = find_case_insensitive_file_path(tablePath.parent_path() / "pinmame" / "altcolor" / gameId / vniFile); !path6.empty())
+         vniPath = path6;
+   }
+   // Priority 4: pinmame/altcolor/<rom>/pin2dmd.pal and pinmame/altcolor/<rom>/pin2dmd.vni
+   else if (auto path7 = find_case_insensitive_file_path(tablePath.parent_path() / "pinmame" / "altcolor" / gameId / pin2dmdPal); !path7.empty())
+   {
+      palPath = path7;
+      if (auto path8 = find_case_insensitive_file_path(tablePath.parent_path() / "pinmame" / "altcolor" / gameId / pin2dmdVni); !path8.empty())
+         vniPath = path8;
+   }
+   // Priority 5: global setting path
    else if (!vniBasePath.empty())
    {
-      if (auto path5 = find_case_insensitive_file_path(vniBasePath / gameId / palFile); !path5.empty())
+      if (auto path9 = find_case_insensitive_file_path(vniBasePath / gameId / palFile); !path9.empty())
       {
-         palPath = path5;
-         if (auto path6 = find_case_insensitive_file_path(vniBasePath / gameId / vniFile); !path6.empty())
-            vniPath = path6;
+         palPath = path9;
+         if (auto path10 = find_case_insensitive_file_path(vniBasePath / gameId / vniFile); !path10.empty())
+            vniPath = path10;
       }
    }
 
