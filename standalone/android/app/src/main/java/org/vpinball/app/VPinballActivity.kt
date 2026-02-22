db3889ba07cf2449b7522da820cc61d3e9aa4923
Jason Millard <jsm174@gmail.com>
2026-02-22 12:28:32 -0500

    android: keep screen enabled for main activity (@syd711)

--- standalone/android/app/src/main/java/org/vpinball/app/VPinballActivity.kt
+++ standalone/android/app/src/main/java/org/vpinball/app/VPinballActivity.kt
@@ -2,6 +2,7 @@ package org.vpinball.app
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.LaunchedEffect
@@ -17,6 +18,8 @@ class VPinballActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.addFlags(FLAG_KEEP_SCREEN_ON)
 
         VPinballManager.onActivityReady(this)
 
