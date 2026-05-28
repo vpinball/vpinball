vpinball/vpinball

commit dbd2a00aaf32cedaae441f00f3de5a59b2218bb4

BGFX: deprecate GLES option and remove fullscreen exclusive
- Fullscreen exclusive was not implemented for BGFX and is not offered by other platforms than Windows 7/8 (which we do not support)
- Fullscreen exclusive has been deprecated in WIndows 10 (july 2015) in favor of full screen optimization (vorderless window), so it is really not worth implementing as we already offer better options
- GLES is now implemented like other options in the general app/table ini scheme

src/core/Settings.cpp
@@ -149,16 +149,23 @@ void Settings::UpdateDefaults()
             break;
 
          const auto& conf = VPX::Window::GetDisplayConfig(GetWindow_Display(i));
+         #ifndef ENABLE_BGFX
          reg.Register(GetWindow_FSWidth_Property(i)->WithDefault(conf.width));
          reg.Register(GetWindow_FSHeight_Property(i)->WithDefault(conf.height));
+         #endif
          reg.Register(GetWindow_Width_Property(i)->WithDefault(i == 0 ? conf.width : (conf.width / 4)));
          reg.Register(GetWindow_Height_Property(i)->WithDefault(i == 0 ? conf.height : min(conf.width * 4 / 9, conf.height)));
          break;
       }
       case VPX::RenderOutput::OutputMode::OM_EMBEDDED:
       {
+         #ifdef ENABLE_BGFX
+         const auto w = GetWindow_Width(VPXWindowId::VPXWINDOW_Playfield);
+         const auto h = GetWindow_Height(VPXWindowId::VPXWINDOW_Playfield);
+         #else
          const auto w = GetWindow_FullScreen(VPXWindowId::VPXWINDOW_Playfield) ? GetWindow_FSWidth(VPXWindowId::VPXWINDOW_Playfield) : GetWindow_Width(VPXWindowId::VPXWINDOW_Playfield);
          const auto h = GetWindow_FullScreen(VPXWindowId::VPXWINDOW_Playfield) ? GetWindow_FSHeight(VPXWindowId::VPXWINDOW_Playfield) : GetWindow_Height(VPXWindowId::VPXWINDOW_Playfield);
+         #endif
          reg.Register(GetWindow_Width_Property(i)->WithDefault(w / 4));
          reg.Register(GetWindow_Height_Property(i)->WithDefault(min(w * 4 / 9, h)));
          break;

src/core/Settings_properties.inl
@@ -22,11 +22,13 @@ PropIntDyn(Player, PlayfieldWndX, "X Position"s, "Horizontal position of the win
 PropIntDyn(Player, PlayfieldWndY, "Y Position"s, "Vertical position of the window on the selected display"s, 0, 16384, 0);
 PropIntDyn(Player, PlayfieldWidth, "Width"s, "Width of the window"s, 0, 16384, 16384);
 PropIntDyn(Player, PlayfieldHeight, "Height"s, "Height of the window"s, 0, 16384, 16384);
+#ifndef ENABLE_BGFX
 PropBoolDyn(Player, PlayfieldFullScreen, "Fullscreen"s, "Use fullscreen exclusive mode\nThis should be avoided unless you need to change the display resolution"s, false);
 PropIntDyn(Player, PlayfieldFSWidth, "Width"s, "Fullscreen display mode width"s, 0, 16384, 16384);
 PropIntDyn(Player, PlayfieldFSHeight, "Height"s, "Fullscreen display mode height"s, 0, 16384, 16384);
 PropFloatDyn(Player, PlayfieldRefreshRate, "Refresh Rate"s, "Fullscreen display mode refresh rate"s, 0.f, 1000.f, 0.f);
 PropIntDyn(Player, PlayfieldColorDepth, "Color Depth"s, "Fullscreen display mode color depth"s, 0, 64, 32);
+#endif
 // Backglass Window
 PropEnumDyn(Backglass, BackglassOutput, "Output Mode"s, "Select between disabled, floating, or embedded in another window mode"s, int /* OutputMode */, 0 /* OM_DISABLED */, "Disabled"s,
    "Floating"s, "Embedded in playfield"s);
@@ -35,11 +37,13 @@ PropIntDyn(Backglass, BackglassWndX, "X Position"s, "Horizontal position of the 
 PropIntDyn(Backglass, BackglassWndY, "Y Position"s, "Vertical position of the window on the selected display"s, 0, 16384, 0);
 PropIntDyn(Backglass, BackglassWidth, "Width"s, "Width of the window"s, 0, 16384, 16384);
 PropIntDyn(Backglass, BackglassHeight, "Height"s, "Height of the window"s, 0, 16384, 16384);
+#ifndef ENABLE_BGFX
 PropBoolDyn(Backglass, BackglassFullScreen, "Fullscreen"s, "Use fullscreen exclusive mode\nThis should be avoided unless you need to change the display resolution"s, false);
 PropIntDyn(Backglass, BackglassFSWidth, "Width"s, "Fullscreen display mode width"s, 0, 16384, 16384);
 PropIntDyn(Backglass, BackglassFSHeight, "Height"s, "Fullscreen display mode height"s, 0, 16384, 16384);
 PropFloatDyn(Backglass, BackglassRefreshRate, "Fullscreen Refresh Rate"s, "Fullscreen display mode refresh rate"s, 0.f, 1000.f, 0.f);
 PropIntDyn(Backglass, BackglassColorDepth, "Color Depth"s, "Fullscreen display mode color depth"s, 0, 64, 32);
+#endif
 // ScoreView Window
 PropEnumDyn(ScoreView, ScoreViewOutput, "Output Mode"s, "Select between disabled, floating, or embedded in another window mode"s, int /* OutputMode */, 0 /* OM_DISABLED */, "Disabled"s,
    "Floating"s, "Embedded in playfield"s);
@@ -48,11 +52,13 @@ PropIntDyn(ScoreView, ScoreViewWndX, "X Position"s, "Horizontal position of the 
 PropIntDyn(ScoreView, ScoreViewWndY, "Y Position"s, "Vertical position of the window on the selected display"s, 0, 16384, 0);
 PropIntDyn(ScoreView, ScoreViewWidth, "Width"s, "Width of the window"s, 0, 16384, 16384);
 PropIntDyn(ScoreView, ScoreViewHeight, "Height"s, "Height of the window"s, 0, 16384, 16384);
+#ifndef ENABLE_BGFX
 PropBoolDyn(ScoreView, ScoreViewFullScreen, "Fullscreen"s, "Use fullscreen exclusive mode\nThis should be avoided unless you need to change the display resolution"s, false);
 PropIntDyn(ScoreView, ScoreViewFSWidth, "Width"s, "Fullscreen display mode width"s, 0, 16384, 16384);
 PropIntDyn(ScoreView, ScoreViewFSHeight, "Height"s, "Fullscreen display mode height"s, 0, 16384, 16384);
 PropFloatDyn(ScoreView, ScoreViewRefreshRate, "Fullscreen Refresh Rate"s, "Fullscreen display mode refresh rate"s, 0.f, 1000.f, 0.f);
 PropIntDyn(ScoreView, ScoreViewColorDepth, "Color Depth"s, "Fullscreen display mode color depth"s, 0, 64, 32);
+#endif
 // Topper Window
 PropEnumDyn(Topper, TopperOutput, "Output Mode"s, "Select between disabled, floating, or embedded in another window mode"s, int /* OutputMode */, 0 /* OM_DISABLED */, "Disabled"s, "Floating"s,
    "Embedded in playfield"s);
@@ -61,22 +67,26 @@ PropIntDyn(Topper, TopperWndX, "X Position"s, "Horizontal position of the Topper
 PropIntDyn(Topper, TopperWndY, "Y Position"s, "Vertical position of the Topper window on the selected display"s, 0, 16384, 0);
 PropIntDyn(Topper, TopperWidth, "Width"s, "Width of the Topper window"s, 0, 16384, 16384);
 PropIntDyn(Topper, TopperHeight, "Height"s, "Height of the Topper window"s, 0, 16384, 16384);
+#ifndef ENABLE_BGFX
 PropBoolDyn(Topper, TopperFullScreen, "Fullscreen"s, "Use fullscreen exclusive mode for the Topper window\nThis should be avoided unless you need to change the display resolution"s, false);
 PropIntDyn(Topper, TopperFSWidth, "Width"s, "Fullscreen display mode width for the Topper window"s, 0, 16384, 16384);
 PropIntDyn(Topper, TopperFSHeight, "Height"s, "Fullscreen display mode height for the Topper window"s, 0, 16384, 16384);
 PropFloatDyn(Topper, TopperRefreshRate, "Fullscreen Refresh Rate"s, "Fullscreen display mode refresh rate"s, 0.f, 1000.f, 0.f);
 PropIntDyn(Topper, TopperColorDepth, "Color Depth"s, "Fullscreen display mode color depth"s, 0, 64, 32);
+#endif
 // VR Preview Window
 PropStringDyn(PlayerVR, PreviewDisplay, "Display"s, "Display used for the VR Preview window"s, ""s);
 PropIntDyn(PlayerVR, PreviewWndX, "X Position"s, "Horizontal position of the window on the selected display"s, 0, 16384, 0);
 PropIntDyn(PlayerVR, PreviewWndY, "Y Position"s, "Vertical position of the window on the selected display"s, 0, 16384, 0);
 PropIntDyn(PlayerVR, PreviewWidth, "Width"s, "Width of the window"s, 0, 16384, 16384);
 PropIntDyn(PlayerVR, PreviewHeight, "Height"s, "Height of the window"s, 0, 16384, 16384);
+#ifndef ENABLE_BGFX
 PropBoolDyn(PlayerVR, PreviewFullScreen, "Fullscreen"s, "Use fullscreen exclusive mode\nThis should be avoided unless you need to change the display resolution."s, false);
 PropIntDyn(PlayerVR, PreviewFSWidth, "Width"s, "Fullscreen display mode width"s, 0, 16384, 16384);
 PropIntDyn(PlayerVR, PreviewFSHeight, "Height"s, "Fullscreen display mode height"s, 0, 16384, 16384);
 PropFloatDyn(PlayerVR, PreviewRefreshRate, "Fullscreen Refresh Rate"s, "Fullscreen display mode refresh rate"s, 0.f, 1000.f, 0.f);
 PropIntDyn(PlayerVR, PreviewColorDepth, "Color Depth"s, "Fullscreen display mode color depth"s, 0, 64, 32);
+#endif
 // Array access (using VPXWindowId)
 PropArray(Window, Mode, int, Enum, Int, m_propInvalid, m_propBackglass_BackglassOutput, m_propScoreView_ScoreViewOutput, m_propTopper_TopperOutput, m_propInvalid);
 PropArray(Window, Display, string, String, String, m_propPlayer_PlayfieldDisplay, m_propBackglass_BackglassDisplay, m_propScoreView_ScoreViewDisplay, m_propTopper_TopperDisplay,
@@ -86,6 +96,7 @@ PropArray(Window, WndY, int, Int, Int, m_propPlayer_PlayfieldWndY, m_propBackgla
 PropArray(Window, Width, int, Int, Int, m_propPlayer_PlayfieldWidth, m_propBackglass_BackglassWidth, m_propScoreView_ScoreViewWidth, m_propTopper_TopperWidth, m_propPlayerVR_PreviewWidth);
 PropArray(
    Window, Height, int, Int, Int, m_propPlayer_PlayfieldHeight, m_propBackglass_BackglassHeight, m_propScoreView_ScoreViewHeight, m_propTopper_TopperHeight, m_propPlayerVR_PreviewHeight);
+#ifndef ENABLE_BGFX
 PropArray(Window, FullScreen, bool, Bool, Int, m_propPlayer_PlayfieldFullScreen, m_propBackglass_BackglassFullScreen, m_propScoreView_ScoreViewFullScreen, m_propTopper_TopperFullScreen,
    m_propPlayerVR_PreviewFullScreen);
 PropArray(Window, FSWidth, int, Int, Int, m_propPlayer_PlayfieldFSWidth, m_propBackglass_BackglassFSWidth, m_propScoreView_ScoreViewFSWidth, m_propTopper_TopperFSWidth,
@@ -96,6 +107,7 @@ PropArray(Window, FSRefreshRate, float, Float, Float, m_propPlayer_PlayfieldRefr
    m_propTopper_TopperRefreshRate, m_propPlayerVR_PreviewRefreshRate);
 PropArray(Window, FSColorDepth, int, Int, Int, m_propPlayer_PlayfieldColorDepth, m_propBackglass_BackglassColorDepth, m_propScoreView_ScoreViewColorDepth, m_propTopper_TopperColorDepth,
    m_propPlayerVR_PreviewColorDepth);
+#endif
 
 // Graphics synchronisation and latency reduction
 PropEnum(Player, SyncMode, "Synchronization"s,

src/renderer/Window.cpp
@@ -70,6 +70,9 @@ Window::Window(const string& title, const Settings& settings, VPXWindowId window
    : m_windowId(windowId)
    , m_isVR(false)
 {
+#ifdef ENABLE_BGFX
+   m_fullscreen = false;
+#else
    m_fullscreen = g_isMobile || settings.GetWindow_FullScreen(m_windowId);
    if (!g_isMobile && m_windowId == VPXWindowId::VPXWINDOW_Playfield)
    {
@@ -79,6 +82,7 @@ Window::Window(const string& title, const Settings& settings, VPXWindowId window
       else if (g_app->m_disEnableTrueFullscreen == 1)
          m_fullscreen = true;
    }
+#endif
 
    // Both fullscreen and windowed modes are anchored to a user selected display
    const DisplayConfig selectedDisplay = GetDisplayConfig(settings.GetWindow_Display((int)m_windowId));
@@ -96,6 +100,7 @@ Window::Window(const string& title, const Settings& settings, VPXWindowId window
    const SDL_DisplayMode* fullscreenDisplayMode = nullptr;
 
    constexpr bool isExtCreatedWindow = g_isMobile && g_isIOS;
+   #ifndef ENABLE_BGFX
    if (m_fullscreen && !isExtCreatedWindow)
    {
       if (g_isAndroid) {
@@ -141,6 +146,7 @@ Window::Window(const string& title, const Settings& settings, VPXWindowId window
                << settings.GetWindow_FSRefreshRate(m_windowId) << "Hz, Bit depth: " << settings.GetWindow_FSColorDepth(m_windowId) << " is not available. Switching to windowed mode.";
       }
    }
+   #endif
 
    // Fullscreen failed or was not requested, setup windowed mode
    if (!m_fullscreen || fullscreenDisplayMode == nullptr)

src/ui/live/ingameui/DisplaySettingsPage.cpp
@@ -271,6 +271,7 @@ void DisplaySettingsPage::BuildWindowPage()
       [this](Settings& settings) { settings.ResetWindow_Display(m_wndId); }, //
       [this](int v, Settings& settings, bool asTableOverride) { settings.SetWindow_Display(m_wndId, m_displays[v].displayName, asTableOverride); }));
 
+#ifndef ENABLE_BGFX
    // TODO this property is directly persisted. It does not follow the overall UI design: App/Table/Live state => Implement live state (will also enable table override)
    AddItem(std::make_unique<InGameUIItem>(
               Settings::m_propWindow_FullScreen[m_wndId], //
@@ -281,8 +282,7 @@ void DisplaySettingsPage::BuildWindowPage()
                  m_player->m_ptable->m_settings.SetWindow_FullScreen(m_wndId, v, false);
                  BuildPage();
               }))
-      .m_excludeFromDefault
-      = true;
+      .m_excludeFromDefault = true;
 
    const bool isFullScreen = m_player->m_ptable->m_settings.GetWindow_FullScreen(m_wndId);
    if (isFullScreen)
@@ -335,6 +335,7 @@ void DisplaySettingsPage::BuildWindowPage()
          [](int, Settings&, bool) { /* Directly stored on change, nothing to do */ }));
    }
    else
+#endif
    {
       const int containerWidth = m_displays[wndDisplay].width;
       const int containerHeight = m_displays[wndDisplay].height;

src/core/AppCommands.cpp
@@ -312,10 +312,12 @@ enum option_names
    OPTION_CUSTOM8,
    OPTION_CUSTOM9,
    OPTION_PREFPATH,
+   OPTION_LESSCPUTHREADS,
+#ifndef ENABLE_BGFX
    OPTION_DISABLETRUEFULLSCREEN,
    OPTION_ENABLETRUEFULLSCREEN,
    OPTION_GLES,
-   OPTION_LESSCPUTHREADS,
+#endif
    OPTION_INVALID,
 };
 struct CommandLineOption
@@ -361,9 +363,11 @@ static const CommandLineOption options[] = {
    { OPTION_CUSTOM9, "c9"s, "Custom value 9"s },
    { OPTION_PREFPATH, "PrefPath"s, "[path]  Use a custom preferences path instead of default"s },
    { OPTION_LESSCPUTHREADS, "LessCPUthreads"s, "Limit the amount of parallel execution"s },
+#ifndef ENABLE_BGFX
    { OPTION_DISABLETRUEFULLSCREEN, "DisableTrueFullscreen"s, "Force-disable True Fullscreen setting [Deprecated, uses ini serttings instead]"s },
    { OPTION_ENABLETRUEFULLSCREEN, "EnableTrueFullscreen"s, "Force-enable True Fullscreen setting [Deprecated, uses ini serttings instead]"s },
    { OPTION_GLES, "GLES"s, "[value]  Overrides the global emission scale (day/night setting, value range: 0.115..0.925) [Deprecated, uses ini serttings instead]"s },
+#endif
 };
 
 std::filesystem::path CommandLineProcessor::GetPathFromArg(const string& arg)
@@ -473,6 +477,7 @@ void CommandLineProcessor::ProcessCommandLine(int nArgs, const char* szArglist[]
          g_app->LimitMultiThreading();
          break;
 
+#ifndef ENABLE_BGFX
       // FIXME remove as this is now handled by the ini system
       case OPTION_DISABLETRUEFULLSCREEN:
          g_app->m_disEnableTrueFullscreen = 0;
@@ -500,6 +505,7 @@ void CommandLineProcessor::ProcessCommandLine(int nArgs, const char* szArglist[]
             OnCommandLineError("Command Line Error"s, "Missing global emission scale adjustment"s);
          }
          break;
+#endif
 
       case OPTION_CUSTOM1:
       case OPTION_CUSTOM2:

src/core/VPApp.h
@@ -26,10 +26,12 @@ public:
    // Global custom parameters that can be set through command line and accessed in the script via GetCustomParam(X)
    wstring m_customParameters[MAX_CUSTOM_PARAM_INDEX];
 
+#ifndef ENABLE_BGFX
    // FIXME Deprecated command line options (supposed to be handled through INI nowadays)
    int m_disEnableTrueFullscreen = -1;
    bool m_bgles = false; // override global emission scale by m_fgles below
    float m_fgles = 0.f;
+#endif
 
    // Script security level
    int m_securitylevel;

src/renderer/RenderDevice.cpp
@@ -619,10 +619,10 @@ void RenderDevice::RenderThread(RenderDevice* rd, bgfx::Init init)
                // we start a flush sequence:
                // - process a few frames without VSync to flush the queue (as they will be discarded or presented directly)
                // - then process a few frame with VSync enabled, to measure the new number of frames in flights
-               // This is not really correct as gpuFrameNum is the last porocessed frame, not the last presented frame. Therefore
+               // This is not really correct as gpuFrameNum is the last processed frame, not the last presented frame. Therefore
                // if all frames are quickly processed, gpuFrameNum will be the same as m_lastPresentFrameIdx, but the present queue
                // will be filled up anyway, leading to high latency. The user needs to limit the maximum number of prerendered frame to
-               // avoid this situation. Still, the tests seem to dhow that the estimate is good enough.
+               // avoid this situation. Still, the tests seem to show that the estimate is good enough.
                const uint32_t framesInFlight = rd->m_lastPresentFrameIdx - bgfx::getStats()->gpuFrameNum;
                if (framesInFlight > 3)
                   framePacingFlushing = 8;

src/renderer/Renderer.cpp
@@ -386,11 +386,13 @@ Renderer::SceneLighting::SceneLighting(PinTable* const table)
 
 void Renderer::SceneLighting::Update()
 {
+#ifndef ENABLE_BGFX
    if (g_app->m_bgles) // Overriden from command line
    {
       m_emissionScale = g_app->m_fgles;
       return;
    }
+#endif
    switch (m_mode)
    {
    case Mode::Table:

commit ba00f92f98948aca103dc5090e390cf7e57c88ae

linux: restore fullscreen for BGFX (regressed by dbd2a00aa)
On Linux BGFX the playfield stopped going fullscreen: a screen-sized borderless
window is not promoted to fullscreen by the compositor the way Windows does, so
it ended up placed under panels and clipped off-screen.

Bring back the Fullscreen setting and toggle as fullscreen-desktop (no video
mode change), default it on for the playfield, and leave Windows+BGFX unchanged.

src/core/Settings.cpp
@@ -153,6 +153,12 @@ void Settings::UpdateDefaults()
          reg.Register(GetWindow_FSWidth_Property(i)->WithDefault(conf.width));
          reg.Register(GetWindow_FSHeight_Property(i)->WithDefault(conf.height));
          #endif
+         #if defined(ENABLE_BGFX) && !defined(_MSC_VER)
+         // On Linux/macOS a screen-sized borderless window is not promoted to fullscreen by the
+         // compositor, so default the playfield to fullscreen(-desktop) for a usable fresh install.
+         if (i == VPXWindowId::VPXWINDOW_Playfield)
+            reg.Register(GetWindow_FullScreen_Property(i)->WithDefault(true));
+         #endif
          reg.Register(GetWindow_Width_Property(i)->WithDefault(i == 0 ? conf.width : (conf.width / 4)));
          reg.Register(GetWindow_Height_Property(i)->WithDefault(i == 0 ? conf.height : min(conf.width * 4 / 9, conf.height)));
          break;

src/core/Settings_properties.inl
@@ -22,8 +22,11 @@ PropIntDyn(Player, PlayfieldWndX, "X Position"s, "Horizontal position of the win
 PropIntDyn(Player, PlayfieldWndY, "Y Position"s, "Vertical position of the window on the selected display"s, 0, 16384, 0);
 PropIntDyn(Player, PlayfieldWidth, "Width"s, "Width of the window"s, 0, 16384, 16384);
 PropIntDyn(Player, PlayfieldHeight, "Height"s, "Height of the window"s, 0, 16384, 16384);
-#ifndef ENABLE_BGFX
+// No fullscreen option on Windows+BGFX: a screen-sized borderless window already acts as fullscreen there.
+#if !defined(ENABLE_BGFX) || !defined(_MSC_VER)
 PropBoolDyn(Player, PlayfieldFullScreen, "Fullscreen"s, "Use fullscreen exclusive mode\nThis should be avoided unless you need to change the display resolution"s, false);
+#endif
+#ifndef ENABLE_BGFX
 PropIntDyn(Player, PlayfieldFSWidth, "Width"s, "Fullscreen display mode width"s, 0, 16384, 16384);
 PropIntDyn(Player, PlayfieldFSHeight, "Height"s, "Fullscreen display mode height"s, 0, 16384, 16384);
 PropFloatDyn(Player, PlayfieldRefreshRate, "Refresh Rate"s, "Fullscreen display mode refresh rate"s, 0.f, 1000.f, 0.f);
@@ -37,8 +40,10 @@ PropIntDyn(Backglass, BackglassWndX, "X Position"s, "Horizontal position of the 
 PropIntDyn(Backglass, BackglassWndY, "Y Position"s, "Vertical position of the window on the selected display"s, 0, 16384, 0);
 PropIntDyn(Backglass, BackglassWidth, "Width"s, "Width of the window"s, 0, 16384, 16384);
 PropIntDyn(Backglass, BackglassHeight, "Height"s, "Height of the window"s, 0, 16384, 16384);
-#ifndef ENABLE_BGFX
+#if !defined(ENABLE_BGFX) || !defined(_MSC_VER)
 PropBoolDyn(Backglass, BackglassFullScreen, "Fullscreen"s, "Use fullscreen exclusive mode\nThis should be avoided unless you need to change the display resolution"s, false);
+#endif
+#ifndef ENABLE_BGFX
 PropIntDyn(Backglass, BackglassFSWidth, "Width"s, "Fullscreen display mode width"s, 0, 16384, 16384);
 PropIntDyn(Backglass, BackglassFSHeight, "Height"s, "Fullscreen display mode height"s, 0, 16384, 16384);
 PropFloatDyn(Backglass, BackglassRefreshRate, "Fullscreen Refresh Rate"s, "Fullscreen display mode refresh rate"s, 0.f, 1000.f, 0.f);
@@ -52,8 +57,10 @@ PropIntDyn(ScoreView, ScoreViewWndX, "X Position"s, "Horizontal position of the 
 PropIntDyn(ScoreView, ScoreViewWndY, "Y Position"s, "Vertical position of the window on the selected display"s, 0, 16384, 0);
 PropIntDyn(ScoreView, ScoreViewWidth, "Width"s, "Width of the window"s, 0, 16384, 16384);
 PropIntDyn(ScoreView, ScoreViewHeight, "Height"s, "Height of the window"s, 0, 16384, 16384);
-#ifndef ENABLE_BGFX
+#if !defined(ENABLE_BGFX) || !defined(_MSC_VER)
 PropBoolDyn(ScoreView, ScoreViewFullScreen, "Fullscreen"s, "Use fullscreen exclusive mode\nThis should be avoided unless you need to change the display resolution"s, false);
+#endif
+#ifndef ENABLE_BGFX
 PropIntDyn(ScoreView, ScoreViewFSWidth, "Width"s, "Fullscreen display mode width"s, 0, 16384, 16384);
 PropIntDyn(ScoreView, ScoreViewFSHeight, "Height"s, "Fullscreen display mode height"s, 0, 16384, 16384);
 PropFloatDyn(ScoreView, ScoreViewRefreshRate, "Fullscreen Refresh Rate"s, "Fullscreen display mode refresh rate"s, 0.f, 1000.f, 0.f);
@@ -67,8 +74,10 @@ PropIntDyn(Topper, TopperWndX, "X Position"s, "Horizontal position of the Topper
 PropIntDyn(Topper, TopperWndY, "Y Position"s, "Vertical position of the Topper window on the selected display"s, 0, 16384, 0);
 PropIntDyn(Topper, TopperWidth, "Width"s, "Width of the Topper window"s, 0, 16384, 16384);
 PropIntDyn(Topper, TopperHeight, "Height"s, "Height of the Topper window"s, 0, 16384, 16384);
-#ifndef ENABLE_BGFX
+#if !defined(ENABLE_BGFX) || !defined(_MSC_VER)
 PropBoolDyn(Topper, TopperFullScreen, "Fullscreen"s, "Use fullscreen exclusive mode for the Topper window\nThis should be avoided unless you need to change the display resolution"s, false);
+#endif
+#ifndef ENABLE_BGFX
 PropIntDyn(Topper, TopperFSWidth, "Width"s, "Fullscreen display mode width for the Topper window"s, 0, 16384, 16384);
 PropIntDyn(Topper, TopperFSHeight, "Height"s, "Fullscreen display mode height for the Topper window"s, 0, 16384, 16384);
 PropFloatDyn(Topper, TopperRefreshRate, "Fullscreen Refresh Rate"s, "Fullscreen display mode refresh rate"s, 0.f, 1000.f, 0.f);
@@ -80,8 +89,10 @@ PropIntDyn(PlayerVR, PreviewWndX, "X Position"s, "Horizontal position of the win
 PropIntDyn(PlayerVR, PreviewWndY, "Y Position"s, "Vertical position of the window on the selected display"s, 0, 16384, 0);
 PropIntDyn(PlayerVR, PreviewWidth, "Width"s, "Width of the window"s, 0, 16384, 16384);
 PropIntDyn(PlayerVR, PreviewHeight, "Height"s, "Height of the window"s, 0, 16384, 16384);
-#ifndef ENABLE_BGFX
+#if !defined(ENABLE_BGFX) || !defined(_MSC_VER)
 PropBoolDyn(PlayerVR, PreviewFullScreen, "Fullscreen"s, "Use fullscreen exclusive mode\nThis should be avoided unless you need to change the display resolution."s, false);
+#endif
+#ifndef ENABLE_BGFX
 PropIntDyn(PlayerVR, PreviewFSWidth, "Width"s, "Fullscreen display mode width"s, 0, 16384, 16384);
 PropIntDyn(PlayerVR, PreviewFSHeight, "Height"s, "Fullscreen display mode height"s, 0, 16384, 16384);
 PropFloatDyn(PlayerVR, PreviewRefreshRate, "Fullscreen Refresh Rate"s, "Fullscreen display mode refresh rate"s, 0.f, 1000.f, 0.f);
@@ -96,9 +107,11 @@ PropArray(Window, WndY, int, Int, Int, m_propPlayer_PlayfieldWndY, m_propBackgla
 PropArray(Window, Width, int, Int, Int, m_propPlayer_PlayfieldWidth, m_propBackglass_BackglassWidth, m_propScoreView_ScoreViewWidth, m_propTopper_TopperWidth, m_propPlayerVR_PreviewWidth);
 PropArray(
    Window, Height, int, Int, Int, m_propPlayer_PlayfieldHeight, m_propBackglass_BackglassHeight, m_propScoreView_ScoreViewHeight, m_propTopper_TopperHeight, m_propPlayerVR_PreviewHeight);
-#ifndef ENABLE_BGFX
+#if !defined(ENABLE_BGFX) || !defined(_MSC_VER)
 PropArray(Window, FullScreen, bool, Bool, Int, m_propPlayer_PlayfieldFullScreen, m_propBackglass_BackglassFullScreen, m_propScoreView_ScoreViewFullScreen, m_propTopper_TopperFullScreen,
    m_propPlayerVR_PreviewFullScreen);
+#endif
+#ifndef ENABLE_BGFX
 PropArray(Window, FSWidth, int, Int, Int, m_propPlayer_PlayfieldFSWidth, m_propBackglass_BackglassFSWidth, m_propScoreView_ScoreViewFSWidth, m_propTopper_TopperFSWidth,
    m_propPlayerVR_PreviewFSWidth);
 PropArray(Window, FSHeight, int, Int, Int, m_propPlayer_PlayfieldFSHeight, m_propBackglass_BackglassFSHeight, m_propScoreView_ScoreViewFSHeight, m_propTopper_TopperFSHeight,

src/renderer/Window.cpp
@@ -74,10 +74,17 @@ Window::Window(const string& title, const Settings& settings, VPXWindowId window
    : m_windowId(windowId)
    , m_isVR(false)
 {
-#ifdef ENABLE_BGFX
+#if defined(ENABLE_BGFX) && defined(_MSC_VER)
+   // Windows+BGFX: a screen-sized borderless window already acts as fullscreen (Windows fullscreen
+   // optimization), so no explicit fullscreen flag is used and there is no Fullscreen setting.
    m_fullscreen = false;
 #else
+   // BGFX has no exclusive fullscreen (video mode change): for it m_fullscreen requests a
+   // fullscreen-desktop window. This is what makes a screen-sized playfield actually fill the
+   // display on Linux/macOS, where the compositor does not promote a borderless window the way
+   // Windows does. Other backends may still use exclusive fullscreen with a display mode (below).
    m_fullscreen = g_isMobile || settings.GetWindow_FullScreen(m_windowId);
+   #ifndef ENABLE_BGFX
    if (!g_isMobile && m_windowId == VPXWindowId::VPXWINDOW_Playfield)
    {
       // FIXME remove command line override => this is hacky and not needed anymore (use INI override instead)
@@ -86,6 +93,7 @@ Window::Window(const string& title, const Settings& settings, VPXWindowId window
       else if (g_app->m_disEnableTrueFullscreen == 1)
          m_fullscreen = true;
    }
+   #endif
 #endif
 
    // Both fullscreen and windowed modes are anchored to a user selected display
@@ -161,7 +169,11 @@ Window::Window(const string& title, const Settings& settings, VPXWindowId window
       m_height = m_fullscreen ? m_screenheight : settings.GetWindow_Height(m_windowId);
       m_refreshrate = selectedDisplay.refreshrate;
       m_bitdepth = selectedDisplay.depth;
+      #if !defined(ENABLE_BGFX) || defined(_MSC_VER)
+      // Exclusive fullscreen failed or was not requested: fall back to windowed. Non-Windows BGFX has
+      // no exclusive mode, so it keeps m_fullscreen here to request a fullscreen-desktop window.
       m_fullscreen = false;
+      #endif
 
       // Constrain window to screen
       if (m_width > m_screenwidth)
@@ -196,8 +208,8 @@ Window::Window(const string& title, const Settings& settings, VPXWindowId window
    assert(m_height > 0 && m_height <= m_screenheight);
    assert(selectedDisplay.left <= wnd_x);
    assert(selectedDisplay.top <= wnd_y);
-   assert((wnd_x + m_width) <= (selectedDisplay.left + (m_fullscreen ? fullscreenDisplayMode->w : selectedDisplay.width))); // The fullscreen mode may have a different orientation than the display on mobile devices
-   assert((wnd_y + m_height) <= (selectedDisplay.top + (m_fullscreen ? fullscreenDisplayMode->h : selectedDisplay.height)));
+   assert((wnd_x + m_width) <= (selectedDisplay.left + ((m_fullscreen && fullscreenDisplayMode) ? fullscreenDisplayMode->w : selectedDisplay.width))); // The fullscreen mode may have a different orientation than the display on mobile devices
+   assert((wnd_y + m_height) <= (selectedDisplay.top + ((m_fullscreen && fullscreenDisplayMode) ? fullscreenDisplayMode->h : selectedDisplay.height)));
    if (m_refreshrate <= 0)
    {
       PLOGE << "Failed to get display refresh rate. VPX will use a 60Hz default which may be wrong and cause bad video synchronization.";

src/ui/live/ingameui/DisplaySettingsPage.cpp
@@ -290,7 +290,10 @@ void DisplaySettingsPage::BuildWindowPage()
       [this](Settings& settings) { settings.ResetWindow_Display(m_wndId); }, //
       [this](int v, Settings& settings, bool asTableOverride) { settings.SetWindow_Display(m_wndId, m_displays[v].displayName, asTableOverride); }));
 
-#ifndef ENABLE_BGFX
+   // Fullscreen toggle. On BGFX fullscreen means fullscreen-desktop (no exclusive video mode change),
+   // so there is no resolution selector below; other backends additionally show a video-mode picker
+   // when fullscreen is enabled. Not offered on Windows+BGFX (borderless screen-sized acts as fullscreen).
+#if !defined(ENABLE_BGFX) || !defined(_MSC_VER)
    // TODO this property is directly persisted. It does not follow the overall UI design: App/Table/Live state => Implement live state (will also enable table override)
    AddItem(std::make_unique<InGameUIItem>(
               Settings::m_propWindow_FullScreen[m_wndId], //
@@ -304,6 +307,10 @@ void DisplaySettingsPage::BuildWindowPage()
       .m_excludeFromDefault = true;
 
    const bool isFullScreen = m_player->m_ptable->m_settings.GetWindow_FullScreen(m_wndId);
+#else
+   const bool isFullScreen = false;
+#endif
+#ifndef ENABLE_BGFX
    if (isFullScreen)
    {
       int defaultMode = 0, selectedMode = 0, i = 0;
@@ -355,6 +362,7 @@ void DisplaySettingsPage::BuildWindowPage()
    }
    else
 #endif
+   if (!isFullScreen)
    {
       const int containerWidth = m_displays[wndDisplay].width;
       const int containerHeight = m_displays[wndDisplay].height;