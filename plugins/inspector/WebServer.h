a2351b5e3f889608cd7d353194020bc8eba97c9d
Vincent Bousquet <vincent.bousqt@gmail.com>
2026-06-24 22:52:34 +0200

    Add (skeleton) inspector plugin to inspect running controllers

---
+++ plugins/inspector/WebServer.h
@@ -0,0 +1,42 @@
+// license:GPLv3+
+
+#pragma once
+
+#include <thread>
+#include <mutex>
+#include <string>
+#include <memory>
+#include <mongoose/mongoose.h>
+
+#include <vector>
+#include <string>
+#include <mutex>
+
+namespace Inspector {
+
+class WebServer {
+public:
+   WebServer();
+   ~WebServer();
+
+   void Start(int port);
+   void Stop();
+
+   void UpdateTreeJson(const std::string& json);
+
+   static void EventHandler(struct mg_connection *c, int ev, void *ev_data);
+
+private:
+   void Info(struct mg_connection *c, struct mg_http_message* hm);
+   void ApiTree(struct mg_connection *c, struct mg_http_message* hm);
+   void Root(struct mg_connection *c, struct mg_http_message* hm);
+
+   std::mutex m_treeMutex;
+   std::string m_treeJson;
+
+   struct mg_mgr m_mgr;
+   bool m_run;
+   std::unique_ptr<std::thread> m_pThread;
+};
+
+} // namespace Inspector
