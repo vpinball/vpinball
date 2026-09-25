e9fb8c2eac2b361786c70f29057db2c62ae3d8e8
Vincent Bousquet <vincent.bousqt@gmail.com>
2026-09-25 13:41:43 +0200

    LiveEditor: rotate curves around their centroid (instead of bounds center)

--- src/ui/live/editor/SurfaceUIPart.h
+++ src/ui/live/editor/SurfaceUIPart.h
@@ -15,7 +15,7 @@ public:
    TransformMask GetTransform(Matrix3D& transform) override;
    void SetTransform(const vec3& pos, const vec3& scale, const vec3& rot) override;
 
-   DragPointCurve* GetDragPointCurve() override { return &m_part->m_curve; }
+   DragPointCurve* GetDragPointCurve() const override { return &m_part->m_curve; }
    float GetDragPointZ(const DragPoint* point) const override { return point->GetZ() + 0.5f * (m_part->m_d.m_heightbottom + m_part->m_d.m_heighttop); }
 
    void RenderOverlay(const EditorRenderContext& ctx) override;
