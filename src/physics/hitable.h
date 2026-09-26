7116a35ea3600708a033badbc4d8df0eeb0a3e09
Vincent Bousquet <vincent.bousqt@gmail.com>
2026-09-26 09:37:23 +0200

    Physics: fix inverted comment

--- src/physics/hitable.h
+++ src/physics/hitable.h
@@ -10,6 +10,6 @@ public:
    virtual bool PhysicUpdate(class PhysicsEngine* physics, const bool isUI) { return false; } // Return false if update was not performed (object will be released and setup from scratch)
    virtual void PhysicRelease(class PhysicsEngine* physics, const bool isUI) = 0;
 
-   virtual bool IsConstCollidable() const { return true; } // Flag if the collidable state reported by IsCollidable may change during simulation
+   virtual bool IsConstCollidable() const { return true; } // Flag if the collidable state reported by IsCollidable won't change during simulation
    virtual bool IsCollidable() const { return true; } // Allow to early out when a quadtree section only contains a unique Hitable
 };
